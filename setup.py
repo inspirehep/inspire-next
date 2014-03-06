19060080d1224856d343530debee7a83fe2ceca0
Javier Martin Montull <javier.martin.montull@cern.ch>
2014-03-06 16:14:34 +0100

    general: tree structure changes

--- setup.py
+++ setup.py
@@ -1,19 +1,24 @@
+# -*- coding: utf-8 -*-
+#
 ## This file is part of INSPIRE.
-## Copyright (C) 2013, 2014 CERN.
+## Copyright (C) 2012, 2013 CERN.
 ##
-## INSPIRE is free software; you can redistribute it and/or
-## modify it under the terms of the GNU General Public License as
-## published by the Free Software Foundation; either version 2 of the
-## License, or (at your option) any later version.
+## INSPIRE is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
 ##
-## INSPIRE is distributed in the hope that it will be useful, but
-## WITHOUT ANY WARRANTY; without even the implied warranty of
-## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-## General Public License for more details.
+## INSPIRE is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
 ##
 ## You should have received a copy of the GNU General Public License
-## along with INSPIRE; if not, write to the Free Software Foundation, Inc.,
-## 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+## along with INSPIRE. If not, see <http://www.gnu.org/licenses/>.
+##
+## In applying this licence, CERN does not waive the privileges and immunities
+## granted to it by virtue of its status as an Intergovernmental Organization
+## or submit itself to any jurisdiction.
 
 """
 INSPIRE overlay
@@ -21,16 +26,10 @@ INSPIRE overlay
 
 INSPIRE overlay repository for Invenio.
 """
-from __future__ import print_function
-
 import re
 import glob
 
 from setuptools import setup, find_packages
-
-
-def match_feature_name(filename):
-    return re.match(r".*requirements-(\w+).txt$", filename).group(1)
 
 
 def read_requirements(filename='requirements.txt'):
@@ -48,46 +47,28 @@ def read_requirements(filename='requirements.txt'):
 
 install_requires, dependency_links = read_requirements()
 
-# Finds all `requirements-*.txt` files and prepares dictionary with extra
-# requirements (NOTE: no links are allowed here!)
-extras_require = dict(map(
-    lambda filename: (match_feature_name(filename),
-                      read_requirements(filename)[0]),
-    glob.glob('requirements-*.txt')))
-
 packages = find_packages(exclude=['docs'])
 
 setup(
     name='Inspire',
-    version='1.9999-dev',
+    version='dev',
     url='https://github.com/inspirehep/inspire-next',
-    license='GPLv2',
+    license='GPLv3',
     author='CERN',
     author_email='admin@inspirehep.net',
     description='Digital library software',
     long_description=__doc__,
     packages=packages,
-    package_dir={'invenio_docs': 'docs'},
-    namespace_packages=["inspire"],
+    namespace_packages=["inspire", "inspire.ext", ],
     include_package_data=True,
     zip_safe=False,
     platforms='any',
     install_requires=install_requires,
     dependency_links=dependency_links,
-    extras_require=extras_require,
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: GPLv2 License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-    ],
     test_suite='inspire.testsuite.suite',
     entry_points={
         'invenio.config': [
-            "site = inspire.config"
+            "inspire = inspire.config"
         ]
     },
 )
