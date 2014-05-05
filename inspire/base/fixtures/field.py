3bc20d497f11c466a3f055c3978362ad1a36511d
Kamil Neczaj <kamil.neczaj@cern.ch>
2014-05-05 13:52:19 +0200

    fixtures: tag, field, field_tag tables from production

---
+++ inspire/base/fixtures/field.py
@@ -0,0 +1,284 @@
+# -*- coding: utf-8 -*-
+#
+## This file is part of INSPIRE.
+## Copyright (C) 2014 CERN.
+##
+## INSPIRE is free software; you can redistribute it and/or
+## modify it under the terms of the GNU General Public License as
+## published by the Free Software Foundation; either version 2 of the
+## License, or (at your option) any later version.
+##
+## INSPIRE is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+## General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with INSPIRE; if not, write to the Free Software Foundation, Inc.,
+## 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+#
+
+from fixture import DataSet 
+
+
+class FieldData(DataSet):
+
+    class FieldData_1:
+        id = 1
+        name = u'any field'
+        code = u'anyfield'
+
+    class FieldData_2:
+        id = 2
+        name = u'title'
+        code = u'title'
+
+    class FieldData_3:
+        id = 3
+        name = u'author'
+        code = u'author'
+
+    class FieldData_4:
+        id = 4
+        name = u'abstract'
+        code = u'abstract'
+
+    class FieldData_5:
+        id = 5
+        name = u'keyword'
+        code = u'keyword'
+
+    class FieldData_6:
+        id = 6
+        name = u'report number'
+        code = u'reportnumber'
+
+    class FieldData_7:
+        id = 7
+        name = u'subject'
+        code = u'subject'
+
+    class FieldData_8:
+        id = 8
+        name = u'reference'
+        code = u'reference'
+
+    class FieldData_9:
+        id = 9
+        name = u'collection'
+        code = u'collection'
+
+    class FieldData_10:
+        id = 10
+        name = u'year'
+        code = u'year'
+
+    class FieldData_11:
+        id = 11
+        name = u'experiment'
+        code = u'experiment'
+
+    class FieldData_12:
+        id = 12
+        name = u'doi'
+        code = u'doi'
+
+    class FieldData_13:
+        id = 13
+        name = u'journal'
+        code = u'journal'
+
+    class FieldData_14:
+        id = 14
+        name = u'record ID'
+        code = u'recid'
+
+    class FieldData_15:
+        id = 15
+        name = u'affiliation'
+        code = u'affiliation'
+
+    class FieldData_16:
+        id = 16
+        name = u'collaboration'
+        code = u'collaboration'
+
+    class FieldData_17:
+        id = 17
+        name = u'exact author'
+        code = u'exactauthor'
+
+    class FieldData_18:
+        id = 18
+        name = u'date created'
+        code = u'datecreated'
+
+    class FieldData_19:
+        id = 19
+        name = u'date modified'
+        code = u'datemodified'
+
+    class FieldData_20:
+        id = 20
+        name = u'refers to'
+        code = u'refersto'
+
+    class FieldData_21:
+        id = 21
+        name = u'cited by'
+        code = u'citedby'
+
+    class FieldData_22:
+        id = 22
+        name = u'fulltext'
+        code = u'fulltext'
+
+    class FieldData_23:
+        id = 23
+        name = u'caption'
+        code = u'caption'
+
+    class FieldData_24:
+        id = 24
+        name = u'first author'
+        code = u'firstauthor'
+
+    class FieldData_25:
+        id = 25
+        name = u'address'
+        code = u'address'
+
+    class FieldData_26:
+        id = 26
+        name = u'postal code'
+        code = u'postalcode'
+
+    class FieldData_27:
+        id = 27
+        name = u'country'
+        code = u'country'
+
+    class FieldData_28:
+        id = 28
+        name = u'city'
+        code = u'city'
+
+    class FieldData_29:
+        id = 29
+        name = u'region'
+        code = u'region'
+
+    class FieldData_30:
+        id = 30
+        name = u'institution name'
+        code = u'institutionname'
+
+    class FieldData_31:
+        id = 31
+        name = u'accelerator'
+        code = u'accelerator'
+
+    class FieldData_32:
+        id = 32
+        name = u'hepdata'
+        code = u'hepdata'
+
+    class FieldData_33:
+        id = 33
+        name = u'isbn'
+        code = u'isbn'
+
+    class FieldData_34:
+        id = 34
+        name = u'publication year'
+        code = u'publicationyear'
+
+    class FieldData_35:
+        id = 35
+        name = u'rank'
+        code = u'rank'
+
+    class FieldData_36:
+        id = 36
+        name = u'series'
+        code = u'series'
+
+    class FieldData_37:
+        id = 37
+        name = u'place'
+        code = u'place'
+
+    class FieldData_38:
+        id = 38
+        name = u'exact first author'
+        code = u'exactfirstauthor'
+
+    class FieldData_39:
+        id = 39
+        name = u'author count'
+        code = u'authorcount'
+
+    class FieldData_40:
+        id = 40
+        name = u'reference to'
+        code = u'rawref'
+
+    class FieldData_41:
+        id = 41
+        name = u'conference number'
+        code = u'confnumber'
+
+    class FieldData_42:
+        id = 42
+        name = u'country code'
+        code = u'countrycode'
+
+    class FieldData_43:
+        id = 43
+        name = u'hepdataparent'
+        code = u'hepdataparent'
+
+    class FieldData_44:
+        id = 44
+        name = u'note'
+        code = u'note'
+
+    class FieldData_45:
+        id = 45
+        name = u'coden'
+        code = u'coden'
+
+    class FieldData_46:
+        id = 46
+        name = u'publisher'
+        code = u'publisher'
+
+    class FieldData_47:
+        id = 47
+        name = u'texkey'
+        code = u'texkey'
+
+    class FieldData_48:
+        id = 48
+        name = u'volume'
+        code = u'volume'
+
+    class FieldData_49:
+        id = 49
+        name = u'exact title'
+        code = u'exacttitle'
+
+    class FieldData_50:
+        id = 50
+        name = u'refers to excluding self cites'
+        code = u'referstoexcludingselfcites'
+
+    class FieldData_51:
+        id = 51
+        name = u'cited by excluding self cites'
+        code = u'citedbyexcludingselfcites'
+
+    class FieldData_52:
+        id = 52
+        name = u'cataloguer nickname'
+        code = u'cataloguer'
---
+++ inspire/base/fixtures/field_tag.py
@@ -0,0 +1,769 @@
+# -*- coding: utf-8 -*-
+#
+## This file is part of INSPIRE.
+## Copyright (C) 2014 CERN.
+##
+## INSPIRE is free software; you can redistribute it and/or
+## modify it under the terms of the GNU General Public License as
+## published by the Free Software Foundation; either version 2 of the
+## License, or (at your option) any later version.
+##
+## INSPIRE is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+## General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with INSPIRE; if not, write to the Free Software Foundation, Inc.,
+## 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+#
+
+from fixture import DataSet 
+
+
+class FieldTagData(DataSet):
+
+    class FieldTagData_1:
+        id_field = 1
+        id_tag = 100
+        score = 10
+
+    class FieldTagData_2:
+        id_field = 1
+        id_tag = 102
+        score = 10
+
+    class FieldTagData_3:
+        id_field = 1
+        id_tag = 103
+        score = 10
+
+    class FieldTagData_4:
+        id_field = 1
+        id_tag = 104
+        score = 10
+
+    class FieldTagData_5:
+        id_field = 1
+        id_tag = 105
+        score = 10
+
+    class FieldTagData_6:
+        id_field = 1
+        id_tag = 106
+        score = 10
+
+    class FieldTagData_7:
+        id_field = 1
+        id_tag = 107
+        score = 10
+
+    class FieldTagData_8:
+        id_field = 1
+        id_tag = 108
+        score = 10
+
+    class FieldTagData_9:
+        id_field = 1
+        id_tag = 109
+        score = 10
+
+    class FieldTagData_10:
+        id_field = 1
+        id_tag = 110
+        score = 10
+
+    class FieldTagData_11:
+        id_field = 1
+        id_tag = 111
+        score = 10
+
+    class FieldTagData_12:
+        id_field = 1
+        id_tag = 112
+        score = 10
+
+    class FieldTagData_13:
+        id_field = 1
+        id_tag = 113
+        score = 10
+
+    class FieldTagData_14:
+        id_field = 1
+        id_tag = 114
+        score = 10
+
+    class FieldTagData_15:
+        id_field = 1
+        id_tag = 16
+        score = 10
+
+    class FieldTagData_16:
+        id_field = 1
+        id_tag = 17
+        score = 10
+
+    class FieldTagData_17:
+        id_field = 1
+        id_tag = 18
+        score = 10
+
+    class FieldTagData_18:
+        id_field = 1
+        id_tag = 19
+        score = 10
+
+    class FieldTagData_19:
+        id_field = 1
+        id_tag = 20
+        score = 10
+
+    class FieldTagData_20:
+        id_field = 1
+        id_tag = 21
+        score = 10
+
+    class FieldTagData_21:
+        id_field = 1
+        id_tag = 22
+        score = 10
+
+    class FieldTagData_22:
+        id_field = 1
+        id_tag = 23
+        score = 10
+
+    class FieldTagData_23:
+        id_field = 1
+        id_tag = 24
+        score = 10
+
+    class FieldTagData_24:
+        id_field = 1
+        id_tag = 25
+        score = 10
+
+    class FieldTagData_25:
+        id_field = 1
+        id_tag = 26
+        score = 10
+
+    class FieldTagData_26:
+        id_field = 1
+        id_tag = 27
+        score = 10
+
+    class FieldTagData_27:
+        id_field = 1
+        id_tag = 28
+        score = 10
+
+    class FieldTagData_28:
+        id_field = 1
+        id_tag = 29
+        score = 10
+
+    class FieldTagData_29:
+        id_field = 1
+        id_tag = 30
+        score = 10
+
+    class FieldTagData_30:
+        id_field = 1
+        id_tag = 31
+        score = 10
+
+    class FieldTagData_31:
+        id_field = 1
+        id_tag = 32
+        score = 10
+
+    class FieldTagData_32:
+        id_field = 1
+        id_tag = 33
+        score = 10
+
+    class FieldTagData_33:
+        id_field = 1
+        id_tag = 34
+        score = 10
+
+    class FieldTagData_34:
+        id_field = 1
+        id_tag = 35
+        score = 10
+
+    class FieldTagData_35:
+        id_field = 1
+        id_tag = 36
+        score = 10
+
+    class FieldTagData_36:
+        id_field = 1
+        id_tag = 37
+        score = 10
+
+    class FieldTagData_37:
+        id_field = 1
+        id_tag = 38
+        score = 10
+
+    class FieldTagData_38:
+        id_field = 1
+        id_tag = 39
+        score = 10
+
+    class FieldTagData_39:
+        id_field = 1
+        id_tag = 40
+        score = 10
+
+    class FieldTagData_40:
+        id_field = 1
+        id_tag = 41
+        score = 10
+
+    class FieldTagData_41:
+        id_field = 1
+        id_tag = 42
+        score = 10
+
+    class FieldTagData_42:
+        id_field = 1
+        id_tag = 43
+        score = 10
+
+    class FieldTagData_43:
+        id_field = 1
+        id_tag = 44
+        score = 10
+
+    class FieldTagData_44:
+        id_field = 1
+        id_tag = 45
+        score = 10
+
+    class FieldTagData_45:
+        id_field = 1
+        id_tag = 46
+        score = 10
+
+    class FieldTagData_46:
+        id_field = 1
+        id_tag = 47
+        score = 10
+
+    class FieldTagData_47:
+        id_field = 1
+        id_tag = 48
+        score = 10
+
+    class FieldTagData_48:
+        id_field = 1
+        id_tag = 49
+        score = 10
+
+    class FieldTagData_49:
+        id_field = 1
+        id_tag = 50
+        score = 10
+
+    class FieldTagData_50:
+        id_field = 1
+        id_tag = 51
+        score = 10
+
+    class FieldTagData_51:
+        id_field = 1
+        id_tag = 52
+        score = 10
+
+    class FieldTagData_52:
+        id_field = 1
+        id_tag = 53
+        score = 10
+
+    class FieldTagData_53:
+        id_field = 1
+        id_tag = 54
+        score = 10
+
+    class FieldTagData_54:
+        id_field = 1
+        id_tag = 55
+        score = 10
+
+    class FieldTagData_55:
+        id_field = 1
+        id_tag = 56
+        score = 10
+
+    class FieldTagData_56:
+        id_field = 1
+        id_tag = 57
+        score = 10
+
+    class FieldTagData_57:
+        id_field = 1
+        id_tag = 58
+        score = 10
+
+    class FieldTagData_58:
+        id_field = 1
+        id_tag = 59
+        score = 10
+
+    class FieldTagData_59:
+        id_field = 1
+        id_tag = 60
+        score = 10
+
+    class FieldTagData_60:
+        id_field = 1
+        id_tag = 61
+        score = 10
+
+    class FieldTagData_61:
+        id_field = 1
+        id_tag = 62
+        score = 10
+
+    class FieldTagData_62:
+        id_field = 1
+        id_tag = 63
+        score = 10
+
+    class FieldTagData_63:
+        id_field = 1
+        id_tag = 64
+        score = 10
+
+    class FieldTagData_64:
+        id_field = 1
+        id_tag = 65
+        score = 10
+
+    class FieldTagData_65:
+        id_field = 1
+        id_tag = 66
+        score = 10
+
+    class FieldTagData_66:
+        id_field = 1
+        id_tag = 67
+        score = 10
+
+    class FieldTagData_67:
+        id_field = 1
+        id_tag = 68
+        score = 10
+
+    class FieldTagData_68:
+        id_field = 1
+        id_tag = 69
+        score = 10
+
+    class FieldTagData_69:
+        id_field = 1
+        id_tag = 70
+        score = 10
+
+    class FieldTagData_70:
+        id_field = 1
+        id_tag = 71
+        score = 10
+
+    class FieldTagData_71:
+        id_field = 1
+        id_tag = 72
+        score = 10
+
+    class FieldTagData_72:
+        id_field = 1
+        id_tag = 73
+        score = 10
+
+    class FieldTagData_73:
+        id_field = 1
+        id_tag = 74
+        score = 10
+
+    class FieldTagData_74:
+        id_field = 1
+        id_tag = 75
+        score = 10
+
+    class FieldTagData_75:
+        id_field = 1
+        id_tag = 76
+        score = 10
+
+    class FieldTagData_76:
+        id_field = 1
+        id_tag = 77
+        score = 10
+
+    class FieldTagData_77:
+        id_field = 1
+        id_tag = 78
+        score = 10
+
+    class FieldTagData_78:
+        id_field = 1
+        id_tag = 79
+        score = 10
+
+    class FieldTagData_79:
+        id_field = 1
+        id_tag = 80
+        score = 10
+
+    class FieldTagData_80:
+        id_field = 1
+        id_tag = 81
+        score = 10
+
+    class FieldTagData_81:
+        id_field = 1
+        id_tag = 82
+        score = 10
+
+    class FieldTagData_82:
+        id_field = 1
+        id_tag = 83
+        score = 10
+
+    class FieldTagData_83:
+        id_field = 1
+        id_tag = 84
+        score = 10
+
+    class FieldTagData_84:
+        id_field = 1
+        id_tag = 85
+        score = 10
+
+    class FieldTagData_85:
+        id_field = 1
+        id_tag = 86
+        score = 10
+
+    class FieldTagData_86:
+        id_field = 1
+        id_tag = 87
+        score = 10
+
+    class FieldTagData_87:
+        id_field = 1
+        id_tag = 88
+        score = 10
+
+    class FieldTagData_88:
+        id_field = 1
+        id_tag = 89
+        score = 10
+
+    class FieldTagData_89:
+        id_field = 1
+        id_tag = 90
+        score = 10
+
+    class FieldTagData_90:
+        id_field = 1
+        id_tag = 91
+        score = 10
+
+    class FieldTagData_91:
+        id_field = 1
+        id_tag = 92
+        score = 10
+
+    class FieldTagData_92:
+        id_field = 1
+        id_tag = 93
+        score = 10
+
+    class FieldTagData_93:
+        id_field = 1
+        id_tag = 94
+        score = 10
+
+    class FieldTagData_94:
+        id_field = 1
+        id_tag = 95
+        score = 10
+
+    class FieldTagData_95:
+        id_field = 1
+        id_tag = 96
+        score = 10
+
+    class FieldTagData_96:
+        id_field = 1
+        id_tag = 97
+        score = 10
+
+    class FieldTagData_97:
+        id_field = 1
+        id_tag = 98
+        score = 10
+
+    class FieldTagData_98:
+        id_field = 1
+        id_tag = 99
+        score = 10
+
+    class FieldTagData_99:
+        id_field = 1
+        id_tag = 122
+        score = 10
+
+    class FieldTagData_100:
+        id_field = 1
+        id_tag = 123
+        score = 10
+
+    class FieldTagData_101:
+        id_field = 1
+        id_tag = 124
+        score = 10
+
+    class FieldTagData_102:
+        id_field = 1
+        id_tag = 125
+        score = 10
+
+    class FieldTagData_103:
+        id_field = 1
+        id_tag = 126
+        score = 10
+
+    class FieldTagData_104:
+        id_field = 1
+        id_tag = 127
+        score = 10
+
+    class FieldTagData_105:
+        id_field = 1
+        id_tag = 128
+        score = 10
+
+    class FieldTagData_106:
+        id_field = 1
+        id_tag = 129
+        score = 10
+
+    class FieldTagData_107:
+        id_field = 1
+        id_tag = 130
+        score = 10
+
+    class FieldTagData_108:
+        id_field = 10
+        id_tag = 11
+        score = 100
+
+    class FieldTagData_109:
+        id_field = 11
+        id_tag = 14
+        score = 100
+
+    class FieldTagData_110:
+        id_field = 12
+        id_tag = 15
+        score = 10
+
+    class FieldTagData_111:
+        id_field = 13
+        id_tag = 116
+        score = 10
+
+    class FieldTagData_112:
+        id_field = 2
+        id_tag = 3
+        score = 100
+
+    class FieldTagData_113:
+        id_field = 2
+        id_tag = 4
+        score = 90
+
+    class FieldTagData_114:
+        id_field = 3
+        id_tag = 1
+        score = 100
+
+    class FieldTagData_115:
+        id_field = 3
+        id_tag = 2
+        score = 90
+
+    class FieldTagData_116:
+        id_field = 4
+        id_tag = 5
+        score = 100
+
+    class FieldTagData_117:
+        id_field = 5
+        id_tag = 6
+        score = 100
+
+    class FieldTagData_118:
+        id_field = 6
+        id_tag = 7
+        score = 30
+
+    class FieldTagData_119:
+        id_field = 6
+        id_tag = 8
+        score = 10
+
+    class FieldTagData_120:
+        id_field = 6
+        id_tag = 9
+        score = 20
+
+    class FieldTagData_121:
+        id_field = 7
+        id_tag = 12
+        score = 100
+
+    class FieldTagData_122:
+        id_field = 7
+        id_tag = 13
+        score = 90
+
+    class FieldTagData_123:
+        id_field = 8
+        id_tag = 10
+        score = 100
+
+    class FieldTagData_124:
+        id_field = 9
+        id_tag = 115
+        score = 100
+
+    class FieldTagData_125:
+        id_field = 14
+        id_tag = 117
+        score = 100
+
+    class FieldTagData_126:
+        id_field = 15
+        id_tag = 118
+        score = 100
+
+    class FieldTagData_127:
+        id_field = 16
+        id_tag = 119
+        score = 100
+
+    class FieldTagData_128:
+        id_field = 17
+        id_tag = 120
+        score = 100
+
+    class FieldTagData_129:
+        id_field = 19
+        id_tag = 131
+        score = 100
+
+    class FieldTagData_130:
+        id_field = 20
+        id_tag = 132
+        score = 100
+
+    class FieldTagData_131:
+        id_field = 21
+        id_tag = 133
+        score = 100
+
+    class FieldTagData_132:
+        id_field = 21
+        id_tag = 134
+        score = 90
+
+    class FieldTagData_133:
+        id_field = 22
+        id_tag = 1
+        score = 100
+
+    class FieldTagData_134:
+        id_field = 22
+        id_tag = 2
+        score = 90
+
+    class FieldTagData_135:
+        id_field = 27
+        id_tag = 135
+        score = 100
+
+    class FieldTagData_136:
+        id_field = 28
+        id_tag = 1
+        score = 100
+
+    class FieldTagData_137:
+        id_field = 29
+        id_tag = 1
+        score = 100
+
+    class FieldTagData_138:
+        id_field = 30
+        id_tag = 1
+        score = 100
+
+    class FieldTagData_139:
+        id_field = 30
+        id_tag = 2
+        score = 90
+
+    class FieldTagData_140:
+        id_field = 1
+        id_tag = 2
+        score = 255
+
+    class FieldTagData_141:
+        id_field = 1
+        id_tag = 3
+        score = 255
+
+    class FieldTagData_142:
+        id_field = 1
+        id_tag = 5
+        score = 255
+
+    class FieldTagData_143:
+        id_field = 1
+        id_tag = 6
+        score = 255
+
+    class FieldTagData_144:
+        id_field = 1
+        id_tag = 7
+        score = 255
+
+    class FieldTagData_145:
+        id_field = 1
+        id_tag = 8
+        score = 255
+
+    class FieldTagData_146:
+        id_field = 1
+        id_tag = 11
+        score = 255
+
+    class FieldTagData_147:
+        id_field = 1
+        id_tag = 12
+        score = 255
+
+    class FieldTagData_148:
+        id_field = 1
+        id_tag = 13
+        score = 255
+
+    class FieldTagData_149:
+        id_field = 1
+        id_tag = 14
+        score = 255
