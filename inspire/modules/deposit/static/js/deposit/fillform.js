58b8d9877ac32ef079a6aac17b13f180658fb1bd
Pedro Gaudêncio <pedro.gaudencio@cern.ch>
2014-05-23 11:17:16 +0200

    deposit: changes to literature submission
    * Renames Publication Information -> Journal Information.
    
    * Adds Language to basic info.
    
    * Completes DOI import.
    
    * Creates 3 input fields: DOI, ArXiv and ISBN.
    
    * Adds Collaboration Field.
    
    * Pagination field renamed to Page range.
    
    * Journal Information reordering.
    
    * Adds Experiment after authors.
    
    * Adds Article ID after Page range (same MARC field).
    
    * Adds number of pages.
    
    * Adds conference information box.
    
    * Adds "Proceedings information (not published in journal)" box.
    
    Reviewed-by: Javier Martin Montull <javier.martin.montull>

--- inspire/modules/deposit/static/js/deposit/fillform.js
+++ inspire/modules/deposit/static/js/deposit/fillform.js
@@ -22,24 +22,15 @@
 
 $(document).ready( function() { 
 
-	/**
-	 * Autofills the form fields with given DOI, ArXiv id or ISBN data
-	 */
-	var is_doi, is_arxiv, is_isbn;
-	var arxiv_newregex = 'arXiv:(\d{4})\.(\d{4})(v\d+)?$';
-	var arxiv_oldregex = '[a-z\-]+(\.[A-Z]{2})?/\d{4}\d+$';
-	var doi_regex = '(^$|(doi:)?10\.\d+(.\d+)*/.*)';
-	var isbn_regex = '(?:ISBN[-– ]*(?:|10|13)|International Standard Book Number)[:\s]*(?P<code>[-\-–0-9Xx]{10,25})';
-
-
 	var article_related = $('*[class~="article-related"]');
 	var thesis_related = $('*[class~="thesis-related"]');
 	var chapter_related = $('*[class~="chapter-related"]');
 	var book_related = $('*[class~="book-related"]');
 	var proceedings_related = $('*[class~="proceedings-related"]');
-	var message_box = $('#flash-message');
-	var tpl_flash_import = Hogan.compile('<div class="alert alert-{{state}}"><a class="close" data-dismiss="alert" href="#"">&times;</a>{{{message}}}</div>');
 
+	/**
+	 * Hide form fields individually related to each document type
+	 */
 	function hideFields(){
 		var field_list = [article_related,
 						  thesis_related,
@@ -57,13 +48,31 @@ $(document).ready( function() {
 	selected_type.change(function(event) {
 		hideFields();
 		$('*[class~="'+selected_type.val()+'-related"]').parent().parent().slideDown();
+			$('*[class~="'+selected_type.val()+'-related"]').parent().parent().parent().effect("highlight",
+			{color: "#e1efbb"}, 2500);
+		$("#collapse-2 .panel-body > .alert").remove('.alert');
+		if(selected_type.val() == "proceedings"){
+			var msg = "<strong>Proceedings:</strong> only for complete proceedings. \
+					   For contributions use Article/Conference paper.";
+			selected_type.parent().parent().parent().append(
+				tpl_flash_message.render({state:'info',
+										  message: msg}));
+		}
 	});
 
-	//TODO: make function generic to fetch anything...add smaller functions for each source
+	/**
+	 * Autofill form fields with given DOI, ArXiv id or ISBN data
+	 *
+	 * Uses REST API to query Crossref and the database to fetch data for
+	 * the inputed ids.
+	 */
 	$("#importData").click(function() {
-			var btn = $(this);
-			btn.button('loading');
-			var doi = $("#field-identifier input").val();
+	//TODO: make function generic to fetch anything...add smaller functions for each source
+		var btn = $(this);
+		btn.button('loading');
+		if($("#doi").val() != ''){
+			// if DOI field is not empty
+			var doi = $("#doi").val();
 			var url = "/deposit/search_doi?doi=" + doi;
 			var import_state, import_message;
 			$.get(url, function( data ) {
@@ -72,100 +81,116 @@ $(document).ready( function() {
 
 				if(data.query.status == "success"){
 					import_state = 'success';
-					var source = data.source.charAt(0).toUpperCase() + data.source.slice(1);
-					import_message = 'The data was successfully imported from ' + source + '.';
-					var title, authors, contributors;
-					if(selected_type.val() == 'thesis'){
-						title = data.query.volume_title;
-					}
-					else if(data.query.publication_type == 'full_text'){
-						title = data.query.article_title;
-						if(data.query.journal_title)
-							$("#journal_title").val(data.query.journal_title);
-						if(data.query.isbn)
-							$("#isbn").val(data.query.isbn);
-						if(data.query.first_page && data.query.last_page)
-							$("#pagination").val(data.query.first_page + "-" + data.query.last_page);
-						if(data.query.volume)
-							$("#volume").val(data.query.volume);
-						if(data.query.year)
-							$("#year").val(data.query.year);
-						if(data.query.issue)
-							$("#issue").val(data.query.issue);
-					}
-					$("#doi").val(doi);
-					$("#title").val(title);
-					authors = document.getElementById("field-authors");
-					contributors = data.query.contributors;
-						if(contributors && contributors.length>0 &&
-						   contributors[0].contributor[0] &&
-						   contributors[0].contributor[1].surname &&
-						   contributors[0].contributor[0].given_name){
-							authors.innerHTML = 
-	'<div class="authors dynamic-field-list ui-sortable" id="authors"> \
-		<input id="authors-__last_index__" name="authors-__last_index__" type="hidden" value="0">';
-							for(i = 0; i<contributors.length; i++){
-								var field = "#authors-" + i + "-name";
-								authors.innerHTML +=
-	'<div class="field-list-element"> \
-			<div class="row"> \
-					<div id="authors-'+i+'"> \
-							<div class="col-xs-6"> \
-									<input class="form-control" id="authors-'+i+'-name" name="authors-'+i+'-name" type="text" value="'+contributors[i].contributor[1].surname +", "+contributors[i].contributor[0].given_name +'"> \
-							</div> \
-							<div class="col-xs-4 col-pad-0"> \
-									<input class="form-control" id="authors-'+i+'-affiliation" name="authors-'+i+'-affiliation" placeholder="Affiliation" type="text" value=""> \
-							</div> \
+					if(data.source == 'crossref'){
+						import_message = 'The data was successfully imported from Crossref.';
+						var title, authors, contributors;
+						if(selected_type.val() == 'thesis'){
+							title = data.query.volume_title;
+						}
+						else if(data.query.publication_type == 'full_text'){
+							title = data.query.article_title;
+							if(data.query.journal_title)
+								$("#journal_title").val(data.query.journal_title);
+							if(data.query.isbn)
+								$("#isbn").val(data.query.isbn);
+							if(data.query.first_page && data.query.last_page)
+								$("#page_range").val(data.query.first_page + "-" + data.query.last_page);
+							if(data.query.volume)
+								$("#volume").val(data.query.volume);
+							if(data.query.year)
+								$("#year").val(data.query.year);
+							if(data.query.issue)
+								$("#issue").val(data.query.issue);
+						}
+						$("#title").val(title);
+						authors = document.getElementById("field-authors");
+						contributors = data.query.contributors;
+							if(contributors && contributors.length>0 &&
+							   contributors[0].contributor[0] &&
+							   contributors[0].contributor[1].surname &&
+							   contributors[0].contributor[0].given_name){
+								authors.innerHTML = 
+'<div class="authors dynamic-field-list ui-sortable" id="authors"> \
+<input id="authors-__last_index__" name="authors-__last_index__" type="hidden" value="0">';
+								for(i = 0; i<contributors.length; i++){
+									var field = "#authors-" + i + "-name";
+									authors.innerHTML +=
+'<div class="field-list-element"> \
+	<div class="row"> \
+			<div id="authors-'+i+'"> \
+					<div class="col-xs-6"> \
+							<input class="form-control" id="authors-'+i+'-name" name="authors-'+i+'-name" type="text" value="'+contributors[i].contributor[1].surname +", "+contributors[i].contributor[0].given_name +'"> \
 					</div> \
-					<div class="col-xs-2"> \
-							<a class="sort-element text-muted sortlink iconlink" rel="tooltip" title="Drag to reorder"> \
-									<i class="fa fa-sort fa-fw"></i> \
-							</a> \
-							<a class="remove-element text-muted iconlink" rel="tooltip" title="Click to remove"> \
-									<i class="fa fa-times fa-fw"></i> \
-							</a> \
+					<div class="col-xs-4 col-pad-0"> \
+							<input class="form-control" id="authors-'+i+'-affiliation" name="authors-'+i+'-affiliation" placeholder="Affiliation" type="text" value=""> \
 					</div> \
 			</div> \
-	</div>';
+			<div class="col-xs-2"> \
+					<a class="sort-element text-muted sortlink iconlink" rel="tooltip" title="Drag to reorder"> \
+							<i class="fa fa-sort fa-fw"></i> \
+					</a> \
+					<a class="remove-element text-muted iconlink" rel="tooltip" title="Click to remove"> \
+							<i class="fa fa-times fa-fw"></i> \
+					</a> \
+			</div> \
+	</div> \
+</div>';
+								}
+								authors.innerHTML +=
+'<div class="empty-element"> \
+<div class="row"> \
+    <div id="authors-__index__"> \
+        <div class="col-xs-6"> \
+            <input class="form-control" id="authors-__index__-name" name="authors-__index__-name" placeholder="Family name, First name" type="text" value=""> \
+        </div> \
+        <div class="col-xs-4 col-pad-0"> \
+            <input class="form-control" id="authors-__index__-affiliation" name="authors-__index__-affiliation" placeholder="Affiliation" type="text" value=""> \
+        </div> \
+    </div> \
+    <div class="col-xs-2"> \
+        <a class="sort-element text-muted sortlink iconlink" rel="tooltip" title="Drag to reorder"></a><a class="remove-element text-muted iconlink" rel="tooltip" title="Click to remove"></a> \
+    </div> \
+</div> \
+</div> \
+<div class="row"><div class="col-xs-12"> \
+<span class="pull-right"> \
+    <a class="add-element"> \
+        <i class="fa fa-plus"></i> Add another author \
+    </a> \
+</span> \
+</div> \
+<p class="text-muted field-desc"><small>Required.</small></p> \
+\
+<div class="alert help-block" id="state-authors" style="margin-top: 5px; display: none;"></div>';
 							}
-							authors.innerHTML +=
-	'<div class="empty-element"> \
-	    <div class="row"> \
-	        <div id="authors-__index__"> \
-	            <div class="col-xs-6"> \
-	                <input class="form-control" id="authors-__index__-name" name="authors-__index__-name" placeholder="Family name, First name" type="text" value=""> \
-	            </div> \
-	            <div class="col-xs-4 col-pad-0"> \
-	                <input class="form-control" id="authors-__index__-affiliation" name="authors-__index__-affiliation" placeholder="Affiliation" type="text" value=""> \
-	            </div> \
-	        </div> \
-	        <div class="col-xs-2"> \
-	            <a class="sort-element text-muted sortlink iconlink" rel="tooltip" title="Drag to reorder"></a><a class="remove-element text-muted iconlink" rel="tooltip" title="Click to remove"></a> \
-	        </div> \
-	    </div> \
-	</div> \
-	<div class="row"><div class="col-xs-12"> \
-	    <span class="pull-right"> \
-	        <a class="add-element"> \
-	            <i class="fa fa-plus"></i> Add another author \
-	        </a> \
-	    </span> \
-	</div> \
-	<p class="text-muted field-desc"><small>Required.</small></p> \
-	\
-	<div class="alert help-block" id="state-authors" style="margin-top: 5px; display: none;"></div>';
-						}
-				}
-				else {
+					} else if(data.source == 'database'){
+						// if DOI and the source is the database
+						import_state = 'info';
+						import_message = 'This DOI already exists in Inspire database.';
+					}
+				} else {
+					// if DOI not found or malformed
 					import_state = 'warning';
 					if(data.query.status == 'notfound')
-						import_message = 'The identifier ' + $("#field-identifier input").val() + ' was not found.';
+						import_message = 'The DOI ' + $("#doi").val() + ' was not found.';
 					else if(data.query.status == 'malformed')
-						import_message = 'The identifier ' + $("#field-identifier input").val() + ' is malformed.';
+						import_message = 'The DOI ' + $("#doi").val() + ' is malformed.';
 				}
-			flash_import({state:import_state, message: import_message});
-			btn.button('reset');
-		});
+				flash_import({state:import_state, message: import_message});
+			});
+		}
+		else if($("#arxiv_id").val() != ''){
+		// if DOI field is empty and ArXiv has something
+			import_state = 'info';
+			import_message = 'The ArXiv ID importing is not available at the moment.';
+		}
+		else if($("#isbn").val() != ''){
+			// if DOI and ArXiv fields are empty and ISBN has something
+			import_state = 'info';
+			import_message = 'The ISBN importing is not available at the moment.';
+		}
+		flash_import({state:import_state, message: import_message});
+		btn.button('reset');
 	});
 
 	/**
