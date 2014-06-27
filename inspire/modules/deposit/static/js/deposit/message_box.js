f5990ae98a93581636624e5ccd83978e897c8fce
Kamil Neczaj <kamil.neczaj@cern.ch>
2014-06-27 11:12:33 +0200

    deposit: MessageBox jQuery plugin
    * Converts a DOM object to a container showing
      messages with given Hogan template.
    
    Signed-off-by: Kamil Neczaj <kamil.neczaj@cern.ch>

---
+++ inspire/modules/deposit/static/js/deposit/message_box.js
@@ -0,0 +1,113 @@
+/*
+ * This file is part of INSPIRE.
+ * Copyright (C) 2014 CERN.
+ *
+ * INSPIRE is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * INSPIRE is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with INSPIRE. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * In applying this licence, CERN does not waive the privileges and immunities
+ * granted to it by virtue of its status as an Intergovernmental Organization
+ * or submit itself to any jurisdiction.
+ */
+
+(function($){
+
+    var MessageBox = (function() {
+
+      /**
+       * Constructor.
+       *
+       * @param element html element on which the plugin
+       * @param options dictionary
+       * @constructor
+       */
+      function MessageBox(element, options) {
+
+        // ensure that there is jQuery selector available 
+        this.$element = $(element);
+        this.options = $.extend({}, $.fn.messageBox.defaults, options);
+
+        this.template = this.options.hoganTemplate;
+
+      }
+
+      MessageBox.prototype = {
+
+        /**
+         * Appends one message. Use append which is more universal.
+         * @param ctx
+         * @private
+         */
+        _appendOne: function(ctx) {
+          this.$element.append(this.template.render(ctx));
+	        this.$element.show('fast');
+        },
+
+        /**
+         * Cleans the message box.
+         */
+        clean: function() {
+          this.$element.empty();
+        },
+
+        /**
+         * Appends one or more messages to the array box.
+         * @param messages can be a single message context or an array of them.
+         */
+        append: function(messages) {
+          if (!Array.isArray(messages)) {
+            this._appendOne(messages);
+          }
+          var that = this;
+          $.each(messages, function() {
+            that._appendOne(this);
+          });
+        }
+      };
+
+      return MessageBox;
+
+    })();
+
+    $.fn.messageBox = function(option) {
+
+      var $elements = this;
+      var objects = [];
+      var dataLabel = 'message-box';
+
+      $elements.each(function() {
+        var $element = $(this);
+        var object = $element.data(dataLabel);
+        var options = typeof option == 'object' && option;
+        // attach jQuery plugin
+        if (!object) {
+          object = new MessageBox($element, options);
+          $element.data(dataLabel, object);
+        }
+        objects.push(object);
+      });
+
+      // return all objects attached to the elements
+      return objects;
+    };
+
+    $.fn.messageBox.defaults = {
+      /**
+       * @param the template used to render a message. Should have parameters
+       *  'state' and 'message'.
+       */
+      hoganTemplate: {},
+    };
+    $.fn.messageBox.Constructor = MessageBox;
+
+})(jQuery);
