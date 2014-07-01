6ab72999393864db0f2490d2e5292981e2580502
Guillaume Lastecoueres <PX9e@gmx.fr>
2014-07-01 14:00:50 +0200

    workflows: definition updates
    * Changes workflow definitions to use new WorkflowBase superclass
      to have an interface for calling rendering functions for the
      Holding Pen.
    
    Tested-by: Jan Aage Lavik <jan.age.lavik@cern.ch>
    Signed-off-by: Guillaume Lastecoueres <guillaume.lastecoueres@cern.ch>

--- inspire/modules/workflows/workflows/process_record_arxiv.py
+++ inspire/modules/workflows/workflows/process_record_arxiv.py
@@ -44,7 +44,78 @@ from ..tasks.filtering import inspire_filter_custom
 from invenio.config import CFG_PREFIX
 
 
-class process_record_arxiv(object):
+class process_record_arxiv(WorkflowBase):
+    @staticmethod
+    def get_title(bwo):
+        """Get the title."""
+        extracted_title = []
+        record = bwo.get_data()
+        if hasattr(record, "get") and "title" in record:
+            if isinstance(record["title"], str):
+                extracted_title = [record["title"]]
+            else:
+                for a_title in record["title"]:
+                    extracted_title.append(record["title"][a_title])
+        else:
+            extracted_title = [" No title"]
+        title_final = ""
+        for i in extracted_title:
+            title_final += "{0} ".format(i)
+        return title_final
+
+    @staticmethod
+    def get_description(bwo):
+        """Get the description column part."""
+        record = bwo.get_data()
+        from invenio.modules.records.api import Record
+
+        try:
+            identifiers = Record(record.dumps()).persistent_identifiers
+            final_identifiers = []
+            for i in identifiers:
+                final_identifiers.append(i['value'])
+        except Exception as e:
+            if hasattr(record, "get"):
+                final_identifiers = [record.get("system_number_external", {}).get("value", 'No ids')]
+            else:
+                final_identifiers = [' No ids']
+
+        categories = [" No categories"]
+        if hasattr(record, "get"):
+            if 'subject' in record:
+                lookup = ["subject", "term"]
+            elif "subject_term":
+                lookup = ["subject_term", "term"]
+            else:
+                lookup = None
+            categories = []
+            if lookup:
+                primary, secondary = lookup
+                category_list = record.get(primary, [])
+                if isinstance(category_list, dict):
+                    category_list = [category_list]
+                for subject in category_list:
+                    category = subject[secondary]
+                    if len(subject) == 2:
+                        if subject.keys()[1] == secondary:
+                            source_list = subject[subject.keys()[0]]
+                        else:
+                            source_list = subject[subject.keys()[1]]
+                    else:
+                        try:
+                            source_list = subject['source']
+                        except KeyError:
+                            source_list = ""
+                    categories.append(category + "(" + source_list + ")")
+
+        from flask import render_template
+        return render_template('workflows/styles/harvesting_record.html',
+                               categories=categories,
+                               identifiers=final_identifiers)
+    @staticmethod
+    def formatter(bwo, **kwargs):
+        return None
+
     object_type = "record"
     workflow = [
         convert_record_with_repository("oaiarXiv2inspire_nofilter.xsl"), convert_record_to_bibfield,
