fb588747267fb9651459d1b06bdd8f0950f6e27d
Georgios Kokosioulis <giokokos@gmail.com>
2014-07-04 11:08:32 +0200

    grunt: support for JS beautifier
    NOTE: run $npm install and $grunt jsbeautifier
    
    Signed-off-by: Georgios Kokosioulis <georgios.kokosioulis@cern.ch>
    Reviewed-by: Javier Martin Montull <javier.martin.montull@cern.ch>

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,32 +1,32 @@
 /*
-* This file is part of INSPIRE.
-* Copyright (C) 2014 CERN.
-*
-* INSPIRE is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* INSPIRE is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with INSPIRE. If not, see <http://www.gnu.org/licenses/>.
-*
-* In applying this licence, CERN does not waive the privileges and immunities
-* granted to it by virtue of its status as an Intergovernmental Organization
-* or submit itself to any jurisdiction.
-*/
+ * This file is part of INSPIRE.
+ * Copyright (C) 2014 CERN.
+ *
+ * INSPIRE is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * INSPIRE is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with INSPIRE. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * In applying this licence, CERN does not waive the privileges and immunities
+ * granted to it by virtue of its status as an Intergovernmental Organization
+ * or submit itself to any jurisdiction.
+ */
 
 module.exports = function(grunt) {
 
   var prefix = process.env.VIRTUAL_ENV || '../..',
-        globalConfig = {
-          bower_path: 'bower_components',
-          installation_path: prefix + '/var/invenio.base-instance/static'
-        };
+    globalConfig = {
+      bower_path: 'bower_components',
+      installation_path: prefix + '/var/invenio.base-instance/static'
+    };
 
   // show elapsed time at the end
   require('time-grunt')(grunt);
@@ -41,21 +41,22 @@ module.exports = function(grunt) {
 
     copy: {
       js: {
-          expand: true,
-          flatten: true,
-          cwd: '<%= globalConfig.bower_path %>/',
-          src: ['buckets/buckets.js',
-                  'jquery-feeds/dist/jquery.feeds.min.js',
-                  'moment/min/moment.min.js',
-                  'bootstrap-multiselect/js/bootstrap-multiselect.js'],
-          dest: '<%= globalConfig.installation_path %>/js/'
+        expand: true,
+        flatten: true,
+        cwd: '<%= globalConfig.bower_path %>/',
+        src: ['buckets/buckets.js',
+          'jquery-feeds/dist/jquery.feeds.min.js',
+          'moment/min/moment.min.js',
+          'bootstrap-multiselect/js/bootstrap-multiselect.js'
+        ],
+        dest: '<%= globalConfig.installation_path %>/js/'
       },
       css: {
-          expand: true,
-          flatten: true,
-          cwd: '<%= globalConfig.bower_path %>/',
-          src: ['bootstrap-multiselect/css/bootstrap-multiselect.css'],
-          dest: '<%= globalConfig.installation_path %>/css/'
+        expand: true,
+        flatten: true,
+        cwd: '<%= globalConfig.bower_path %>/',
+        src: ['bootstrap-multiselect/css/bootstrap-multiselect.css'],
+        dest: '<%= globalConfig.installation_path %>/css/'
       }
     },
     jshint: {
@@ -69,6 +70,30 @@ module.exports = function(grunt) {
         }
       },
       all: ['inspire/modules/**/static/js/**/*.js']
+    },
+    jsbeautifier: {
+      files: ["inspire/modules/**/static/js/**/*.js"],
+      options: {
+        js: {
+          braceStyle: "collapse",
+          breakChainedMethods: false,
+          e4x: false,
+          evalCode: false,
+          indentChar: " ",
+          indentLevel: 0,
+          indentSize: 2,
+          indentWithTabs: false,
+          jslintHappy: false,
+          keepArrayIndentation: false,
+          keepFunctionIndentation: false,
+          maxPreserveNewlines: 10,
+          preserveNewlines: true,
+          spaceBeforeConditional: true,
+          spaceInParen: false,
+          unescapeStrings: false,
+          wrapLineLength: 0
+        }
+      }
     }
   });
 
