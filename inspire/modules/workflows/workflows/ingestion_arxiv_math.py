0c04c0a148abe187eb68c685d26ff3bf24d482a6
Jan Aage Lavik <jan.age.lavik@cern.ch>
2014-07-08 12:21:55 +0200

    workflows: definition fix
    * Fixes some missing imports in the workflow definitions.
    
    * Implements rendering functions for the workflows.
    
    Signed-off-by: Jan Aage Lavik <jan.age.lavik@cern.ch>

--- inspire/modules/workflows/workflows/ingestion_arxiv_math.py
+++ inspire/modules/workflows/workflows/ingestion_arxiv_math.py
@@ -51,6 +51,7 @@ from invenio.modules.workflows.tasks.logic_tasks import (
 )
 
 from invenio.legacy.bibsched.bibtask import task_update_progress, write_message
+from invenio.modules.workflows.utils import WorkflowBase
 
 
 class ingestion_arxiv_math(WorkflowBase):
@@ -73,8 +74,7 @@ class ingestion_arxiv_math(WorkflowBase):
             result_progress = {
                 'success': (result_temp['total'] - result_temp['failed']),
                 'failed': result_temp['failed'],
-                'success_per': (result_temp['total'] - result_temp['failed'])*100 /
-                          result_temp['total'],
+                'success_per': (result_temp['total'] - result_temp['failed'])*100 / result_temp['total'],
                 'failed_per': result_temp['failed']*100 / result_temp['total'],
                 'total': result_temp['total']}
         else:
@@ -86,6 +86,7 @@ class ingestion_arxiv_math(WorkflowBase):
                                identifiers=identifiers,
                                result_progress=result_progress,
                                current_task=current_task)
+
     @staticmethod
     def get_title(bwo):
         return "Supervising harvesting of {0}".format(
