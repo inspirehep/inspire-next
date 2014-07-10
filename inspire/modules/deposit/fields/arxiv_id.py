987eda358ae6152a67bfd04af17989b2b40b7691
Pedro Gaudêncio <pedro.gaudencio@cern.ch>
2014-07-10 14:41:12 +0200

    deposit: addition of ArXiv ID form field
    * Replaces arXiv id TextField by ArXivField and adds input validation.
    
    Signed-off-by: Pedro Gaudêncio <pedro.gaudencio@cern.ch>
    Reviewed-by: Javier Martin Montull <javier.martin.montull@cern.ch>

---
+++ inspire/modules/deposit/fields/arxiv_id.py
@@ -0,0 +1,51 @@
+#
+## This file is part of INSPIRE.
+## Copyright (C) 2014 CERN.
+##
+## INSPIRE is free software; you can redistribute it and/or
+## modify it under the terms of the GNU General Public License as
+## published by the Free Software Foundation; either version 2 of the
+## License, or (at your option) any later version.
+##
+## INSPIRE is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+## General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with INSPIRE; if not, write to the Free Software Foundation, Inc.,
+## 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+#
+
+from wtforms import TextField
+from wtforms.validators import ValidationError
+
+from invenio.modules.deposit.field_base import WebDepositField
+from invenio.utils.persistentid import is_arxiv
+from invenio.modules.deposit.filter_utils import strip_prefixes, strip_string
+
+__all__ = ['ArXivField']
+
+
+def arxiv_syntax_validation(form, field):
+    message = "The provided ArXiv ID is invalid - it should look \
+                similar to 'hep-th/1234567' or '1234.5678'."
+
+    if field.data and not is_arxiv(field.data):
+        raise ValidationError(message)
+
+
+class ArXivField(WebDepositField, TextField):
+    def __init__(self, **kwargs):
+        defaults = dict(
+            icon='barcode',
+            validators=[arxiv_syntax_validation],
+            filters=[
+                strip_string,
+                strip_prefixes("arxiv:", "arXiv:"),
+            ],
+            placeholder="e.g. hep-th/1234567 or 1234.5678...",
+            widget_classes="form-control"
+        )
+        defaults.update(kwargs)
+        super(ArXivField, self).__init__(**defaults)
--- inspire/modules/deposit/forms.py
+++ inspire/modules/deposit/forms.py
@@ -32,6 +32,7 @@ from invenio.modules.deposit.field_widgets import plupload_widget, \
     ExtendedListWidget, \
     ItemWidget
 from invenio.modules.deposit.autocomplete_utils import kb_dynamic_autocomplete
+from .fields import ArXivField
 
 #
 # Field class names
@@ -147,10 +148,8 @@ class LiteratureForm(WebDepositForm):
         export_key='doi'
     )
 
-    arxiv_id = fields.TextField(
-        placeholder=_("1406.0000"),
-        label=_('arXiv ID'),
-        widget_classes='form-control',
+    arxiv_id = ArXivField(
+        label=_('ArXiv ID'),
     )
 
     isbn = fields.TextField(
