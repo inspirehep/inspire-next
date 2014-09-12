36a03581fdd23336321e163b35c2d1d905966ead
Kamil Neczaj <kamil.neczaj@cern.ch>
2014-09-12 11:02:05 +0200

    deposit: conferences typeahead requirejs impr.
    * Adds requirejs dependencies.
    
    * Removes a closure unneeded after requirejs adaptation.
    
    * Breaks too long lines.
    
    Signed-off-by: Kamil Neczaj <kamil.neczaj@cern.ch>

--- inspire/modules/deposit/static/js/deposit/conferences_typeahead.js
+++ inspire/modules/deposit/static/js/deposit/conferences_typeahead.js
@@ -21,220 +21,230 @@
  */
 
 define(function(require, exports, module) {
-  (function($) {
+
+  var $ = require('jquery');
+  // $.fn.typeahead()
+  var Bloodhound = require('typeahead');
+
+  /**
+   * Constructor.
+   *
+   * @param element html element on which the plugin
+   * @param options dictionary
+   * @constructor
+   */
+  function ConferencesTypeahead(element, options) {
+
+    this.$element = $(element);
+    this.options = $.extend({}, $.fn.conferencesTypeahead.defaults, options);
+
+    this.value = this.$element.val();
+    this.cachedQuery = this.$element.val();
+
+    this.suggestionTemplate = this.options.suggestionTemplate;
+    this.selectedValueTemplate = this.options.selectedValueTemplate;
+
+    var that = this;
+
+    var engine = new Bloodhound({
+      name: 'conferences',
+      remote: '/search?cc=Conferences&p=/^%QUERY/&of=recjson',
+      datumTokenizer: function(datum) {
+        return that.datumTokenizer.call(that, datum);
+      },
+      queryTokenizer: Bloodhound.tokenizers.whitespace
+    });
+
+    engine.initialize();
+
+    this.$element.typeahead({
+      minLength: 1
+    }, {
+      source: engine.ttAdapter(),
+      // the key of a value which is rather passed to typeahead than displayed
+      // the display values are selected by templates.
+      displayKey: 'conference',
+      templates: {
+        empty: function(data) {
+          return that.options.cannotFindMessage;
+        },
+        suggestion: function(data) {
+          return that.suggestionTemplate.render.call(
+            that.suggestionTemplate, data.conference
+          );
+        }
+      }
+    });
+
+    this.ttTypeahead = this.$element.data("ttTypeahead");
+
+    // saves overwritten functions
+    this.orgTypeahead = {
+      setInputValue: that.ttTypeahead.input.setInputValue,
+    };
+
+    this.ttTypeahead.input.setInputValue = function(value, silent) {
+      return that.setInputFieldValue.apply(that, [value, silent]);
+    };
+  }
+
+  ConferencesTypeahead.prototype = {
 
     /**
-     * Constructor.
-     *
-     * @param element html element on which the plugin
-     * @param options dictionary
-     * @constructor
+     * Connects events to functions.
      */
-    function ConferencesTypeahead(element, options) {
-
-      this.$element = $(element);
-      this.options = $.extend({}, $.fn.conferencesTypeahead.defaults, options);
-
-      this.value = this.$element.val();
-      this.cachedQuery = this.$element.val();
-
-      this.suggestionTemplate = this.options.suggestionTemplate;
-      this.selectedValueTemplate = this.options.selectedValueTemplate;
-
+    connectEvents: function connectEvents() {
       var that = this;
 
-      var engine = new Bloodhound({
-        name: 'conferences',
-        remote: '/search?cc=Conferences&p=/^%QUERY/&of=recjson',
-        datumTokenizer: function(datum) {
-          return that.datumTokenizer.call(that, datum);
-        },
-        queryTokenizer: Bloodhound.tokenizers.whitespace
+      this.ttTypeahead.input.$input.on("keydown", function(event) {
+        return that.onKeydown(event);
       });
-
-      engine.initialize();
-
-      this.$element.typeahead({
-        minLength: 1
-      }, {
-        source: engine.ttAdapter(),
-        // the key of a value which is rather passed to typeahead than displayed
-        // the display values are selected by templates.
-        displayKey: 'conference',
-        templates: {
-          empty: function(data) {
-            return that.options.cannotFindMessage;
-          },
-          suggestion: function(data) {
-            return that.suggestionTemplate.render.call(that.suggestionTemplate, data.conference);
-          }
-        }
+      this.ttTypeahead.input.onSync("queryChanged", function(event, query) {
+        that.cachedQuery = query;
       });
+    },
 
-      this.ttTypeahead = this.$element.data("ttTypeahead");
+    /**
+     * A function which overwrites typeahead's setInputFieldValue(), shows
+     * a description of a autocompleted value.
+     *
+     * @param value value to set - from typeahead
+     */
+    setInputFieldValue: function setInputFieldValue(value, silent) {
+      if (Array.isArray(value)) {
+        this.value = value[0];
+      } else {
+        this.value = value;
+      }
+      var renderedValue = this.getUserFriendlyValue();
+      this.orgTypeahead.setInputValue.call(
+        this.ttTypeahead.input, renderedValue, silent);
+    },
 
-      // saves overwritten functions
-      this.orgTypeahead = {
-        setInputValue: that.ttTypeahead.input.setInputValue,
-      };
+    /**
+     * Removes non alpha-numeric characters from beginning and end of a string.
+     *
+     * @param str {String} input string
+     * @returns {String} trimmed string
+     * @private
+     */
+    _trimNonAlphaNumericChar: function _trimNonAlphaNumericChar(str) {
+      return str.replace(/^[^A-Z0-9]+|[^A-Z0-9]+$/ig, '');
+    },
 
-      this.ttTypeahead.input.setInputValue = function(value, silent) {
-        return that.setInputFieldValue.apply(that, [value, silent]);
-      };
-    }
+    /**
+     * Tokenizer used by the bloodhound engine
+     *
+     * @param datum {Object} an item returned by query
+     * @returns {Array} array of tokens; the result query is a one consisting
+     *   of queries for every token connected with OR operator
+     */
+    datumTokenizer: function datumTokenizer(datum) {
+      if ((typeof datum) === 'string' || !datum.conference) {
+        return [datum];
+      }
+      var tokens = [];
+      datum = datum.conference;
+      if (datum.date && (typeof datum.date) === 'string') {
+        tokens = tokens.concat(datum.date.split(/\.?\s+-?\s*/));
+      }
+      if (datum.title && (typeof datum.title) === 'string') {
+        var titleNameTokens = datum.title.split(/\s+/);
+        titleNameTokens = $.map(titleNameTokens,
+          this._trimNonAlphaNumericChar
+        );
+        tokens = tokens.concat(titleNameTokens);
+      }
+      if (datum.place && (typeof datum.place) === 'string') {
+        tokens = tokens.concat(datum.place.split(/,?\s+/));
+      }
+      if (datum.conference_id && (typeof datum.conference_id) === 'string') {
+        tokens.push(datum.conference_id);
+      }
+      return tokens;
+    },
 
-    ConferencesTypeahead.prototype = {
+    /**
+     * Returns value of the field which identifies the chosen suggestion,
+     * not the description shown to a user.
+     *
+     * @returns {String}
+     */
+    getRawValue: function getRawValue() {
+      if (this.isFieldValueAutocompleted() && this.value.conference_id) {
+        return this.value.conference_id;
+      }
+      return this.value;
+    },
 
-      /**
-       * Connects events to functions.
-       */
-      connectEvents: function connectEvents() {
-        var that = this;
+    /**
+     * Description of an autocompleted value.
+     *
+     * @returns {String}
+     */
+    getUserFriendlyValue: function getUserFriendlyValue() {
+      if (this.isFieldValueAutocompleted()) {
+        return this.selectedValueTemplate.render(this.value);
+      }
+      return this.value;
+    },
 
-        this.ttTypeahead.input.$input.on("keydown", function(event) {
-          return that.onKeydown(event);
-        });
-        this.ttTypeahead.input.onSync("queryChanged", function(event, query) {
-          that.cachedQuery = query;
-        });
-      },
+    /**
+     * Checks if the field is value is the autocompletition result or just
+     * a string typed by a user
+     *
+     * @returns {bool}
+     */
+    isFieldValueAutocompleted: function isFieldValueAutocompleted() {
+      return (typeof this.value) === 'object';
+    },
 
-      /**
-       * A function which overwrites typeahead's setInputFieldValue(), shows a description
-       * of a autocompleted value.
-       *
-       * @param value value to set - from typeahead
-       */
-      setInputFieldValue: function setInputFieldValue(value, silent) {
-        if (Array.isArray(value)) {
-          this.value = value[0];
-        } else {
-          this.value = value;
-        }
-        var renderedValue = this.getUserFriendlyValue();
-        this.orgTypeahead.setInputValue.call(
-          this.ttTypeahead.input, renderedValue, silent);
-      },
+    /**
+     * Handles keydown event.
+     *
+     * @param event {Event} keydown event
+     */
+    onKeydown: function onKeydown(event) {
+      var keyCode = event.which || event.keyCode;
+      // backspace or delete
+      if ((keyCode === 8 || keyCode === 46) &&
+        this.isFieldValueAutocompleted()) {
+        // if the field stores autocompletition result reset it to query
+        this.resetToCachedQuery();
+        // show the dropdown as if query value was changed
+        this.ttTypeahead._onQueryChanged('queryChanged',
+          this.ttTypeahead.input.query);
+        return false;
+      }
+    },
 
-      /**
-       * Removes non alpha-numeric characters from beginning and end of a string.
-       *
-       * @param str {String} input string
-       * @returns {String} trimmed string
-       * @private
-       */
-      _trimNonAlphaNumericChar: function _trimNonAlphaNumericChar(str) {
-        return str.replace(/^[^A-Z0-9]+|[^A-Z0-9]+$/ig, '');
-      },
+    /**
+     * Resets input field value to what was lastly typed in.
+     */
+    resetToCachedQuery: function resetToCachedQuery() {
+      this.ttTypeahead.input.query = this.cachedQuery;
+      this.ttTypeahead.input.resetInputValue();
+    },
+  };
 
-      /**
-       * Tokenizer used by the bloodhound engine
-       *
-       * @param datum {Object} an item returned by query
-       * @returns {Array} array of tokens; the result query is a one consisting of queries
-       *   for every token connected with OR operator
-       */
-      datumTokenizer: function datumTokenizer(datum) {
-        if ((typeof datum) === 'string' || !datum.conference) {
-          return [datum];
-        }
-        var tokens = [];
-        datum = datum.conference;
-        if (datum.date && (typeof datum.date) === 'string') {
-          tokens = tokens.concat(datum.date.split(/\.?\s+-?\s*/));
-        }
-        if (datum.title && (typeof datum.title) === 'string') {
-          var titleNameTokens = datum.title.split(/\s+/);
-          titleNameTokens = $.map(titleNameTokens, this._trimNonAlphaNumericChar);
-          tokens = tokens.concat(titleNameTokens);
-        }
-        if (datum.place && (typeof datum.place) === 'string') {
-          tokens = tokens.concat(datum.place.split(/,?\s+/));
-        }
-        if (datum.conference_id && (typeof datum.conference_id) === 'string') {
-          tokens.push(datum.conference_id);
-        }
-        return tokens;
-      },
+  $.fn.conferencesTypeahead = jQueryPlugin(ConferencesTypeahead,
+    'conferences-typeahead');
 
-      /**
-       * Returns value of the field which identifies the chosen suggestion,
-       * not the description shown to a user.
-       *
-       * @returns {String}
-       */
-      getRawValue: function getRawValue() {
-        if (this.isFieldValueAutocompleted() && this.value.conference_id) {
-          return this.value.conference_id;
-        }
-        return this.value;
-      },
-
-      /**
-       * Description of an autocompleted value.
-       *
-       * @returns {String}
-       */
-      getUserFriendlyValue: function getUserFriendlyValue() {
-        if (this.isFieldValueAutocompleted()) {
-          return this.selectedValueTemplate.render(this.value);
-        }
-        return this.value;
-      },
-
-      /**
-       * Checks if the field is value is the autocompletition result or just
-       * a string typed by a user
-       *
-       * @returns {bool}
-       */
-      isFieldValueAutocompleted: function isFieldValueAutocompleted() {
-        return (typeof this.value) === 'object';
-      },
-
-      /**
-       * Handles keydown event.
-       *
-       * @param event {Event} keydown event
-       */
-      onKeydown: function onKeydown(event) {
-        var keyCode = event.which || event.keyCode;
-        // backspace or delete
-        if ((keyCode === 8 || keyCode === 46) && this.isFieldValueAutocompleted()) {
-          // if the field stores autocompletition result reset it to query
-          this.resetToCachedQuery();
-          // show the dropdown as if query value was changed
-          this.ttTypeahead._onQueryChanged('queryChanged', this.ttTypeahead.input.query);
-          return false;
-        }
-      },
-
-      /**
-       * Resets input field value to what was lastly typed in.
-       */
-      resetToCachedQuery: function resetToCachedQuery() {
-        this.ttTypeahead.input.query = this.cachedQuery;
-        this.ttTypeahead.input.resetInputValue();
-      },
-    };
-
-    $.fn.conferencesTypeahead = jQueryPlugin(ConferencesTypeahead, 'conferences-typeahead');
-
-    $.fn.conferencesTypeahead.defaults = {
-      /**
-       * @param {Hogan template} a template used to render a suggestion
-       */
-      suggestionTemplate: {},
-      /**
-       * @param {Hogan template} a template used to render a the descriptive field value
-       */
-      selectedValueTemplate: {},
-      /**
-       * @param {String} a message shown when no suggestion is available
-       */
-      cannotFindMessage: undefined,
-    };
+  $.fn.conferencesTypeahead.defaults = {
+    /**
+     * @param {Hogan template} a template used to render a suggestion
+     */
+    suggestionTemplate: {},
+    /**
+     * @param {Hogan template} a template used to render a the descriptive
+     *   field value
+     */
+    selectedValueTemplate: {},
+    /**
+     * @param {String} a message shown when no suggestion is available
+     */
+    cannotFindMessage: undefined,
+  };
 
   module.exports = ConferencesTypeahead;
-  })(jQuery);
 });
