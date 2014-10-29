5bfcf851dac460986b0f46191f7b973c85b3f535
Kamil Neczaj <kamil.neczaj@cern.ch>
2014-10-29 10:15:11 +0000

    base: jquery-plugin creator adapted to requirejs
    Signed-off-by: Kamil Neczaj <kamil.neczaj@cern.ch>

--- inspire/modules/deposit/static/js/deposit/message_box.js
+++ inspire/modules/deposit/static/js/deposit/message_box.js
@@ -22,7 +22,9 @@
 
 
 define(function(require, exports, module) {
-  var $ = require("jquery")
+  var $ = require("jquery");
+  var jQueryPlugin = require('js/jquery_plugin');
+  
   var MessageBox = (function() {
 
     /**
