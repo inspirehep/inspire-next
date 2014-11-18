249c747e68dd0c0a0743e17ebc6aaf0275502b20
Georgios Papoutsakis <georgios.papoutsakis@cern.ch>
2014-11-18 14:22:51 +0100

    workflows: process arxiv math workflow update
    * Adds HP_IDENTIFIERS Knowledge Base. Requires inveniomanage database recreate.
    
    * Matches incoming records with objects in Holding Pen.
    
    * If an object is matched updates old object and deletes the new one.
    
    * Adds possible new identifiers to the Knowledge Base.
    
    Signed-off-by: Georgios Papoutsakis <georgios.papoutsakis@cern.ch>

--- inspire/modules/workflows/tasks/matching.py
+++ inspire/modules/workflows/tasks/matching.py
@@ -25,6 +25,8 @@
 import requests
 import traceback
 
+from functools import wraps
+
 from invenio.base.globals import cfg
 from invenio.modules.deposit.models import Deposition
 
@@ -43,22 +45,17 @@ def match_record_arxiv_remote(arxiv_id):
 
 def match_record_arxiv_remote_oaiharvest(obj, eng):
     """Look on the remote server if the record exists using arXiv id."""
-    report_numbers = obj.get_data().get('report_number', {})
-    if isinstance(report_numbers, dict):
-        arxiv_id = report_numbers.get('primary')
-    else:
-        arxiv_id = None
-        for number in report_numbers:
-            if number.get("source", "").lower() == "arxiv":
-                arxiv_id = number.get("primary")
-                break
-    try:
-        return match_record_arxiv_remote(arxiv_id)
-    except requests.ConnectionError:
-        obj.log.error("Error connecting to remote server:\n {0}".format(
-            traceback.format_exc()
-        ))
-        return False
+    report_numbers = obj.get_data().get('report_number', [])
+    for number in report_numbers:
+        if number.get("source", "").lower() == "arxiv":
+            arxiv_id = number.get("primary")
+            try:
+                return match_record_arxiv_remote(arxiv_id)
+            except requests.ConnectionError:
+                obj.log.error("Error connecting to remote server:\n {0}".format(
+                    traceback.format_exc()
+                ))
+                return False
 
 
 def match_record_arxiv_remote_deposit(obj, eng):
@@ -104,3 +101,75 @@ def match_record_remote_oaiharvest(obj, eng):
     """Look on the remote server if the record exists using doi and arxiv_id."""
     return match_record_arxiv_remote_deposit(obj, eng) or\
         match_record_doi_remote_deposit(obj, eng)
+
+
+def save_identifiers_oaiharvest(kb_name):
+    """Save the record identifiers into a KB."""
+    @wraps(save_identifiers_oaiharvest)
+    def _save_identifiers_oaiharvest(obj, eng):
+        from inspire.utils.knowledge import save_keys_to_kb
+
+        identifiers = []
+        report_numbers = obj.get_data().get('report_number', [])
+        for number in report_numbers:
+            if number.get("source", "").lower() == "arxiv":
+                arxiv_id = number.get("primary")
+                identifiers.append(arxiv_id)
+        save_keys_to_kb(kb_name, identifiers, obj.id)
+
+    return _save_identifiers_oaiharvest
+
+
+def match_record_HP_oaiharvest(kb_name):
+    """Check if a oaiharvest record exists in HP."""
+    @wraps(match_record_HP_oaiharvest)
+    def _match_record_HP_oaiharvest(obj, eng):
+        from inspire.utils.knowledge import check_keys
+
+        identifiers = []
+        report_numbers = obj.get_data().get('report_number', [])
+        for number in report_numbers:
+            if number.get("source", "").lower() == "arxiv":
+                arxiv_id = number.get("primary")
+                identifiers.append(arxiv_id)
+        return check_keys(kb_name, identifiers)
+
+    return _match_record_HP_oaiharvest
+
+
+def delete_self_and_stop_processing(obj, eng):
+    """Delete both versions of itself and stops the workflow."""
+    from invenio.modules.workflows.models import BibWorkflowObject
+    # delete snapshot created with original data
+    initial_obj = BibWorkflowObject.query.filter(
+        BibWorkflowObject.id_parent == obj.id
+    ).one()
+    BibWorkflowObject.delete(initial_obj.id)
+    # delete self
+    BibWorkflowObject.delete(obj.id)
+    eng.skipToken()
+
+
+def update_old_object(kb_name):
+    """Update the data of the old object with the new data."""
+    @wraps(update_old_object)
+    def _update_old_object(obj, eng):
+        from inspire.utils.knowledge import get_value
+        from invenio.modules.workflows.models import BibWorkflowObject
+
+        identifiers = []
+        report_numbers = obj.get_data().get('report_number', [])
+        for number in report_numbers:
+            if number.get("source", "").lower() == "arxiv":
+                arxiv_id = number.get("primary")
+                identifiers.append(arxiv_id)
+
+        object_id = get_value(kb_name, identifiers)
+        if object_id:
+            old_object = BibWorkflowObject.query.get(object_id)
+            if old_object:
+                # record waiting approval
+                old_object.set_data(obj.get_data())
+                old_object.save()
+
+    return _update_old_object
