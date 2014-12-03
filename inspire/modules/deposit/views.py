f2f705841cbdf1bf6e3c40218bf7cdaeb7d36ff6
Pedro Gaudencio <pedro.gaudencio@cern.ch>
2014-12-03 11:11:11 +0200

    deposit: statistics for submitted depositions
    Signed-off-by: Pedro Gaudencio <pedro.gaudencio@cern.ch>

--- inspire/modules/deposit/views.py
+++ inspire/modules/deposit/views.py
@@ -17,7 +17,23 @@
 ## 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 #
 
-from flask import Blueprint
+from datetime import datetime
+
+from flask import Blueprint, \
+    render_template, \
+    request, \
+    jsonify
+from flask.ext.login import login_required
+from flask.ext.wtf import Form
+from wtforms.fields import DateTimeField, SubmitField
+
+from invenio.base.i18n import _
+from invenio.modules.deposit.signals import template_context_created
+from invenio.modules.deposit.models import Deposition
+from invenio.modules.deposit.views.deposit import blueprint as deposit_blueprint
+
+from utils import process_metadata_for_charts
+
 
 blueprint = Blueprint(
     'inspire_deposit',
@@ -25,3 +41,45 @@ blueprint = Blueprint(
     template_folder='templates',
     static_folder="static",
 )
+
+
+CHART_TYPES = {'column': 'Column',
+               'pie': 'Pie'}
+
+
+class FilterDateForm(Form):
+
+    """Date form."""
+
+    since_date = DateTimeField(
+        label=_('From'),
+        description='Format: YYYY-MM-DD.')
+    until_date = DateTimeField(
+        label=_('To'),
+        description='Format: YYYY-MM-DD.')
+    submit = SubmitField(_("Filter depositions"))
+
+
+@deposit_blueprint.route('/stats',
+                         methods=['GET', 'POST'])
+@login_required
+def show_stats():
+    """Render the stats for all the depositions."""
+    form = FilterDateForm()
+
+    submitted_depositions = [d for d in Deposition.get_depositions() if d.has_sip(sealed=True)]
+
+    ctx = process_metadata_for_charts(submitted_depositions)
+    ctx.update(dict(
+        depositions=submitted_depositions,
+        form=form,
+        chart_types=CHART_TYPES
+    ))
+
+    # Send signal to allow modifications to the template context
+    template_context_created.send(
+        '%s.%s' % (deposit_blueprint.name, show_stats.__name__),
+        context=ctx
+    )
+
+    return render_template('deposit/stats/all_depositions.html', **ctx)
