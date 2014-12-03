8237ef37db0b1a237f13bfcb33815fb70b1983d9
Javier Martin Montull <javier.martin.montull@cern.ch>
2014-12-03 11:54:29 +0100

    deposit: increase autocomplete limit
    Signed-off-by: Javier Martin Montull <javier.martin.montull@cern.ch>

--- inspire/modules/deposit/static/js/deposit/affiliations_typeahead.js
+++ inspire/modules/deposit/static/js/deposit/affiliations_typeahead.js
@@ -30,7 +30,7 @@ define([
     this.dataEngine = new Bloodhound({
       name: 'affiliations',
       remote: {
-        url: '/search?cc=Institutions&p=%QUERY*&of=recjson&f=affautocomplete',
+        url: '/search?cc=Institutions&p=%QUERY*&of=recjson&f=affautocomplete&rg=50',
         filter: function(response) {
           return $.map(response, function(item, idx) {
             return item.institution;
