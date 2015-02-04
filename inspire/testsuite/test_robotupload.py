fa05e03fc8902467935d327fa258eb6d6d49c971
Jan Aage Lavik <jan.age.lavik@cern.ch>
2015-02-04 09:16:18 +0100

    robotupload: update tests
    * Fixes the broken robotupload tests since the mode was introduced.
    
    * Adds some cosmetic updates.
    
    Signed-off-by: Jan Aage Lavik <jan.age.lavik@cern.ch>

--- inspire/testsuite/test_robotupload.py
+++ inspire/testsuite/test_robotupload.py
@@ -1,21 +1,23 @@
 # -*- coding: utf-8 -*-
 #
-## This file is part of INSPIRE.
-## Copyright (C) 2014 CERN.
-##
-## INSPIRE is free software; you can redistribute it and/or
-## modify it under the terms of the GNU General Public License as
-## published by the Free Software Foundation; either version 2 of the
-## License, or (at your option) any later version.
-##
-## INSPIRE is distributed in the hope that it will be useful, but
-## WITHOUT ANY WARRANTY; without even the implied warranty of
-## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-## General Public License for more details.
-##
-## You should have received a copy of the GNU General Public License
-## along with INSPIRE; if not, write to the Free Software Foundation, Inc.,
-## 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+# This file is part of INSPIRE.
+# Copyright (C) 2014, 2015 CERN.
+#
+# INSPIRE is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of the
+# License, or (at your option) any later version.
+#
+# INSPIRE is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with INSPIRE; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+
+"""Tests for robotupload."""
 
 from __future__ import print_function, absolute_import
 from invenio.testsuite import make_test_suite, run_test_suite, InvenioTestCase
@@ -25,8 +27,11 @@ import httpretty
 
 class RobotUploadTests(InvenioTestCase):
 
+    """Test the robotupload functions."""
+
     @httpretty.activate
     def test_robotupload_bad_xml(self):
+        """Test proper handling when bad MARCXML is sent."""
         from inspire.utils.robotupload import make_robotupload_marcxml
         httpretty.register_uri(
             httpretty.POST,
@@ -38,12 +43,14 @@ class RobotUploadTests(InvenioTestCase):
         response = make_robotupload_marcxml(
             "http://localhost:4000",
             invalid_marcxml,
+            mode="insert",
         )
         self.assertEqual(response.status_code, 400)
         self.assertTrue("not valid" in response.text)
 
     @httpretty.activate
     def test_robotupload_success(self):
+        """Test proper handling when good MARCXML is sent."""
         from inspire.utils.robotupload import make_robotupload_marcxml
         httpretty.register_uri(
             httpretty.POST,
@@ -55,12 +62,33 @@ class RobotUploadTests(InvenioTestCase):
         response = make_robotupload_marcxml(
             "http://localhost:4000",
             valid_marcxml,
+            mode="insert",
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue("[INFO] bibupload batchupload" in response.text)
+
+    @httpretty.activate
+    def test_robotupload_success_append(self):
+        """Test proper handling when good MARCXML is sent."""
+        from inspire.utils.robotupload import make_robotupload_marcxml
+        httpretty.register_uri(
+            httpretty.POST,
+            "http://localhost:4000/batchuploader/robotupload/append",
+            body="[INFO] bibupload batchupload --append /dummy/file/path\n",
+            status=200
+        )
+        valid_marcxml = "<record></record>"
+        response = make_robotupload_marcxml(
+            "http://localhost:4000",
+            valid_marcxml,
+            mode="append",
         )
         self.assertEqual(response.status_code, 200)
         self.assertTrue("[INFO] bibupload batchupload" in response.text)
 
     @httpretty.activate
     def test_robotupload_callback_url(self):
+        """Test passing of a callback URL."""
         from inspire.utils.robotupload import make_robotupload_marcxml
         body = (
             "[INFO] bibupload batchupload --insert /some/path"
@@ -76,6 +104,7 @@ class RobotUploadTests(InvenioTestCase):
         response = make_robotupload_marcxml(
             "http://localhost:4000",
             valid_marcxml,
+            mode="insert",
             callback_url="http://localhost",
         )
         self.assertEqual(response.status_code, 200)
