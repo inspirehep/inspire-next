ba392dd7d0256fad9cd474096445eb4220e4da44
Jan Aage Lavik <jan.age.lavik@cern.ch>
2015-02-18 10:57:04 +0100

    robotupload: use new clean_xml before shipping
    Signed-off-by: Jan Aage Lavik <jan.age.lavik@cern.ch>

--- inspire/utils/robotupload.py
+++ inspire/utils/robotupload.py
@@ -1,24 +1,26 @@
 # -*- coding: utf-8 -*-
 #
-## This file is part of INSPIRE.
-## Copyright (C) 2014 CERN.
-##
-## INSPIRE is free software: you can redistribute it and/or modify
-## it under the terms of the GNU General Public License as published by
-## the Free Software Foundation, either version 3 of the License, or
-## (at your option) any later version.
-##
-## INSPIRE is distributed in the hope that it will be useful,
-## but WITHOUT ANY WARRANTY; without even the implied warranty of
-## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-## GNU General Public License for more details.
-##
-## You should have received a copy of the GNU General Public License
-## along with INSPIRE. If not, see <http://www.gnu.org/licenses/>.
-##
-## In applying this license, CERN does not waive the privileges and immunities
-## granted to it by virtue of its status as an Intergovernmental Organization
-## or submit itself to any jurisdiction.
+# This file is part of INSPIRE.
+# Copyright (C) 2014, 2015 CERN.
+#
+# INSPIRE is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# INSPIRE is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with INSPIRE. If not, see <http://www.gnu.org/licenses/>.
+#
+# In applying this license, CERN does not waive the privileges and immunities
+# granted to it by virtue of its status as an Intergovernmental Organization
+# or submit itself to any jurisdiction.
+
+"""Function for sending robotuploads to other Invenio instances."""
 
 import os
 import requests
@@ -27,6 +29,8 @@ import requests
 def make_robotupload_marcxml(url, marcxml, mode, **kwargs):
     """Make a robotupload request."""
     from invenio.utils.url import make_user_agent_string
+    from inspire.utils.text import clean_xml
+
     from invenio.base.globals import cfg
     headers = {
         "User-agent": make_user_agent_string("inspire"),
@@ -40,7 +44,7 @@ def make_robotupload_marcxml(url, marcxml, mode, **kwargs):
     url = os.path.join(base_url, "batchuploader/robotupload", mode)
     return requests.post(
         url=url,
-        data=marcxml,
+        data=str(clean_xml(marcxml)),
         headers=headers,
         params=kwargs,
     )
