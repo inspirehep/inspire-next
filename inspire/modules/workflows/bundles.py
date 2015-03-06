7a6a7760e4233aa6285f39940ef3e44c7b2fc8a6
Pedro Gaudencio <pedro.gaudencio@cern.ch>
2015-03-06 16:22:29 +0100

    deposit: bootstrap-switch lib introdution
    Signed-off-by: Pedro Gaudencio <pedro.gaudencio@cern.ch>

---
+++ inspire/modules/workflows/bundles.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+#
+## This file is part of INSPIRE.
+## Copyright (C) 2015 CERN.
+##
+## INSPIRE is free software; you can redistribute it and/or
+## modify it under the terms of the GNU General Public License as
+## published by the Free Software Foundation; either version 2 of the
+## License, or (at your option) any later version.
+##
+## INSPIRE is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+## General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with INSPIRE; if not, write to the Free Software Foundation, Inc.,
+## 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+#
+
+from invenio.modules.workflows.bundles import (
+    js as _workflows_js,
+    css as _workflows_styles
+)
+
+# '_' prefix indicates private variables, and prevents duplicated import by
+# auto-discovery service of invenio
+
+_workflows_js.bower.update({
+    'bootstrap-switch': 'git://github.com/nostalgiaz/bootstrap-switch.git',
+})
+
+_workflows_styles.contents += (
+    'vendors/bootstrap-switch/dist/css/bootstrap3/bootstrap-switch.css',
+)
