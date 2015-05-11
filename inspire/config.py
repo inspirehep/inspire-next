08dd59848e670e23d3f7b39a585d15b3a296b71a
Javier Martin Montull <javier.martin.montull@cern.ch>
2015-05-11 11:49:58 +0200

    ranker: add missing configuration

--- inspire/config.py
+++ inspire/config.py
@@ -81,6 +81,7 @@ PACKAGES = [
     'inspire.modules.formatter',
     'inspire.modules.harvester',
     'inspire.modules.oaiharvester',
+    'inspire.modules.ranker',
     'inspire.modules.styleguide',
     'inspire.modules.workflows',
     'invenio.modules.access',
