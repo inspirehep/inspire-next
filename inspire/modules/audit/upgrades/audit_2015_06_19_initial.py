1353eaa0dcc07ce4aa80c7eadfe0508e1e2637ad
Ilias Koutsakis <ilias.koutsakis@cern.ch>
2015-06-19 16:30:59 +0200

    workflows: audit logging for guessing
    * Sends a signal that logs relevant information about the
      prediction after user action.
    
    * Adds an upgrade recipe.
    
    * Amends some PEP8 stuff and other minor things.
    
    Amended-by: Jan Aage Lavik <jan.age.lavik@cern.ch>
    Signed-off-by: Ilias Koutsakis <ilias.koutsakis@cern.ch>

---
+++ inspire/modules/audit/upgrades/audit_2015_06_19_initial.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of Invenio.
+# Copyright (C) 2015 CERN.
+#
+# Invenio is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of the
+# License, or (at your option) any later version.
+#
+# Invenio is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Invenio; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+
+from invenio.ext.sqlalchemy import db
+from invenio.modules.upgrader.api import op
+
+from inspire.modules.audit import Audit
+
+depends_on = []
+
+
+def info():
+    """Info message."""
+    return "Creates audit table."
+
+
+def do_upgrade():
+    """Implement your upgrades here."""
+    if not op.has_table("audit_logging"):
+        Audit.__table__.create(db.engine)
+
+
+def estimate():
+    """Estimate running time of upgrade in seconds (optional)."""
+    return 1
+
+
+def pre_upgrade():
+    """Run pre-upgrade checks (optional)."""
+    # Example of raising errors:
+    # raise RuntimeError("Description of error 1", "Description of error 2")
+
+
+def post_upgrade():
+    """Run post-upgrade checks (optional)."""
+    # Example of issuing warnings:
+    # warnings.warn("A continuable error occurred")
