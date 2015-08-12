7364909e2a3e93036064855dcf4835c092adcfd5
Jan Aage Lavik <jan.age.lavik@cern.ch>
2015-08-12 15:58:13 +0200

    workflows: new Payload data model
    * Adds new Payload data structure for use in harvesting workflows.
      This data structure is based upon the Deposition data model.
    
    * Updates existing harvesting workflow to use this new data structure.
    
    * Adds testing skeleton for this new data structure.
    
    Co-authored-by: Jacopo Notarstefano <jacopo.notarstefano@cern.ch>
    Signed-off-by: Jan Aage Lavik <jan.age.lavik@cern.ch>

---
+++ inspire/testsuite/test_workflows.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of INSPIRE.
+# Copyright (C) 2014, 2015 CERN.
+#
+# INSPIRE is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of the
+# License, or (at your option) any later version.
+#
+# INSPIRE is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with INSPIRE; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+
+"""Tests for workflows."""
+
+from __future__ import print_function, absolute_import
+
+from flask_registry import ImportPathRegistry
+
+from invenio.celery import celery
+from invenio.testsuite import InvenioTestCase, make_test_suite, run_test_suite
+
+from invenio.modules.workflows.registry import WorkflowsRegistry
+
+TEST_PACKAGES = [
+    'inspire.modules.*',
+    'inspire.testsuite',
+]
+
+
+class WorkflowTasksTestCase(InvenioTestCase):
+
+    """ Workflow class for testing."""
+
+    def create_registries(self):
+        """Create registries for testing."""
+        self.app.extensions['registry']['workflows.tests'] = \
+            ImportPathRegistry(initial=TEST_PACKAGES)
+        self.app.extensions['registry']['workflows'] = \
+            WorkflowsRegistry(
+                'workflows', app=self.app, registry_namespace='workflows.tests'
+            )
+        self.app.extensions['registry']['workflows.actions'] = \
+            WorkflowsRegistry(
+                'actions', app=self.app, registry_namespace='workflows.tests'
+            )
+
+    def cleanup_registries(self):
+        """Clean registries for testing."""
+        del self.app.extensions['registry']['workflows.tests']
+        del self.app.extensions['registry']['workflows']
+        del self.app.extensions['registry']['workflows.actions']
+
+
+class WorkflowTest(WorkflowTasksTestCase):
+
+    """Class to test the delayed workflows."""
+
+    def setUp(self):
+        """ Setup tests."""
+        self.create_registries()
+        celery.conf['CELERY_ALWAYS_EAGER'] = True
+
+    def tearDown(self):
+        """ Clean up created objects."""
+        from invenio.modules.workflows.models import Workflow
+        self.delete_objects(
+            Workflow.get(Workflow.module_name == "unit_tests").all())
+        self.cleanup_registries()
+
+    def test_foo(self):
+        b = "foo"
+        import ipdb; ipdb.set_trace()
+        print(b)
+
+
+TEST_SUITE = make_test_suite(WorkflowTest)
+
+if __name__ == "__main__":
+    run_test_suite(TEST_SUITE)
