6833d2d208c642e17f00df15d11d8de7ea064a94
Jan Aage Lavik <jan.age.lavik@cern.ch>
2015-09-29 16:39:28 +0200

    citations: adds missing upgrade recipe
    Signed-off-by: Jan Aage Lavik <jan.age.lavik@cern.ch>

---
+++ inspire/modules/citations/upgrades/citations_2015_09_29_initial.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of Invenio.
+# Copyright (C) 2015 CERN.
+#
+# Invenio is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of the
+# License, or (at your option) any later version.
+#
+# Invenio is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Invenio; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+
+"""Creates tables related to citations."""
+
+from inspire.modules.citations.models import Citation, Citation_Log
+
+from invenio.ext.sqlalchemy import db
+from invenio_upgrader.api import op
+
+
+depends_on = []
+
+
+def info():
+    """Info message."""
+    return __doc__
+
+
+def do_upgrade():
+    """Implement your upgrades here."""
+    if not op.has_table("rnkCITATIONLOG"):
+        Citation_Log.__table__.create(db.engine)
+    if not op.has_table("rnkCITATIONDICT"):
+        Citation.__table__.create(db.engine)
+
+
+def estimate():
+    """Estimate running time of upgrade in seconds (optional)."""
+    return 1
+
+
+def pre_upgrade():
+    """Run pre-upgrade checks (optional)."""
+    # Example of raising errors:
+    # raise RuntimeError("Description of error 1", "Description of error 2")
+
+
+def post_upgrade():
+    """Run post-upgrade checks (optional)."""
+    # Example of issuing warnings:
+    # warnings.warn("A continuable error occurred")
