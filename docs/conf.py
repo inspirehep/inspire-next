ae79a66e2a184b3070fddc1b796fd464f21cd339
Jan Aage Lavik <jan.age.lavik@cern.ch>
2015-11-04 13:09:56 +0100

    docs: add backports to mocked list
    Signed-off-by: Jan Aage Lavik <jan.age.lavik@cern.ch>

--- docs/conf.py
+++ docs/conf.py
@@ -37,7 +37,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
         return Mock()
 
-MOCK_MODULES = ['lzma', 'scikit-learn', 'scipy', 'numpy']
+MOCK_MODULES = ['lzma', 'scikit-learn', 'scipy', 'numpy', 'backports']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # If extensions (or modules to document with autodoc) are in another directory,
