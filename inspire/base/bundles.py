4c413323b734e58a3d747475729c50a6468be2e3
Samuele Kaplun <samuele.kaplun@cern.ch>
2015-11-06 21:41:43 +0100

    testsuite: test fixes
    * Amends tests to reflect API changes in 835286c94.
    
    * Improves kwalitee.
    
    Signed-off-by: Samuele Kaplun <samuele.kaplun@cern.ch>

--- inspire/base/bundles.py
+++ inspire/base/bundles.py
@@ -3,34 +3,31 @@
 # This file is part of INSPIRE.
 # Copyright (C) 2014, 2015 CERN.
 #
-# INSPIRE is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# INSPIRE is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of the
+# License, or (at your option) any later version.
 #
-# INSPIRE is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# INSPIRE is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with INSPIRE. If not, see <http://www.gnu.org/licenses/>.
-#
-# In applying this licence, CERN does not waive the privileges and immunities
-# granted to it by virtue of its status as an Intergovernmental Organization
-# or submit itself to any jurisdiction.
-
+# along with INSPIRE; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 
 """Inspire bundles."""
 
-from invenio_ext.assets import Bundle, RequireJSFilter
-from invenio_base.bundles import jquery as _j, invenio as _i
-from invenio_search.bundles import js as _search_js
-from invenio_formatter.bundles import css as _formatter_css
-
 # '_' prefix indicates private variables, and prevents duplicated import by
 # auto-discovery service of invenio
-from invenio_base.bundles import styles as _base_styles
+from invenio_base.bundles import invenio as _i, jquery as _j, styles as _base_styles
+
+from invenio_ext.assets import Bundle, RequireJSFilter
+
+from invenio_formatter.bundles import css as _formatter_css
+
+from invenio_search.bundles import js as _search_js
 
 js = Bundle(
     'js/inspire_base_init.js',
@@ -42,7 +39,6 @@ js = Bundle(
         "jQuery-menu-aim": "latest"
     }
 )
-
 
 
 dependencies_to_remove = ["MathJax"]
--- inspire/testsuite/test_cvlatex_html_text.py
+++ inspire/testsuite/test_cvlatex_html_text.py
@@ -17,14 +17,19 @@
 # along with INSPIRE; if not, write to the Free Software Foundation, Inc.,
 # 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 
-import pkg_resources
 import os
 
 from dojson.contrib.marc21.utils import create_record
 
-from invenio.testsuite import InvenioTestCase, make_test_suite, run_test_suite
 from inspire.dojson.hep import hep
+
 from invenio.base.wrappers import lazy_import
+
+from invenio.testsuite import InvenioTestCase, make_test_suite, run_test_suite
+
+import pkg_resources
+
+
 Cv_latex_html_text = lazy_import('inspire.utils.cv_latex_html_text.Cv_latex_html_text')
 
 
@@ -61,51 +66,51 @@ class CvLatexHtmlTextTests(InvenioTestCase):
     def test_title(self):
         """Test if title is created correctly"""
         self.assertEqual(self.sample_cv_latex_html['title'],
-                         Cv_latex_html_text(self.hep_record, 'cv_latex_html').
+                         Cv_latex_html_text(self.hep_record, 'cv_latex_html', "<br/>").
                          _get_title())
 
         self.assertEqual(self.sample_cv_latex_text['title'],
-                         Cv_latex_html_text(self.hep_record, 'cv_latex_text').
+                         Cv_latex_html_text(self.hep_record, 'cv_latex_text', "\n").
                          _get_title())
 
     def test_author(self):
         """Test if author is created correctly"""
         self.assertEqual(self.sample_cv_latex_html['author'],
-                         Cv_latex_html_text(self.hep_record, 'cv_latex_html').
+                         Cv_latex_html_text(self.hep_record, 'cv_latex_html', "<br/>").
                          _get_author()[0])
 
         self.assertEqual(self.sample_cv_latex_text['author'],
-                         Cv_latex_html_text(self.hep_record, 'cv_latex_text').
+                         Cv_latex_html_text(self.hep_record, 'cv_latex_text', "\n").
                          _get_author()[0])
 
     def test_arxiv(self):
         """Test if arxiv is created correctly"""
         self.assertEqual(self.sample_cv_latex_html['arxiv'],
-                         Cv_latex_html_text(self.hep_record, 'cv_latex_html').
+                         Cv_latex_html_text(self.hep_record, 'cv_latex_html', "<br/>").
                          _get_arxiv())
 
         self.assertEqual(self.sample_cv_latex_text['arxiv'],
-                         Cv_latex_html_text(self.hep_record, 'cv_latex_text').
+                         Cv_latex_html_text(self.hep_record, 'cv_latex_text', "\n").
                          _get_arxiv())
 
     def test_doi(self):
         """Test if doi is created correctly"""
         self.assertEqual(self.sample_cv_latex_html['doi'],
-                         Cv_latex_html_text(self.hep_record, 'cv_latex_html').
+                         Cv_latex_html_text(self.hep_record, 'cv_latex_html', "<br/>").
                          _get_doi())
 
         self.assertEqual(self.sample_cv_latex_text['doi'],
-                         Cv_latex_html_text(self.hep_record, 'cv_latex_text').
+                         Cv_latex_html_text(self.hep_record, 'cv_latex_text', "\n").
                          _get_doi())
 
     def test_publi_info(self):
         """Test if publication info is created correctly"""
         self.assertEqual(self.sample_cv_latex_html['publi_info'],
-                         Cv_latex_html_text(self.hep_record, 'cv_latex_html').
+                         Cv_latex_html_text(self.hep_record, 'cv_latex_html', "<br/>").
                          _get_publi_info())
 
         self.assertEqual(self.sample_cv_latex_text['publi_info'],
-                         Cv_latex_html_text(self.hep_record, 'cv_latex_text').
+                         Cv_latex_html_text(self.hep_record, 'cv_latex_text', "\n").
                          _get_publi_info())
 TEST_SUITE = make_test_suite(CvLatexHtmlTextTests)
 
