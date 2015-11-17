2e92987e674a948024dcc9bb3816ed523972ef16
Javier Martin Montull <javier.martin.montull@cern.ch>
2015-11-17 17:50:31 +0100

    records: enhance records with inspire subjects
    * Adds INSPIRE subjects before record index. This subjects are
      used for faceting.
    
    Signed-off-by: Javier Martin Montull <javier.martin.montull@cern.ch>

--- inspire/modules/records/__init__.py
+++ inspire/modules/records/__init__.py
@@ -19,3 +19,5 @@
 # In applying this license, CERN does not waive the privileges and immunities
 # granted to it by virtue of its status as an Intergovernmental Organization
 # or submit itself to any jurisdiction.
+
+from .receivers import populate_inspire_subjects
