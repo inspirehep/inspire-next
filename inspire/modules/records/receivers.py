20cc952b33e13317df22d572ff813726f31dd7ba
Javier Martin Montull <javier.martin.montull@cern.ch>
2015-11-17 17:50:31 +0100

    records: enhance records with inspire subjects
    * Adds INSPIRE subjects before record index. This subjects are
      used for faceting.
    
    Signed-off-by: Javier Martin Montull <javier.martin.montull@cern.ch>

---
+++ inspire/modules/records/receivers.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of INSPIRE.
+# Copyright (C) 2015 CERN.
+#
+# INSPIRE is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# INSPIRE is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with INSPIRE. If not, see <http://www.gnu.org/licenses/>.
+#
+# In applying this license, CERN does not waive the privileges and immunities
+# granted to it by virtue of its status as an Intergovernmental Organization
+# or submit itself to any jurisdiction.
+
+from invenio_records.signals import before_record_index
+
+
+@before_record_index.connect
+def populate_inspire_subjects(recid, json):
+    """
+    Populate a json record before indexing it to elastic.
+
+    Adds a field for faceting INSPIRE subjects
+    """
+    inspire_subjects = [
+        s['term'] for s in json.get('subject_terms', [])
+        if s['scheme'] == 'INSPIRE'
+    ]
+    json['facet_inspire_subjects'] = inspire_subjects
