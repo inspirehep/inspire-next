28d9b1b9cf6e95c9dcfc26dc8f8d4546299d6364
Panos Paparrigopoulos <panos.paparrigopoulos@cern.ch>
2015-11-19 10:22:20 +0100

    authors: authors names tokenizer
    * Ports BibIndexAuthorTokenizer from old invenio.
    
    * Enhances json record before indexing it to elasticsearch
      with all the possible variations of the authors name.
      (closes #529)
    
    Signed-off-by: Panos Paparrigopoulos <panos.paparrigopoulos@cern.ch>

--- inspire/modules/authors/testsuite/test_utils.py
+++ inspire/modules/authors/testsuite/test_utils.py
@@ -1,30 +1,27 @@
 # -*- coding: utf-8 -*-
 #
 # This file is part of INSPIRE.
-# Copyright (C) 2014, 2015 CERN.
+# Copyright (C) 2015 CERN.
 #
-# INSPIRE is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# INSPIRE is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of the
+# License, or (at your option) any later version.
 #
-# INSPIRE is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# INSPIRE is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with INSPIRE. If not, see <http://www.gnu.org/licenses/>.
-#
-# In applying this licence, CERN does not waive the privileges and immunities
-# granted to it by virtue of its status as an Intergovernmental Organization
-# or submit itself to any jurisdiction.
+# along with INSPIRE; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 
 """Tests for utils."""
 
-from ..utils import bai
+from invenio.testsuite import InvenioTestCase, make_test_suite, run_test_suite
 
-from invenio.testsuite import make_test_suite, run_test_suite, InvenioTestCase
+from ..utils import bai
 
 
 class BaiTests(InvenioTestCase):
@@ -54,6 +51,26 @@ class BaiTests(InvenioTestCase):
         self.assertEqual(bai("José de Goya y Lucientes, Francisco Y H"),
                          "F.Y.H.Jose.de.Goya.y.Lucientes")
 
+    def test_name_variations_existance(self):
+        """Test if name variations are saved in the record """
+        from invenio_ext.es import es
+        from inspire.modules.authors.utils import author_tokenize
+        rec = es.get(index='hep', id=1343791)
+        rec_list = rec['_source']['authors'][0]['name_variations']
+        name = rec['_source']['authors'][0]['full_name']
+        token_list = author_tokenize(name)
+        self.assertEqual(token_list, rec_list)
+
+    def test_author_tokenizer(self):
+        """Test author name tokenizer """
+        from inspire.modules.authors.utils import author_tokenize
+        token_list = author_tokenize("Ellis Richard")
+        self.assertEqual(token_list, ['E Richard',
+                                      'Ellis Richard',
+                                      'Richard E',
+                                      'Richard Ellis',
+                                      'Richard, E',
+                                      'Richard, Ellis'])
 
 TEST_SUITE = make_test_suite(BaiTests)
 
