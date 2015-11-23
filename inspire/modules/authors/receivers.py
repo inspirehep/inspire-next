97c6ba36c7f640cfb3be5be18823b4305100b7d5
Grzegorz Jacenków <grzegorz.jacenkow@cern.ch>
2015-11-23 10:56:28 +0100

    authors: phonetic name notation
    * Adds assign_signature_block in receivers.py resposible for
      assigning phonetic names of authors (signatures) in HEP
      collection. The blocks are assigned on before_record_index
      signal.
    
    * Extends HEP mappings with signature_block and the type of
      authors to be nested.
    
    * Renames HEP schema ml_block to signature_block.
    
    * Cleans up assign_uuid in receivers.py. Removes
      before_record_update signal.
    
    * Extends HEP schema with regex pattern for uuid.
    
    Signed-off-by: Grzegorz Jacenków <grzegorz.jacenkow@cern.ch>

--- inspire/modules/authors/receivers.py
+++ inspire/modules/authors/receivers.py
@@ -17,13 +17,52 @@
 # along with INSPIRE; if not, write to the Free Software Foundation, Inc.,
 # 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 
+import numpy as np
 import uuid
 
+from beard.clustering import block_phonetic
 from invenio_records.signals import (
     before_record_index,
     before_record_insert,
-    before_record_update,
 )
+
+
+@before_record_index.connect
+def assign_signature_block(recid, json):
+    """Assign phonetic block to each signature.
+
+    The method extends the given signature with a phonetic
+    notation of the author's full name, based on
+    nysiis algorithm. The phonetic block is assigned before
+    the signature is indexed by an elasticsearch instance.
+    """
+    authors = json.get('authors', [])
+
+    for author in authors:
+        if 'full_name' in author:
+            name = {'author_name': author['full_name']}
+
+            signature_block = block_phonetic(
+                np.array([name], dtype=np.object).reshape(-1, 1),
+                threshold=0,
+                phonetic_algorithm='nysiis'
+            )
+
+            author['signature_block'] = signature_block[0]
+
+
+@before_record_insert.connect
+def assign_uuid(sender, *args, **kwargs):
+    """Assign uuid to each signature.
+
+    The method assigns to each signature a universally unique
+    identifier based on Python's built-in uuid4. The identifier
+    is allocated during the inseration of a new record.
+    """
+    authors = sender.get('authors', [])
+
+    for author in authors:
+        author['uuid'] = str(uuid.uuid4())
 
 
 @before_record_index.connect
@@ -32,7 +71,8 @@ def generate_name_variatons(recid, json):
     Adds a field for all the possible variations of an authors name
 
     :param recid: The id of the record that is going to be indexed.
-    :param json: The json representation of the record that is going to be indexed.
+    :param json: The json representation of the record that is going to be
+                 indexed.
     """
     from inspire.modules.authors.utils import author_tokenize
     authors = json.get("authors")
@@ -41,17 +81,3 @@ def generate_name_variatons(recid, json):
             name = author.get("full_name")
             if name:
                 author.update({"name_variations": author_tokenize(name)})
-
-
-@before_record_insert.connect
-@before_record_update.connect
-def assign_uuid(sender, *args, **kwargs):
-    """Assign uuid to each author of a HEP paper."""
-    if 'authors' in sender:
-        authors = sender['authors']
-
-        for author in authors:
-            if 'uuid' not in author:
-                author['uuid'] = str(uuid.uuid4())
-
-        sender['authors'] = authors
