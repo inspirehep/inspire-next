66eaf574ec27d5cfa12ffbf4a58a96f3b4b2e5c0
Panos Paparrigopoulos <panos.paparrigopoulos@cern.ch>
2015-12-01 10:49:27 +0100

    facets: experiments validation
    * Trying to fix misspelled experiment names.
    
    * Adds a new field 'facet_experiment' that should be used
    to generate facets.
    
    Signed-off-by: Panos Paparrigopoulos <panos.paparrigopoulos@cern.ch>

--- inspirehep/modules/records/testsuite/test_records.py
+++ inspirehep/modules/records/testsuite/test_records.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # This file is part of INSPIRE.
-# Copyright (C) 2014, 2015 CERN.
+# Copyright (C) 2015 CERN.
 #
 # INSPIRE is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License as
@@ -17,4 +17,22 @@
 # along with INSPIRE; if not, write to the Free Software Foundation, Inc.,
 # 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 
-from .receivers import *
+from invenio.testsuite import InvenioTestCase
+
+
+class RecordsTests(InvenioTestCase):
+
+    """Test records"""
+
+    def test_facets_experiments(self):
+        """Test if misspelled experiments are corrected."""
+        from invenio_ext.es import es
+        record = es.get(index='hep', id='559118')
+        source = record.get("_source")
+        exp = source.get("accelerator_experiments")
+        # Gets misspelled experiment
+        experiment = exp[0].get("experiment")
+        # This should contain the corected experiment name
+        facet_experiment = exp[0].get("facet_experiment")
+        self.assertEqual(experiment, "CERN-LHC-LHCB")
+        self.assertEqual(facet_experiment, "CERN-LHC-LHCb")
