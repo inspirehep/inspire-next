db8a76ef4a3feeb24a76e8ae7e1463d7da52788d
Javier Martin Montull <javier.martin.montull@cern.ch>
2016-03-01 17:11:00 +0100

    search: initial AngularJS app
    * Uses AngularJS app to show search results.
    
    Signed-off-by: Javier Martin Montull <javier.martin.montull@cern.ch>

---
+++ inspirehep/modules/search/static/js/search/app.js
@@ -0,0 +1,33 @@
+/*
+ * This file is part of Invenio.
+ * Copyright (C) 2015, 2016 CERN.
+ *
+ * Invenio is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * Invenio is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Invenio; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * In applying this license, CERN does not
+ * waive the privileges and immunities granted to it by virtue of its status
+ * as an Intergovernmental Organization or submit itself to any jurisdiction.
+ */
+
+require([
+    'node_modules/invenio-search-js/dist/invenio-search-js',
+  ], function(search) {
+    // When the DOM is ready bootstrap the `invenio-serach-js`
+    angular.element(document).ready(function() {
+      angular.bootstrap(
+        document.getElementById("invenio-search"), ['invenioSearch']
+      );
+    });
+});
