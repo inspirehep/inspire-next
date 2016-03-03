7f526a9961573371c551e405efd9eedda9251f66
Nick Papoutsis <admin@nils.gr>
2016-03-03 16:05:40 +0100

    templates: detailed record collection colors
    * Changes colour of logo and search button according to collection.
    
    Signed-off-by: Nick Papoutsis <admin@nils.gr>

--- setup.py
+++ setup.py
@@ -236,6 +236,7 @@ setup(
             'inspirehep_forms_css = inspirehep.modules.forms.bundles:css',
             'inspirehep_forms_js = inspirehep.modules.forms.bundles:js',
             'inspirehep_detailed_js = inspirehep.modules.theme.bundles:detailedjs',
+            'inspirehep_detailed_css = inspirehep.modules.theme.bundles:detailedcss',
             'inspirehep_author_update_css = inspirehep.modules.authors.bundles:css',
             'invenio_search_ui_search_css = inspirehep.modules.search.bundles:css',
             'invenio_search_ui_search_js = inspirehep.modules.search.bundles:js',
