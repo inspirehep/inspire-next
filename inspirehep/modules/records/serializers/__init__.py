36a589a7dbc0e808a09aff73ca95b548b5b5b0f8
Javier Martin Montull <javier.martin.montull@cern.ch>
2016-03-03 09:40:57 +0100

    records: brief results serializer
    * Introduces new serializer for brief search results in order to preprocess
      records coming from Elasticsearch.
    
    Signed-off-by: Javier Martin Montull <javier.martin.montull@cern.ch>

---
+++ inspirehep/modules/records/serializers/__init__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of INSPIRE.
+# Copyright (C) 2016 CERN.
+#
+# INSPIRE is free software; you can redistribute it
+# and/or modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of the
+# License, or (at your option) any later version.
+#
+# INSPIRE is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with INSPIRE; if not, write to the
+# Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+# MA 02111-1307, USA.
+#
+# In applying this license, CERN does not
+# waive the privileges and immunities granted to it by virtue of its status
+# as an Intergovernmental Organization or submit itself to any jurisdiction.
+
+"""Record serialization."""
+
+from __future__ import absolute_import, print_function
+
+from invenio_records_rest.serializers.response import search_responsify
+from invenio_records_rest.serializers.schemas.json import RecordSchemaJSONV1
+from .json import JSONBriefSerializer
+
+json_brief_v1 = JSONBriefSerializer(RecordSchemaJSONV1)
+json_brief_v1_search = search_responsify(json_brief_v1, 'application/json')
---
+++ inspirehep/modules/records/serializers/json.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of INSPIRE.
+# Copyright (C) 2016 CERN.
+#
+# INSPIRE is free software; you can redistribute it
+# and/or modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of the
+# License, or (at your option) any later version.
+#
+# INSPIRE is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with INSPIRE; if not, write to the
+# Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+# MA 02111-1307, USA.
+#
+# In applying this license, CERN does not
+# waive the privileges and immunities granted to it by virtue of its status
+# as an Intergovernmental Organization or submit itself to any jurisdiction.
+
+"""Marshmallow based JSON serializer for records."""
+
+from __future__ import absolute_import, print_function
+
+from invenio_records_rest.serializers.json import JSONSerializer
+
+
+def process_es_hit(record):
+    """Process record coming from Elasticsearch."""
+    if 'authors' in record:
+        record['authors'] = record['authors'][:10]
+    del record['references']
+
+    return record
+
+
+class JSONBriefSerializer(JSONSerializer):
+    """JSON brief format serializer."""
+
+    @staticmethod
+    def preprocess_search_hit(pid, record_hit, links_factory=None):
+        """Prepare a record hit from Elasticsearch for serialization."""
+        links_factory = links_factory or (lambda x: dict())
+        record = dict(
+            pid=pid,
+            metadata=process_es_hit(record_hit['_source']),
+            links=links_factory(pid),
+            revision=record_hit['_version'],
+            created=None,
+            updated=None,
+        )
+        # Move created/updated attrs from source to object.
+        for key in ['_created', '_updated']:
+            if key in record['metadata']:
+                record[key[1:]] = record['metadata'][key]
+                del record['metadata'][key]
+        return record
