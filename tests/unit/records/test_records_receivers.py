8b0ecbe44cca71ff3e58bdf22bec2adf396a8796
Mihai Bivol <mm.bivol@gmail.com>
2016-03-08 11:52:17 +0100

    records: recids in ES
    * Adds receiver for extracting recids from $ref fields.
    * Adds unit test for receiver
    
    Signed-off-by: Mihai Bivol <mihai.bivol@cern.ch>

---
+++ tests/unit/records/test_records_receivers.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of INSPIRE.
+# Copyright (C) 2016 CERN.
+#
+# INSPIRE is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of the
+# License, or (at your option) any later version.
+#
+# INSPIRE is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with INSPIRE; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+
+from inspirehep.modules.records import receivers
+
+
+def test_populate_recid_from_ref_naming():
+    json_dict = {
+        'simple_key': {'$ref': 'http://x/y/1'},
+        'key_with_record': {'$ref': 'http://x/y/2'},
+        'record': {'$ref': 'http://x/y/3'},
+        'embedded_list': [{'record': {'$ref': 'http://x/y/4'}}],
+        'embedded_record': {'record': {'$ref': 'http://x/y/5'}}
+    }
+
+    receivers.populate_recid_from_ref(None, json_dict)
+
+    assert json_dict['simple_key_recid'] == 1
+    assert json_dict['key_with_recid'] == 2
+    assert json_dict['recid'] == 3
+    assert json_dict['embedded_list'][0]['recid'] == 4
+    assert json_dict['embedded_record']['recid'] == 5
+
+
+def test_populate_recid_from_ref_deleted_records():
+    json_dict = {
+        'deleted_records': [{'$ref': 'http://x/y/1'},
+                            {'$ref': 'http://x/y/2'}]
+    }
+
+    receivers.populate_recid_from_ref(None, json_dict)
+
+    assert json_dict['deleted_recids'] == [1, 2]
+
