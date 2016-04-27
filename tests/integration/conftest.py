69155c7dafe4ac42c2fe8e5ae1ce0b8927c94d14
Jacopo Notarstefano <jacopo.notarstefano@cern.ch>
2016-04-27 13:57:37 +0000

    tests: increase timeout of bulk ES query
    Signed-off-by: Jacopo Notarstefano <jacopo.notarstefano@cern.ch>

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -61,7 +61,7 @@ def app(request):
         migrate('./inspirehep/demosite/data/demo-records.xml.gz', wait_for_results=True)
         es.indices.refresh('records-hep')  # Makes sure that all HEP records were migrated.
 
-        add_citation_counts(request_timeout=20)
+        add_citation_counts(request_timeout=40)
         es.indices.refresh('records-hep')  # Makes sure that all citation counts were added.
 
         yield app
