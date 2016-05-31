e67cfa2da740aa4fccf92e5350002ce4238d9a4b
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2016-05-31 22:55:23 +0200

    tests: unit tests for current dojson utils
    Signed-off-by: Jacopo Notarstefano <jacopo.notarstefano@cern.ch>

--- inspirehep/dojson/utils/__init__.py
+++ inspirehep/dojson/utils/__init__.py
@@ -3,29 +3,81 @@
 # This file is part of INSPIRE.
 # Copyright (C) 2015, 2016 CERN.
 #
-# INSPIRE is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License as
-# published by the Free Software Foundation; either version 2 of the
-# License, or (at your option) any later version.
+# INSPIRE is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
-# INSPIRE is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# General Public License for more details.
+# INSPIRE is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with INSPIRE; if not, write to the Free Software Foundation, Inc.,
-# 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+# along with INSPIRE. If not, see <http://www.gnu.org/licenses/>.
+#
+# In applying this licence, CERN does not waive the privileges and immunities
+# granted to it by virtue of its status as an Intergovernmental Organization
+# or submit itself to any jurisdiction.
 
 """DoJSON related utilities."""
+
+from __future__ import absolute_import, division, print_function
 
 import re
 import six
 
 try:
     from flask import current_app
-except ImportError:
+except ImportError:  # pragma: no cover
     current_app = None
+
+
+def create_profile_url(profile_id):
+    """Create HEP author profile link based on the profile_id."""
+    base_url = 'http://inspirehep.net/record/'
+
+    try:
+        int(profile_id)
+        return base_url + str(profile_id)
+    except (TypeError, ValueError):
+        return ''
+
+
+def get_recid_from_ref(ref_obj):
+    """Retrieve recid from jsonref reference object.
+
+    If no recid can be parsed, return None.
+    """
+    if not isinstance(ref_obj, dict):
+        return None
+    url = ref_obj.get('$ref', '')
+    try:
+        res = int(url.split('/')[-1])
+    except ValueError:
+        res = None
+    return res
+
+
+def get_record_ref(recid, record_type='record'):
+    """Create record jsonref reference object from recid.
+
+    None recids will return a None object.
+    Valid recids will return an object in the form of:
+        {'$ref': url_for_record}
+    """
+    if recid is None:
+        return None
+    default_server = 'http://inspirehep.net'
+    if current_app:
+        server = current_app.config.get('SERVER_NAME') or default_server
+    else:
+        server = default_server
+    # This config might also be http://inspirehep.net or
+    # https://inspirehep.net.
+    if not re.match('^https?://', server):
+        server = 'http://{}'.format(server)
+    return {'$ref': '{}/api/{}/{}'.format(server, record_type, recid)}
 
 
 def legacy_export_as_marc(json, tabsize=4):
@@ -83,17 +135,6 @@ def legacy_export_as_marc(json, tabsize=4):
     return "".join(export)
 
 
-def create_profile_url(profile_id):
-    """Create HEP author profile link based on the profile_id."""
-    base_url = 'http://inspirehep.net/record/'
-
-    try:
-        int(profile_id)
-        return base_url + str(profile_id)
-    except (TypeError, ValueError):
-        return ''
-
-
 def strip_empty_values(obj):
     """Recursively strips empty values."""
     if isinstance(obj, dict):
@@ -110,39 +151,3 @@ def strip_empty_values(obj):
         return type(obj)(new_obj)
     else:
         return obj
-
-
-def get_record_ref(recid, record_type='record'):
-    """Create record jsonref reference object from recid.
-
-    None recids will return a None object.
-    Valid recids will return an object in the form of:
-        {'$ref': url_for_record}
-    """
-    if recid is None:
-        return None
-    default_server = 'http://inspirehep.net'
-    if current_app:
-        server = current_app.config.get('SERVER_NAME') or default_server
-    else:
-        server = default_server
-    # This config might also be http://inspirehep.net or
-    # https://inspirehep.net.
-    if not re.match('^https?://', server):
-        server = 'http://{}'.format(server)
-    return {'$ref': '{}/api/{}/{}'.format(server, record_type, recid)}
-
-
-def get_recid_from_ref(ref_obj):
-    """Retrieve recid from jsonref reference object.
-
-    If no recid can be parsed, return None.
-    """
-    if not isinstance(ref_obj, dict):
-        return None
-    url = ref_obj.get('$ref', '')
-    try:
-        res = int(url.split('/')[-1])
-    except ValueError:
-        res = None
-    return res
