54fbfa05c14c70a2a2155ff14e0173816bd731b2
Panos Paparrigopoulos <panos.paparrigopoulos@cern.ch>
2016-06-07 16:40:12 +0200

    utils: adds/updates record helpers
    Signed-off-by: Panos Paparrigopoulos <panos.paparrigopoulos@cern.ch>

--- tests/unit/utils/test_utils_record.py
+++ tests/unit/utils/test_utils_record.py
@@ -19,61 +19,284 @@
 
 """Tests for record-related utilities."""
 
+from __future__ import absolute_import, print_function
+
+from inspirehep.utils.record import get_abstract, get_subtitle, get_title, get_value
+
+from invenio_records.api import Record
+
 import pytest
 
-from inspirehep.utils.record import get_title, get_value
+
+def test_get_title_returns_empty_string_when_no_titles():
+    no_titles = Record({})
+
+    expected = ''
+    result = get_title(no_titles)
+
+    assert expected == result
 
 
-@pytest.fixture
-def double_title():
-    return {
-        "titles": [
+def test_get_subtitle_returns_empty_string_when_no_titles():
+    no_titles = Record({})
+
+    expected = ''
+    result = get_subtitle(no_titles)
+
+    assert expected == result
+
+
+def test_get_abstract_returns_empty_string_when_no_titles():
+    no_abstracts = Record({})
+
+    expected = ''
+    result = get_abstract(no_abstracts)
+
+    assert expected == result
+
+
+def test_get_title_returns_empty_string_when_titles_is_empty():
+    empty_titles = Record({'titles': []})
+
+    expected = ''
+    result = get_title(empty_titles)
+
+    assert expected == result
+
+
+def test_get_subtitle_returns_empty_string_when_titles_is_empty():
+    empty_titles = Record({'titles': []})
+
+    expected = ''
+    result = get_subtitle(empty_titles)
+
+    assert expected == result
+
+
+def test_get_abstract_returns_empty_string_when_abstracts_is_empty():
+    empty_abstracts = Record({'abstracts': []})
+
+    expected = ''
+    result = get_abstract(empty_abstracts)
+
+    assert expected == result
+
+
+def test_get_title_returns_the_only_title():
+    single_title = Record({
+        'titles': [
+            {
+                'source': "arXiv",
+                'title': 'The Large Hadron Collider'
+            }
+        ]
+    })
+
+    expected = 'The Large Hadron Collider'
+    result = get_title(single_title)
+
+    assert expected == result
+
+
+def test_get_subtitle_returns_the_only_subtitle():
+    single_subtitle = Record({
+        'titles': [
             {
                 "source": "arXiv",
-                "title": "Parton distributions with LHC data"
-            },
-            {
-                "title": "Parton distributions with LHC data"
+                'subtitle': 'Harvest of Run 1'
             }
         ]
-    }
+    })
+
+    expected = 'Harvest of Run 1'
+    result = get_subtitle(single_subtitle)
+
+    assert expected == result
 
 
-@pytest.fixture
-def single_title():
-    return {
+def test_get_abstract_returns_the_only_abstract():
+    single_abstract = Record({
+        "abstracts": [
+            {
+                "source": "arXiv",
+                "value": "abstract",
+            }
+        ]
+    })
+
+    expected = 'abstract'
+    result = get_abstract(single_abstract)
+
+    assert expected == result
+
+
+def test_get_title_returns_the_non_arxiv_title_with_source():
+    double_title = Record({
         "titles": [
             {
-                "subtitle": "Harvest of Run 1",
-                "title": "The Large Hadron Collider"
+                "source": "other",
+                "title": "Importance of a consistent choice of alpha(s) in the matching of AlpGen and Pythia"
+            },
+            {
+                "source": "arXiv",
+                "title": "Monte Carlo tuning in the presence of Matching"
+            }
+        ],
+    })
+
+    expected = 'Importance of a consistent choice of alpha(s) in the matching of AlpGen and Pythia'
+    result = get_title(double_title)
+
+    assert expected == result
+
+
+def test_get_title_returns_the_non_arxiv_title():
+    double_title = Record({
+        "titles": [
+            {
+                "title": "Importance of a consistent choice of alpha(s) in the matching of AlpGen and Pythia"
+            },
+            {
+                "source": "arXiv",
+                "title": "Monte Carlo tuning in the presence of Matching"
+            }
+        ],
+    })
+
+    expected = 'Importance of a consistent choice of alpha(s) in the matching of AlpGen and Pythia'
+    result = get_title(double_title)
+
+    assert expected == result
+
+
+def test_get_subtitle_returns_the_non_arxiv_subtitle_with_source():
+    double_subtitle = Record({
+        "titles": [
+            {
+                "source": "other",
+                "subtitle": "Importance of a consistent choice of alpha(s) in the matching of AlpGen and Pythia"
+            },
+            {
+                "source": "arXiv",
+                "subtitle": "Monte Carlo tuning in the presence of Matching"
+            }
+        ],
+    })
+
+    expected = 'Importance of a consistent choice of alpha(s) in the matching of AlpGen and Pythia'
+    result = get_subtitle(double_subtitle)
+
+    assert expected == result
+
+
+def test_get_subtitle_returns_the_non_arxiv_subtitle():
+    double_subtitle = Record({
+        "titles": [
+            {
+                "subtitle": "Importance of a consistent choice of alpha(s) in the matching of AlpGen and Pythia"
+            },
+            {
+                "source": "arXiv",
+                "subtitle": "Monte Carlo tuning in the presence of Matching"
+            }
+        ],
+    })
+
+    expected = 'Importance of a consistent choice of alpha(s) in the matching of AlpGen and Pythia'
+    result = get_subtitle(double_subtitle)
+
+    assert expected == result
+
+
+def test_get_abstract_returns_the_non_arxiv_abstract():
+    double_abstract = Record({
+        "abstracts": [
+            {
+                "source": "arXiv",
+                "value": "arXiv abstract"
+            },
+            {
+                "value": "abstract"
             }
         ]
-    }
+    })
+
+    expected = 'abstract'
+    result = get_abstract(double_abstract)
+
+    assert expected == result
 
 
-@pytest.fixture
-def empty_title():
-    return {
-        "titles": []
-    }
+def test_get_abstract_with_multiple_sources_returns_the_non_arxiv_abstract():
+    double_abstract = Record({
+        "abstracts": [
+            {
+                "source": "arXiv",
+                "value": "arXiv abstract"
+            },
+            {
+                "source": "other",
+                "value": "abstract"
+            }
+        ]
+    })
+
+    expected = 'abstract'
+    result = get_abstract(double_abstract)
+
+    assert expected == result
 
 
-def test_get_title(double_title, single_title, empty_title):
-    """Test get title utility."""
-    assert get_title(double_title) == "Parton distributions with LHC data"
-    assert get_title(single_title) == "The Large Hadron Collider"
-    assert get_title(empty_title) == ""
+def test_get_value_returns_the_two_titles():
+    double_title = Record({
+        "titles": [
+            {
+                "title": "Importance of a consistent choice of alpha(s) in the matching of AlpGen and Pythia"
+            },
+            {
+                "title": "Monte Carlo tuning in the presence of Matching"
+            }
+        ],
+    })
 
-    no_title_key = {
-        "not_titles": []
-    }
-    assert get_title(no_title_key) == ""
+    expected = 2
+    result = len(get_value(double_title, "titles.title"))
+
+    assert expected == result
 
 
-def test_get_value(double_title, single_title, empty_title):
-    """Test get_value utility"""
-    assert len(get_value(double_title, "titles.title")) == 2
-    assert get_value(double_title, "titles.title[0]") == "Parton distributions with LHC data"
-    assert get_value(single_title, "titles.title") == ["The Large Hadron Collider"]
-    assert get_value(empty_title, "titles.title") == []
-    assert get_value(empty_title, "foo", {}) == {}
+def test_get_value_returns_the_selected_title():
+    double_title = Record({
+        "titles": [
+            {
+                "title": "Importance of a consistent choice of alpha(s) in the matching of AlpGen and Pythia"
+            },
+            {
+                "title": "Monte Carlo tuning in the presence of Matching"
+            }
+        ],
+    })
+
+    expected = 'Importance of a consistent choice of alpha(s) in the matching of AlpGen and Pythia'
+    result = get_value(double_title, "titles.title[0]")
+
+    assert expected == result
+
+
+def test_get_value_returns_single_title():
+    empty_titles = Record({'titles': []})
+
+    expected = []
+    result = get_value(empty_titles, "titles.title")
+
+    assert expected == result
+
+
+@pytest.mark.xfail(reason='Returns None instead of {}.')
+def test_get_value_returns_empty_dic_when_there_are_no_titles():
+    empty_titles = Record({'titles': []})
+
+    expected = {}
+    result = get_value(empty_titles, "foo")
+
+    assert expected == result
