a0448bf869c61fc494ac4a09d3627e10134946a4
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2016-07-12 18:31:51 +0200

    setup: add flask-shell-ipython
    Signed-off-by: Jacopo Notarstefano <jacopo.notarstefano@cern.ch>

--- setup.py
+++ setup.py
@@ -73,7 +73,7 @@ install_requires = [
     'Flask>=0.11.1',
     'Flask-Breadcrumbs>=0.3.0',
     'Flask-Script>=2.0.5',
-    'Flask-CLI>=0.4.0',
+    'flask-shell-ipython>=0.2.2',
     'jsmin',
     'pytest-runner>=2.7.0',
     'workflow>=2.0.0',
