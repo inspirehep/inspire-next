04842a42c75d151a2919ad7829552113bb51822e
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2016-07-18 00:26:56 +0200

    tests: unit tests for DoJSON experiments
    Signed-off-by: Jacopo Notarstefano <jacopo.notarstefano@cern.ch>

--- tests/unit/dojson/test_dojson_experiments.py
+++ tests/unit/dojson/test_dojson_experiments.py
@@ -22,10 +22,12 @@
 
 from __future__ import absolute_import, division, print_function
 
+import pytest
+
 from dojson.contrib.marc21.utils import create_record
 
 from inspirehep.dojson.experiments import experiments
-from inspirehep.dojson.utils import strip_empty_values
+from inspirehep.dojson.utils import clean_record
 
 
 def test_contact_details_from_marcxml_270_single_p_single_m():
@@ -44,7 +46,7 @@ def test_contact_details_from_marcxml_270_single_p_single_m():
             'email': 'lindner@mpi-hd.mpg.de',
         },
     ]
-    result = strip_empty_values(experiments.do(create_record(snippet)))
+    result = clean_record(experiments.do(create_record(snippet)))
 
     assert expected == result['contact_details']
 
@@ -66,7 +68,7 @@ def test_contact_details_from_marcxml_270_double_p_single_m():
             'email': 'lindner@mpi-hd.mpg.de',
         },
     ]
-    result = strip_empty_values(experiments.do(create_record(snippet)))
+    result = clean_record(experiments.do(create_record(snippet)))
 
     assert expected == result['contact_details']
 
@@ -88,7 +90,7 @@ def test_contact_details_from_marcxml_270_single_p_double_m():
             'name': 'Manfred Lindner'
         },
     ]
-    result = strip_empty_values(experiments.do(create_record(snippet)))
+    result = clean_record(experiments.do(create_record(snippet)))
 
     assert expected == result['contact_details']
 
@@ -115,7 +117,7 @@ def test_contact_details_from_multiple_marcxml_270():
             'name': 'Wynton Marsalis',
         },
     ]
-    result = strip_empty_values(experiments.do(create_record(snippet)))
+    result = clean_record(experiments.do(create_record(snippet)))
 
     assert expected == result['contact_details']
 
@@ -134,7 +136,7 @@ def test_experiment_names_from_marcxml_119():
             'title': 'CERN-ALPHA',
         },
     ]
-    result = strip_empty_values(experiments.do(create_record(snippet)))
+    result = clean_record(experiments.do(create_record(snippet)))
 
     assert expected == result['experiment_names']
 
@@ -149,7 +151,7 @@ def test_experiment_names_and_affiliation_from_marcxml_119():
         '</record>'
     )
 
-    result = strip_empty_values(experiments.do(create_record(snippet)))
+    result = clean_record(experiments.do(create_record(snippet)))
 
     assert result['affiliation'][0] == 'CERN'
     assert result['experiment_names'][0]['title'] == 'CERN-ALPHA'
@@ -166,7 +168,7 @@ def test_experiment_names_and_affiliation_from_marcxml_119_two_u():
         '</record>'
     )
 
-    result = strip_empty_values(experiments.do(create_record(snippet)))
+    result = clean_record(experiments.do(create_record(snippet)))
 
     assert result['affiliation'] == ['Cambridge U.', 'Edinburgh U.']
     assert result['experiment_names'][0]['title'] == 'LATTICE-UKQCD'
@@ -186,7 +188,7 @@ def test_titles_from_marcxml_245():
             'title': 'The ALPHA experiment',
         },
     ]
-    result = strip_empty_values(experiments.do(create_record(snippet)))
+    result = clean_record(experiments.do(create_record(snippet)))
 
     assert expected == result['titles']
 
@@ -205,7 +207,7 @@ def test_title_variants_from_marcxml_419():
             'title': 'ALPHA',
         },
     ]
-    result = strip_empty_values(experiments.do(create_record(snippet)))
+    result = clean_record(experiments.do(create_record(snippet)))
 
     assert expected == result['title_variants']
 
@@ -230,6 +232,289 @@ def test_multiple_title_variants_from_marcxml_419():
             'title': 'CERN-NA-048-3',
         },
     ]
-    result = strip_empty_values(experiments.do(create_record(snippet)))
+    result = clean_record(experiments.do(create_record(snippet)))
 
     assert expected == result['title_variants']
+
+
+def test_description_from_520__a():
+    snippet = (
+        '<datafield tag="520" ind1=" " ind2=" ">'
+        '  <subfield code="a">The Muon Accelerator Program (MAP) was created in 2010 to unify the DOE supported R&amp;D in the U.S. aimed at developing the concepts and technologies required for Muon Colliders and Neutrino Factories. These muon based facilities have the potential to discover and explore new exciting fundamental physics, but will require the development of demanding technologies and innovative concepts. The MAP aspires to prove the feasibility of a Muon Collider within a few years, and to make significant contributions to the international effort devoted to developing Neutrino Factories. MAP was formally approved on March 18, 2011.</subfield>'
+        '</datafield>'
+    )  # record/1108188
+
+    expected = [
+        'The Muon Accelerator Program (MAP) was created in 2010 to unify the DOE supported R&D in the U.S. aimed at developing the concepts and technologies required for Muon Colliders and Neutrino Factories. These muon based facilities have the potential to discover and explore new exciting fundamental physics, but will require the development of demanding technologies and innovative concepts. The MAP aspires to prove the feasibility of a Muon Collider within a few years, and to make significant contributions to the international effort devoted to developing Neutrino Factories. MAP was formally approved on March 18, 2011.'
+    ]
+    result = clean_record(experiments.do(create_record(snippet)))
+
+    assert expected == result['description']
+
+
+def test_description_from_multiple_520__a():
+    snippet = (
+        '<record>'
+        '  <datafield tag="520" ind1=" " ind2=" ">'
+        '    <subfield code="a">DAMA is an observatory for rare processes which develops and uses several low-background set-ups at the Gran Sasso National Laboratory of the I.N.F.N. (LNGS). The main experimental set-ups are: i) DAMA/NaI (about 100 kg of highly radiopure NaI(Tl)), which completed its data taking on July 2002</subfield>'
+        '  </datafield>'
+        '  <datafield tag="520" ind1=" " ind2=" ">'
+        '    <subfield code="a">ii) DAMA/LXe (about 6.5 kg liquid Kr-free Xenon enriched either in 129Xe or in 136Xe)</subfield>'
+        '  </datafield>'
+        '  <datafield tag="520" ind1=" " ind2=" ">'
+        '    <subfield code="a">iii) DAMA/R&amp;D, devoted to tests on prototypes and to small scale experiments, mainly on the investigations of double beta decay modes in various isotopes. iv) the second generation DAMA/LIBRA set-up (about 250 kg highly radiopure NaI(Tl)) in operation since March 2003</subfield>'
+        '  </datafield>'
+        '  <datafield tag="520" ind1=" " ind2=" ">'
+        '    <subfield code="a">v) the low background DAMA/Ge detector mainly devoted to sample measurements: in some measurements on rare processes the low-background Germanium detectors of the LNGS facility are also used. Moreover, a third generation R&amp;D is in progress towards a possible 1 ton set-up, DAMA proposed in 1996. In particular, the DAMA/NaI and the DAMA/LIBRA set-ups have investigated the presence of Dark Matter particles in the galactic halo by exploiting the Dark Matter annual modulation signature.</subfield>'
+        '  </datafield>'
+        '</record>'
+    )  # record/1110568
+
+    expected = [
+        'DAMA is an observatory for rare processes which develops and uses several low-background set-ups at the Gran Sasso National Laboratory of the I.N.F.N. (LNGS). The main experimental set-ups are: i) DAMA/NaI (about 100 kg of highly radiopure NaI(Tl)), which completed its data taking on July 2002',
+        'ii) DAMA/LXe (about 6.5 kg liquid Kr-free Xenon enriched either in 129Xe or in 136Xe)',
+        'iii) DAMA/R&D, devoted to tests on prototypes and to small scale experiments, mainly on the investigations of double beta decay modes in various isotopes. iv) the second generation DAMA/LIBRA set-up (about 250 kg highly radiopure NaI(Tl)) in operation since March 2003',
+        'v) the low background DAMA/Ge detector mainly devoted to sample measurements: in some measurements on rare processes the low-background Germanium detectors of the LNGS facility are also used. Moreover, a third generation R&D is in progress towards a possible 1 ton set-up, DAMA proposed in 1996. In particular, the DAMA/NaI and the DAMA/LIBRA set-ups have investigated the presence of Dark Matter particles in the galactic halo by exploiting the Dark Matter annual modulation signature.',
+    ]
+    result = clean_record(experiments.do(create_record(snippet)))
+
+    assert expected == result['description']
+
+
+@pytest.mark.xfail(reason='spokeperson is populated instead')
+def test_spokepersons_from_702__a_i_z():
+    snippet = (
+        '<datafield tag="702" ind1=" " ind2=" ">'
+        '  <subfield code="a">Hogan, Craig J.</subfield>'
+        '  <subfield code="i">INSPIRE-00090662</subfield>'
+        '  <subfield code="z">Current</subfield>'
+        '</datafield>'
+    )  # record/1108189
+
+    expected = [
+        {
+            'ids': [
+                {
+                    'type': 'INSPIRE',
+                    'value': 'INSPIRE-00090662',
+                },
+            ],
+            'name': 'Hogan, Craig J.',
+            'current': True,
+            'curated_relation': False,
+        },
+    ]
+    result = clean_record(experiments.do(create_record(snippet)))
+
+    assert expected == result['spokepersons']
+
+
+@pytest.mark.xfail(reason='spokeperson is populated instead')
+def test_spokepersons_from_double_702__a_i():
+    snippet = (
+        '<record>'
+        '  <datafield tag="702" ind1=" " ind2=" ">'
+        '    <subfield code="a">Feldman, Gary</subfield>'
+        '    <subfield code="i">INSPIRE-00080677</subfield>'
+        '    <subfield code="x">1010209</subfield>'
+        '  </datafield>'
+        '  <datafield tag="702" ind1=" " ind2=" ">'
+        '    <subfield code="a">Messier, Mark</subfield>'
+        '    <subfield code="i">INSPIRE-00107105</subfield>'
+        '  </datafield>'
+        '</record>'
+    )  # record/1402897
+
+    expected = [
+        {
+            'ids': [
+                {
+                    'type': 'INSPIRE',
+                    'value': 'INSPIRE-00080677',
+                },
+            ],
+            'name': 'Feldman, Gary',
+            'record': {'$ref': 'http://localhost:5000/api/authors/1010209'},
+            'curated_relation': True,
+        },
+        {
+            'ids': [
+                {
+                    'type': 'INSPIRE',
+                    'value': 'INSPIRE-00107105',
+                },
+            ],
+            'name': 'Messier, Mark',
+            'curated_relation': False,
+        },
+    ]
+    result = clean_record(experiments.do(create_record(snippet)))
+
+    assert expected == result['spokepersons']
+
+
+def test_collaboration_from_710__g():
+    snippet = (
+        '<datafield tag="710" ind1=" " ind2=" ">'
+        '  <subfield code="g">DarkSide</subfield>'
+        '</datafield>'
+    )  # record/1108199
+
+    result = clean_record(experiments.do(create_record(snippet)))
+
+    assert result['collaboration'] == 'DarkSide'
+    assert 'collaboration_alternative_names' not in result
+
+
+@pytest.mark.xfail(reason='collaboration_alternative_names is not populated')
+def test_collaboration_from_double_710__g():
+    snippet = (
+        '<record>'
+        '  <datafield tag="710" ind1=" " ind2=" ">'
+        '    <subfield code="g">MiniBooNE</subfield>'
+        '  </datafield>'
+        '  <datafield tag="710" ind1=" " ind2=" ">'
+        '    <subfield code="g">BooNE</subfield>'
+        '  </datafield>'
+        '</record>'
+    )  # record/1110641
+
+    result = clean_record(experiments.do(create_record(snippet)))
+
+    assert result['collaboration'] == 'BooNE'
+    assert result['collaboration_alternative_names'] == ['MiniBooNE']
+
+
+@pytest.mark.xfail(reason='curated_relation is missing')
+def test_related_experiments_from_510__a_w_0():
+    snippet = (
+        '<datafield tag="510" ind1=" " ind2=" ">'
+        '  <subfield code="0">1262631</subfield>'
+        '  <subfield code="a">LZ</subfield>'
+        '  <subfield code="w">b</subfield>'
+        '</datafield>'
+    )  # record/1108192
+
+    expected = [
+        {
+            'name': 'LZ',
+            'record': {'$ref': 'http://localhost:5000/api/experiments/1262631'},
+            'relation': 'successor',
+            'curated_relation': True,
+        },
+    ]
+    result = clean_record(experiments.do(create_record(snippet)))
+
+    assert expected == result['related_experiments']
+
+
+@pytest.mark.xfail(reason='curated_relation is missing')
+def test_related_experiments_from_double_510__a_w_0():
+    snippet = (
+        '<record>'
+        '  <datafield tag="510" ind1=" " ind2=" ">'
+        '    <subfield code="0">1108293</subfield>'
+        '    <subfield code="a">XENON</subfield>'
+        '    <subfield code="w">a</subfield>'
+        '  </datafield>'
+        '  <datafield tag="510" ind1=" " ind2=" ">'
+        '    <subfield code="0">1386527</subfield>'
+        '    <subfield code="a">XENON100</subfield>'
+        '    <subfield code="w">a</subfield>'
+        '  </datafield>'
+        '</record>'
+    )  # record/1386519
+
+    expected = [
+        {
+            'name': 'XENON',
+            'record': {'$ref': 'http://localhost:5000/api/experiments/1108293'},
+            'relation': 'predecessor',
+            'curated_relation': True,
+        },
+        {
+            'name': 'XENON100',
+            'record': {'$ref': 'http://localhost:5000/api/experiments/1386527'},
+            'relation': 'predecessor',
+            'curated_relation': True,
+        },
+    ]
+    result = clean_record(experiments.do(create_record(snippet)))
+
+    assert expected == result['related_experiments']
+
+
+@pytest.mark.xfail(reason='overwrites with None')
+def test_date_started_from_046__q_s_and_046__r():
+    snippet = (
+        '<record>'
+        '  <datafield tag="046" ind1=" " ind2=" ">'
+        '    <subfield code="q">2009-08-19</subfield>'
+        '    <subfield code="s">2009-11-30</subfield>'
+        '  </datafield>'
+        '  <datafield tag="046" ind1=" " ind2=" ">'
+        '    <subfield code="r">2009-10-08</subfield>'
+        '  </datafield>'
+        '</record>'
+    )  # record/1318099
+
+    expected = '2009-11-30'
+    result = clean_record(experiments.do(create_record(snippet)))
+
+    assert expected == result['date_started']
+
+
+def test_date_started_from_046__q_and_046__r_and_046__x():
+    snippet = (
+        '<record>'
+        '  <datafield tag="046" ind1=" " ind2=" ">'
+        '    <subfield code="q">2010</subfield>'
+        '  </datafield>'
+        '  <datafield tag="046" ind1=" " ind2=" ">'
+        '    <subfield code="r">2011-03-18</subfield>'
+        '  </datafield>'
+        '  <datafield tag="046" ind1=" " ind2=" ">'
+        '    <subfield code="x">yes</subfield>'
+        '  </datafield>'
+        '</record>'
+    )  # record/1108188
+
+    result = clean_record(experiments.do(create_record(snippet)))
+
+    assert 'date_started' not in result
+
+
+@pytest.mark.xfail(reason='overwrites with None')
+def test_date_started_and_date_completed_from_046():
+    snippet = (
+        '<record>'
+        '  <datafield tag="046" ind1=" " ind2=" ">'
+        '    <subfield code="s">1996</subfield>'
+        '  </datafield>'
+        '  <datafield tag="046" ind1=" " ind2=" ">'
+        '    <subfield code="t">2002</subfield>'
+        '  </datafield>'
+        '  <datafield tag="046" ind1=" " ind2=" ">'
+        '    <subfield code="x">yes</subfield>'
+        '  </datafield>'
+        '</record>'
+    )  # record/1108324
+
+    result = clean_record(experiments.do(create_record(snippet)))
+
+    assert result['date_started'] == '1996'
+    assert result['date_completed'] == '2002'
+
+
+@pytest.mark.xfail(reason='a list is returned')
+def test_accelerator_from_693__a():
+    snippet = (
+        '<datafield tag="693" ind1=" " ind2=" ">'
+        '  <subfield code="a">AD</subfield>'
+        '</datafield>'
+    )  # record/1108206
+
+    expected = 'AD'
+    result = clean_record(experiments.do(create_record(snippet)))
+
+    assert expected == result['accelerator']
