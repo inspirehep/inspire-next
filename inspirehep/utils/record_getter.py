51be2baa3d97add467921cb716f40805d434e462
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2016-08-08 17:53:31 +0200

    utils: proper logging in record_getter
    * Ensures that Sentry can cluster together these log events.
      Addresses #1371.
    
    Signed-off-by: Jacopo Notarstefano <jacopo.notarstefano@cern.ch>

--- inspirehep/utils/record_getter.py
+++ inspirehep/utils/record_getter.py
@@ -22,7 +22,7 @@
 
 """Resource-aware json reference loaders to be used with jsonref."""
 
-from __future__ import absolute_import, print_function
+from __future__ import absolute_import, division, print_function
 
 from functools import wraps
 
@@ -55,9 +55,7 @@ def raise_record_getter_error_and_log(f):
         try:
             return f(*args)
         except Exception as e:
-            current_app.logger.error('Error in retrieving record {0} '
-                                     'caused by {1}'.format(repr(args),
-                                                            repr(e)))
+            current_app.logger.exception("Can't load recid %s", args)
             raise RecordGetterError(e.message, e)
 
     return wrapper
@@ -76,8 +74,7 @@ def get_es_record(record_type, recid):
 
 @raise_record_getter_error_and_log
 def get_es_record_by_uuid(uuid):
-    pid = PersistentIdentifier.query.filter_by(
-        object_uuid=uuid).one()
+    pid = PersistentIdentifier.query.filter_by(object_uuid=uuid).one()
     search_conf = current_app.config['RECORDS_REST_ENDPOINTS'][pid.pid_type]
 
     return es.get_source(
