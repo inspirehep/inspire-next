6dabf6c89ec8bfe74990a192b5a3cafc69150d8e
Grzegorz Jacenków <grzegorz.jacenkow@cern.ch>
2016-08-18 16:44:44 +0200

    api: extend /authors endpoint
    Co-authored-by: Jacopo Notarstefano <jacopo.notarstefano@cern.ch>
    Signed-off-by: Grzegorz Jacenków <grzegorz.jacenkow@cern.ch>

---
+++ inspirehep/modules/authors/rest/citations.py
@@ -0,0 +1,120 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of INSPIRE.
+# Copyright (C) 2016 CERN.
+#
+# INSPIRE is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# INSPIRE is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with INSPIRE. If not, see <http://www.gnu.org/licenses/>.
+#
+# In applying this licence, CERN does not waive the privileges and immunities
+# granted to it by virtue of its status as an Intergovernmental Organization
+# or submit itself to any jurisdiction.
+
+from __future__ import absolute_import, division, print_function
+
+import json
+
+from inspirehep.modules.search import LiteratureSearch
+
+
+class AuthorAPICitations(object):
+    """API endpoint for author collection returning citations."""
+
+    def serialize(self, pid, record, links_factory=None):
+        """Return a list of citations for a given author recid.
+
+        :param pid:
+            Persistent identifier instance.
+
+        :param record:
+            Record instance.
+
+        :param links_factory:
+            Factory function for the link generation, which are added to
+            the response.
+        """
+        author_pid = pid.pid_value
+        citations = []
+
+        search = LiteratureSearch().query({
+            "match": {
+                "authors.recid": author_pid
+            }
+        }).params(
+            _source=[
+                "authors.recid",
+                "control_number",
+                "self",
+            ]
+        )
+
+        # For each publication co-authored by a given author...
+        for result in search.scan():
+            result_source = result.to_dict()
+            authors = set([i['recid'] for i in result_source['authors']])
+
+            nested_search = LiteratureSearch().query({
+                "match": {
+                    "references.recid": result_source['control_number']
+                }
+            }).params(
+                _source=[
+                    "authors.recid",
+                    "collections",
+                    "control_number",
+                    "earliest_date",
+                    "self",
+                ]
+            )
+
+            # The source record that is being cited.
+            citee = result_source['self']
+
+            # Check all publications, which cite the parent record.
+            for nested_result in nested_search.scan():
+                nested_result_source = nested_result.to_dict()
+
+                # Not every signature has a recid (at least for demo records).
+                try:
+                    nested_authors = set(
+                        [i['recid'] for i in nested_result_source['authors']]
+                    )
+                except KeyError:
+                    nested_authors = set()
+
+                citation = {}
+                citation['citee'] = {'record': citee}
+                citation['citer'] = {'record': nested_result_source['self']}
+
+                # If at least one author is shared, it's a self-citation.
+                citation['self_citation'] = len(authors & nested_authors) > 0
+
+                # Get the earliest date of a citer.
+                try:
+                    citation['date'] = nested_result_source['earliest_date']
+                except KeyError:
+                    pass
+
+                # Get status if a citer is published.
+                # FIXME: As discussed with Sam, we should have a boolean flag
+                #        for this type of information.
+                try:
+                    citation['published_paper'] = "Published" in [
+                        i['primary'] for i in nested_result_source[
+                            'collections']]
+                except KeyError:
+                    citation['published_paper'] = False
+
+                citations.append(citation)
+
+        return json.dumps(citations)
---
+++ tests/integration/test_author_api.py
@@ -0,0 +1,191 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of INSPIRE.
+# Copyright (C) 2016 CERN.
+#
+# INSPIRE is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# INSPIRE is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with INSPIRE. If not, see <http://www.gnu.org/licenses/>.
+#
+# In applying this licence, CERN does not waive the privileges and immunities
+# granted to it by virtue of its status as an Intergovernmental Organization
+# or submit itself to any jurisdiction.
+
+from __future__ import absolute_import, division, print_function
+
+import json
+
+import six
+from jsonschema import validate
+
+
+def test_api_authors_root(app):
+    with app.test_client() as client:
+        response = client.get('/api/authors/983220')
+
+        assert response.status_code == 200
+
+        response_json = json.loads(response.data)
+
+        assert response_json['id'] == 983220
+
+
+def test_api_authors_citations(app):
+    schema = {
+        'items': {
+            'properties': {
+                'citee': {
+                    'properties': {
+                        'record': {
+                            'properties': {'$ref': {'type': 'string'}},
+                            'type': 'object'
+                        }
+                    },
+                    'type': 'object'
+                },
+                'citer': {
+                    'properties': {
+                        'record': {
+                            'properties': {'$ref': {'type': 'string'}},
+                            'type': 'object'
+                        }
+                    },
+                    'type': 'object'
+                },
+                'date': {
+                    'format': 'date',
+                    'type': 'string'
+                },
+                'published_paper': {'type': 'boolean'},
+                'self_citation': {'type': 'boolean'}
+            },
+            'type': 'object'
+        },
+        'type': 'array'
+    }
+
+    with app.test_client() as client:
+        response = client.get('/api/authors/983220/citations')
+
+        assert response.status_code == 200
+
+        response_json = json.loads(response.data)
+
+        assert validate(response_json, schema) is None
+        assert len(response_json) == 197
+
+
+def test_api_authors_coauthors(app):
+    schema = {
+        'items': {
+            'properties': {
+                'count': {'type': 'integer'},
+                'full_name': {'type': 'string'},
+                'id': {'type': 'integer'},
+                'record': {
+                    'properties': {'$ref': {'type': 'string'}},
+                    'type': 'object'
+                }
+            },
+            'type': 'object'
+        },
+        'type': 'array'
+    }
+
+    with app.test_client() as client:
+        response = client.get('/api/authors/983220/coauthors')
+
+        assert response.status_code == 200
+
+        response_json = json.loads(response.data)
+
+        assert validate(response_json, schema) is None
+        assert len(response_json) == 100
+
+
+def test_api_authors_publications(app):
+    schema = {
+        'items': {
+            'properties': {
+                'collaborations': {
+                    'items': {'type': 'string'},
+                    'type': 'array'
+                },
+                'citations': {'type': 'integer'},
+                'date': {
+                    'format': 'date',
+                    'type': 'string'
+                },
+                'id': {'type': 'integer'},
+                'journals': {
+                    'properties': {
+                        'record': {
+                            'properties': {'$ref': {'type': 'string'}},
+                            'type': 'object'
+                        }
+                    },
+                    'type': 'object'
+                },
+                'title': {'type': 'string'},
+                'type': {'type': 'string'}
+            },
+            'type': 'object'
+        },
+        'type': 'array'
+    }
+
+    with app.test_client() as client:
+        response = client.get('/api/authors/983220/publications')
+
+        assert response.status_code == 200
+
+        response_json = json.loads(response.data)
+
+        assert validate(response_json, schema) is None
+        assert len(response_json) == 30
+
+
+def test_api_author_stats(app):
+    schema = {
+        'properties': {
+            'citations': {'type': 'integer'},
+            'fields': {
+                'items': {'type': 'string'},
+                'type': 'array',
+            },
+            'hindex': {'type': 'integer'},
+            'i10index': {'type': 'integer'},
+            'keywords': {
+                'items': {
+                    'properties': {
+                        'count': {'type': 'integer'},
+                        'keyword': {'type': 'string'}
+                    },
+                    'type': 'object'
+                },
+                'type': 'array'
+            },
+            'publications': {'type': 'integer'},
+            'types': {'type': 'object'},
+        },
+        'type': 'object'
+    }
+
+    with app.test_client() as client:
+        response = client.get('/api/authors/983220/stats')
+
+        assert response.status_code == 200
+
+        response_json = json.loads(response.data)
+
+        assert validate(response_json, schema) is None
+        assert len(response_json['keywords']) == 25
