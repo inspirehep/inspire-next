ef5a6dc490ace6ce5389a64e414b66a97804b6e4
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2016-09-01 01:51:00 +0200

    warnings: fix license and imports
    Signed-off-by: Jacopo Notarstefano <jacopo.notarstefano@cern.ch>

--- inspirehep/modules/warnings/__init__.py
+++ inspirehep/modules/warnings/__init__.py
@@ -1,21 +1,24 @@
 # -*- coding: utf-8 -*-
 #
-# This file is part of INSPIRE
+# This file is part of INSPIRE.
 # Copyright (C) 2016 CERN.
 #
-# INSPIRE is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License as
-# published by the Free Software Foundation; either version 2 of the
-# License, or (at your option) any later version.
+# INSPIRE is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
-# INSPIRE is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# General Public License for more details.
+# INSPIRE is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with INSPIRE; if not, write to the Free Software Foundation,
-# Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+# along with INSPIRE. If not, see <http://www.gnu.org/licenses/>.
+#
+# In applying this licence, CERN does not waive the privileges and immunities
+# granted to it by virtue of its status as an Intergovernmental Organization
+# or submit itself to any jurisdiction.
 
 """Warnings extension that removes deprecation warnings.
 
@@ -23,7 +26,9 @@ Invenio-Base enables deprecation warnings and this extension removes
 this setting unless ``DEBUG_SHOW_DEPRECATION_WARNINGS`` is set to True.
 """
 
-from __future__ import absolute_import
+from __future__ import absolute_import, division, print_function
+
+import warnings
 
 
 class INSPIREWarnings(object):
@@ -41,7 +46,6 @@ class INSPIREWarnings(object):
                               False)
 
         if not app.config['DEBUG_SHOW_DEPRECATION_WARNINGS']:
-            import warnings
             warnings.filterwarnings(
                 'ignore',
                 category=DeprecationWarning
