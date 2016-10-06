c94fa5cafcf0ed013b0d2971fbf1eef009189ea0
Riccardo Candido <riccardocandido@gmail.com>
2016-10-06 11:09:26 +0200

    tests: add test for the affilation autocompletion (basic info)

--- tests/acceptance/test_literature_suggestion_form.py
+++ tests/acceptance/test_literature_suggestion_form.py
@@ -90,6 +90,19 @@ def test_format_input_DOI(selenium, login):
     assert "The provided DOI is invalid - it should look" not in selenium.page_source
 
 
+def test_basic_info_autocomplete_affilation(selenium, login):
+    """Test the autocompletion for the affilation in the basic info section"""
+    selenium.get(os.environ['SERVER_NAME'] + '/submit/literature/create')
+    selenium.find_element_by_id("skipImportData").click()
+    WebDriverWait(selenium, 10).until(EC.text_to_be_present_in_element((By.ID, "form_container"), 'Type of Document'))
+    field = selenium.find_element_by_id('authors-0-affiliation')
+    field.send_keys("oxf")
+    WebDriverWait(selenium, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "tt-suggestions")))
+    field.send_keys(Keys.DOWN)
+    field.send_keys(Keys.ENTER)
+    assert 'Oxford U.' in field.get_attribute('value')
+
+
 # Form Tests
 def test_literature_create_article_journal_manually(selenium, login):
     """Submit the form for article creation from scratch"""
