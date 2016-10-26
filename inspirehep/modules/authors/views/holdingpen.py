503c4e6c095555399cbe700e8ed1da8e080a8678
Riccardo Candido <riccardocandido@gmail.com>
2016-10-26 10:34:02 +0200

    authors: fix key error in workflow comments field

--- inspirehep/modules/authors/views/holdingpen.py
+++ inspirehep/modules/authors/views/holdingpen.py
@@ -319,7 +319,7 @@ def newreview():
 
     # Converting json to populate form
     convert_for_form(workflow_metadata)
-    workflow_metadata['comments'] = workflow_metadata['_private_note']
+    workflow_metadata['comments'] = workflow_metadata.get('_private_note')
     research_fields = workflow_metadata.pop('research_field')
     final_research_fields = []
     for field in research_fields:
