2c54764afa776d12a69c44312c493a1049ea250d
Riccardo Candido <riccardocandido@gmail.com>
2016-11-02 14:33:45 +0100

    tests: refactor tests for the holding panel pages

--- tests/acceptance/test_literature_suggestion_form.py
+++ tests/acceptance/test_literature_suggestion_form.py
@@ -23,6 +23,8 @@
 from __future__ import absolute_import, division, print_function
 
 from bat_framework.pages import create_literature
+from bat_framework.pages import holding_panel_list
+from bat_framework.pages import holding_panel_detail
 
 
 # Components Tests
@@ -55,6 +57,7 @@ def test_literature_create_thesis_manually(login):
         'extra-comments': 'comments about the document'
         }
     assert 'The INSPIRE staff will review it and your changes will be added to INSPIRE.' in create_literature.submit_thesis(input_data)
+    _check_back_office(input_data)
 
 
 def test_literature_create_article_journal_manually(login):
@@ -86,6 +89,7 @@ def test_literature_create_article_journal_manually(login):
         'extra-comments': 'comments about the document'
         }
     assert 'The INSPIRE staff will review it and your changes will be added to INSPIRE.' in create_literature.submit_journal_article(input_data)
+    _check_back_office(input_data)
 
 
 def test_literature_create_article_journal_with_proceeding_manually(login):
@@ -118,6 +122,31 @@ def test_literature_create_article_journal_with_proceeding_manually(login):
         'extra-comments': 'comments about the document'
         }
     assert 'The INSPIRE staff will review it and your changes will be added to INSPIRE.' in create_literature.submit_journal_article_with_proceeding(input_data)
+    _check_back_office(input_data)
+
+
+def _check_back_office(input_data):
+    holding_panel_list.go_to()
+    record = holding_panel_list.load_submission_record(input_data)
+    assert 'Computing' in record
+    assert 'Accelerators' in record
+    assert 'My Title For Test' in record
+    assert 'admin@inspirehep.net' in record
+    assert 'Mister White; Mister Brown' in record
+    assert 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr.' in record
+    holding_panel_detail.go_to()
+    record = holding_panel_detail.load_submitted_record(input_data)
+    assert 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr.' in record
+    assert 'Submitted by admin@inspirehep.net\non' in record
+    assert 'Wisconsin U., Madison' in record
+    assert 'My Title For Test' in record
+    assert 'Mister Brown' in record
+    assert 'Mister White' in record
+    assert 'Accelerators' in record
+    assert 'Computing' in record
+    assert 'CERN' in record
+
+    assert 'Accepted as Non-CORE' in holding_panel_detail.accept_record()
 
 
 def test_pdf_link(login):
