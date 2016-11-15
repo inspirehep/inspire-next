c4eb622aaf3694939676fbd9979300eb93a5cfe4
rikirenz <riccardocandido@gmail.com>
2016-11-15 11:29:02 +0100

    tasks: add control for duplicate notes

---
+++ tests/unit/workflows/test_workflows_tasks_submission.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of INSPIRE.
+# Copyright (C) 2016 CERN.
+#
+# INSPIRE is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# INSPIRE is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with INSPIRE. If not, see <http://www.gnu.org/licenses/>.
+#
+# In applying this licence, CERN does not waive the privileges and immunities
+# granted to it by virtue of its status as an Intergovernmental Organization
+# or submit itself to any jurisdiction.
+
+from __future__ import absolute_import, division, print_function
+
+from inspirehep.modules.workflows.tasks.submission import add_note_entry
+
+
+class StubObj(object):
+    def __init__(self, data, extra_data):
+        self.data = data
+        self.extra_data = extra_data
+
+
+class DummyEng(object):
+    pass
+
+
+def test_add_note_entry_does_not_add_value_that_is_already_present():
+    obj = StubObj({'public_notes': [{'value': '*Temporary entry*'}]}, {'core': 'something'})
+    eng = DummyEng()
+
+    assert add_note_entry(obj, eng) is None
+    assert {'public_notes': [{'value': '*Temporary entry*'}]} == obj.data
