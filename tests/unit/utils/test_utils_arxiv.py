cdb2052b5d5215b0acc384371f71784e5374b83f
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2016-11-16 16:58:15 +0100

    tests: refactor tests in utils/arxiv
    Signed-off-by: Jacopo Notarstefano <jacopo.notarstefano@gmail.com>

--- tests/unit/utils/test_utils_arxiv.py
+++ tests/unit/utils/test_utils_arxiv.py
@@ -22,46 +22,61 @@
 
 from __future__ import absolute_import, division, print_function
 
-import pytest
-
 from inspirehep.utils.arxiv import get_clean_arXiv_id
 
 
-@pytest.fixture
-def arxiv_record():
-    return {
-        "arxiv_eprints": [
-            {
-                "value": "arXiv:1002.2647",
-            }
-        ]
-    }
-
-
-@pytest.fixture
-def arxiv_record_old():
-    return {
-        "arxiv_eprints": [
-            {
-                "value": "physics/0112006",
-            }
-        ]
-    }
-
-
-@pytest.fixture
-def arxiv_record_oai():
-    return {
-        "arxiv_eprints": [
-            {
-                "value": "oai:arXiv.org:physics/0112006",
-            }
-        ]
-    }
-
-
-def test_arxiv_id_getter(arxiv_record, arxiv_record_old, arxiv_record_oai):
-    assert "1002.2647" == get_clean_arXiv_id(arxiv_record)
-    assert "physics/0112006" == get_clean_arXiv_id(arxiv_record_old)
-    assert "physics/0112006" == get_clean_arXiv_id(arxiv_record_oai)
+def test_get_clean_arXiv_id_returns_none_when_no_arxiv_eprints():
     assert get_clean_arXiv_id({}) is None
+
+
+def test_get_clean_arXiv_id_from_arxiv_eprints_using_new_style():
+    record = {
+        'arxiv_eprints': [
+            {'value': 'arxiv:1002.2647'},
+        ],
+    }
+
+    expected = '1002.2647'
+    result = get_clean_arXiv_id(record)
+
+    assert expected == result
+
+
+def test_get_clean_arXiv_id_from_arxiv_eprints_using_old_style():
+    record = {
+        'arxiv_eprints': [
+            {'value': 'physics/0112006'},
+        ],
+    }
+
+    expected = 'physics/0112006'
+    result = get_clean_arXiv_id(record)
+
+    assert expected == result
+
+
+def test_get_clean_arXiv_id_from_arxiv_eprints_with_oai_prefix():
+    record = {
+        'arxiv_eprints': [
+            {'value': 'oai:arXiv.org:physics/0112006'},
+        ],
+    }
+
+    expected = 'physics/0112006'
+    result = get_clean_arXiv_id(record)
+
+    assert expected == result
+
+
+def test_get_clean_arXiv_id_from_arxiv_eprints_selects_last():
+    record = {
+        'arxiv_eprints': [
+            {'value': 'oai:arXiv.org:0801.4782'},
+            {'value': 'oai:arXiv.org:0805.1410'},
+        ],
+    }
+
+    expected = '0805.1410'
+    result = get_clean_arXiv_id(record)
+
+    assert expected == result
