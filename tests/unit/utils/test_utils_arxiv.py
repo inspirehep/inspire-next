5e5a5e2bd060a0d4475a6d778a5dd6df74b0d3a1
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2016-11-16 16:47:11 +0100

    tests: fix licenses and imports in utils/arxiv
    Signed-off-by: Jacopo Notarstefano <jacopo.notarstefano@gmail.com>

--- tests/unit/utils/test_utils_arxiv.py
+++ tests/unit/utils/test_utils_arxiv.py
@@ -3,21 +3,24 @@
 # This file is part of INSPIRE.
 # Copyright (C) 2016 CERN.
 #
-# INSPIRE is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License as
-# published by the Free Software Foundation; either version 2 of the
-# License, or (at your option) any later version.
+# INSPIRE is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
-# INSPIRE is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# General Public License for more details.
+# INSPIRE is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with INSPIRE; if not, write to the Free Software Foundation, Inc.,
-# 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+# along with INSPIRE. If not, see <http://www.gnu.org/licenses/>.
+#
+# In applying this licence, CERN does not waive the privileges and immunities
+# granted to it by virtue of its status as an Intergovernmental Organization
+# or submit itself to any jurisdiction.
 
-"""Tests for arXiv utilities."""
+from __future__ import absolute_import, division, print_function
 
 import pytest
 
@@ -58,7 +61,6 @@ def arxiv_record_oai():
 
 
 def test_arxiv_id_getter(arxiv_record, arxiv_record_old, arxiv_record_oai):
-    """Test retrieval of arXiv ID."""
     assert "1002.2647" == get_clean_arXiv_id(arxiv_record)
     assert "physics/0112006" == get_clean_arXiv_id(arxiv_record_old)
     assert "physics/0112006" == get_clean_arXiv_id(arxiv_record_oai)
