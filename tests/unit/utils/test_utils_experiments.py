ccc71fe4ee76ee4a4c230840cedefa451c2b48c5
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2016-11-26 01:25:12 +0100

    tests: tests for conferences and experiments utils
    Signed-off-by: Jacopo Notarstefano <jacopo.notarstefano@gmail.com>

---
+++ tests/unit/utils/test_utils_experiments.py
@@ -0,0 +1,105 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of INSPIRE.
+# Copyright (C) 2016 CERN.
+#
+# INSPIRE is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# INSPIRE is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with INSPIRE. If not, see <http://www.gnu.org/licenses/>.
+#
+# In applying this licence, CERN does not waive the privileges and immunities
+# granted to it by virtue of its status as an Intergovernmental Organization
+# or submit itself to any jurisdiction.
+
+from __future__ import absolute_import, division, print_function
+
+from elasticsearch_dsl.result import Response
+
+from inspirehep.utils.experiments import render_people, render_contributions
+
+
+def test_render_people():
+    hits = Response({
+        'hits': {
+            'hits': [
+                {
+                    '_type': 'authors',
+                    '_source': {
+                        'control_number': 1,
+                        'name': {
+                            'preferred_name': 'preferred_name',
+                        },
+                    },
+                },
+            ],
+            'total': 1,
+        },
+    }).hits
+
+    expected = ([
+        [
+            "<a href='/authors/1'>preferred_name</a>",
+        ],
+    ], 1)
+    result = render_people(hits)
+
+    assert expected == result
+
+
+def test_render_contributions():
+    hits = Response({
+        'hits': {
+            'hits': [
+                {
+                    '_type': 'hep',
+                    '_source': {
+                        'citation_count': 1,
+                        'control_number': 1,
+                        'publication_info': [
+                            {'journal_title': 'first-journal_title'},
+                        ],
+                        'titles': [
+                            {'title': 'first-title'},
+                        ],
+                    },
+                },
+                {
+                    '_type': 'hep',
+                    '_source': {
+                        'control_number': 2,
+                        'titles': [
+                            {'title': 'second-title'},
+                        ],
+                    },
+                },
+            ],
+            'total': 2,
+        },
+    }).hits
+
+    expected = ([
+        [
+            "<a href='/literature/1'>first-title</a>",
+            u'\n  \n  \n\n\n  \n',
+            'first-journal_title',
+            1,
+        ],
+        [
+            "<a href='/literature/2'>second-title</a>",
+            u'\n  \n  \n\n\n  \n',
+            '',
+            0,
+        ],
+    ], 2)
+    result = render_contributions(hits)
+
+    assert expected == result
