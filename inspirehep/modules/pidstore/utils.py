4d0d49dc0c74f6e7453cb71e02b5d9b63354a27f
Spiros Delviniotis <spyridon.delviniotis@cern.ch>
2016-11-29 17:18:54 +0100

    pidstore: add get_index_from_endpoint util
    Signed-off-by: Spiros Delviniotis <spyridon.delviniotis@cern.ch>
    Co-authored-by: Jacopo Notarstefano <jacopo.notarstefano@cern.ch>

--- inspirehep/modules/pidstore/utils.py
+++ inspirehep/modules/pidstore/utils.py
@@ -53,6 +53,21 @@ def get_endpoint_from_schema(schema):
     return endpoint
 
 
+def get_index_from_endpoint(endpoint):
+    """Return the index corresponding to an endpoint."""
+    ENDPOINT_TO_INDEX = {
+        'authors': 'records-authors',
+        'conferences': 'records-conferences',
+        'experiments': 'records-experiments',
+        'institutions': 'records-institutions',
+        'jobs': 'records-jobs',
+        'journals': 'records-journals',
+        'literature': 'records-hep',
+    }
+
+    return ENDPOINT_TO_INDEX[endpoint]
+
+
 def get_pid_type_from_endpoint(endpoint):
     """Return the ``pid_type`` corresponding to an endpoint."""
     ENDPOINT_TO_PID_TYPE = {
