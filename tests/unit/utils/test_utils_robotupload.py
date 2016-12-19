df283182c86cd1c7b1bda24ed7ba9f60791f1996
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2016-12-19 12:44:17 +0100

    tests: rewrite tests for utils/robotupload
    Signed-off-by: Jacopo Notarstefano <jacopo.notarstefano@gmail.com>

--- tests/unit/utils/test_utils_robotupload.py
+++ tests/unit/utils/test_utils_robotupload.py
@@ -1,107 +1,69 @@
 # -*- coding: utf-8 -*-
 #
 # This file is part of INSPIRE.
-# Copyright (C) 2014, 2015 CERN.
+# Copyright (C) 2016 CERN.
 #
-# INSPIRE is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License as
-# published by the Free Software Foundation; either version 2 of the
-# License, or (at your option) any later version.
+# INSPIRE is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
-# INSPIRE is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# General Public License for more details.
+# INSPIRE is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with INSPIRE; if not, write to the Free Software Foundation, Inc.,
-# 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+# along with INSPIRE. If not, see <http://www.gnu.org/licenses/>.
+#
+# In applying this licence, CERN does not waive the privileges and immunities
+# granted to it by virtue of its status as an Intergovernmental Organization
+# or submit itself to any jurisdiction.
 
-"""Tests for robotupload."""
-
-from __future__ import print_function, absolute_import
+from __future__ import absolute_import, division, print_function
 
 import httpretty
+import pytest
+from mock import patch
 
 from inspirehep.utils.robotupload import make_robotupload_marcxml
 
 
-@httpretty.activate
-def test_robotupload_bad_xml():
-    """Test proper handling when bad MARCXML is sent."""
+@pytest.mark.httpretty
+def test_make_robotupload_marcxml():
+    httpretty.HTTPretty.allow_net_connect = False
     httpretty.register_uri(
-        httpretty.POST,
-        "http://localhost:4000/batchuploader/robotupload/insert",
-        body="[ERROR] MARCXML is not valid.\n",
-        status=400
-    )
-    invalid_marcxml = "record></record>"
-    response = make_robotupload_marcxml(
-        "http://localhost:4000",
-        invalid_marcxml,
-        mode="insert",
-    )
-    assert response.status_code == 400
-    assert "not valid" in response.text
+        httpretty.POST, 'http://localhost:5000/batchuploader/robotupload/insert')
+
+    make_robotupload_marcxml(
+        'http://localhost:5000', '<record></record>', 'insert')
+
+    httpretty.HTTPretty.allow_net_connect = True
 
 
-@httpretty.activate
-def test_robotupload_success():
-    """Test proper handling when good MARCXML is sent."""
+@pytest.mark.httpretty
+def test_make_robotupload_marcxml_falls_back_to_config_when_url_is_none(app):
+    httpretty.HTTPretty.allow_net_connect = False
     httpretty.register_uri(
-        httpretty.POST,
-        "http://localhost:4000/batchuploader/robotupload/insert",
-        body="[INFO] bibupload batchupload --insert /dummy/file/path\n",
-        status=200
-    )
-    valid_marcxml = "<record></record>"
-    response = make_robotupload_marcxml(
-        "http://localhost:4000",
-        valid_marcxml,
-        mode="insert",
-    )
-    assert response.status_code == 200
-    assert "[INFO] bibupload batchupload" in response.text
+        httpretty.POST, 'http://inspirehep.net/batchuploader/robotupload/insert')
+
+    config = {'LEGACY_ROBOTUPLOAD_URL': 'http://inspirehep.net'}
+
+    with app.app_context():
+        with patch.dict(app.config, config):
+            make_robotupload_marcxml(None, '<record></record>', 'insert')
+
+    httpretty.HTTPretty.allow_net_connect = True
 
 
-@httpretty.activate
-def test_robotupload_success_append():
-    """Test proper handling when good MARCXML is sent."""
+@pytest.mark.httpretty
+def test_make_robotupload_marcxml_raises_when_url_is_none_and_config_is_empty():
+    httpretty.HTTPretty.allow_net_connect = False
     httpretty.register_uri(
-        httpretty.POST,
-        "http://localhost:4000/batchuploader/robotupload/append",
-        body="[INFO] bibupload batchupload --append /dummy/file/path\n",
-        status=200
-    )
-    valid_marcxml = "<record></record>"
-    response = make_robotupload_marcxml(
-        "http://localhost:4000",
-        valid_marcxml,
-        mode="append",
-    )
-    assert response.status_code == 200
-    assert "[INFO] bibupload batchupload" in response.text
+        httpretty.POST, 'http://localhost:5000/batchuploader/robotupload/insert')
 
+    with pytest.raises(ValueError) as excinfo:
+        make_robotupload_marcxml(None, '<record></record>', 'insert')
+    assert 'LEGACY_ROBOTUPLOAD_URL' in str(excinfo.value)
 
-@httpretty.activate
-def test_robotupload_callback_url():
-    """Test passing of a callback URL."""
-    body = (
-        "[INFO] bibupload batchupload --insert /some/path"
-        "--callback-url http://localhost"
-    )
-    httpretty.register_uri(
-        httpretty.POST,
-        "http://localhost:4000/batchuploader/robotupload/insert",
-        body=body,
-        status=200
-    )
-    valid_marcxml = "<record></record>"
-    response = make_robotupload_marcxml(
-        "http://localhost:4000",
-        valid_marcxml,
-        mode="insert",
-        callback_url="http://localhost",
-    )
-    assert response.status_code == 200
-    assert "--callback-url http://localhost" in response.text
+    httpretty.HTTPretty.allow_net_connect = True
