10358c467dc1b9707f44227f97ae96996663cfd8
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2017-01-06 00:46:52 +0100

    records: update links for merged records
    Signed-off-by: Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
    Co-authored-by: Spiros Delviniotis <spyridon.delviniotis@cern.ch>

--- tests/integration/test_record.py
+++ tests/integration/test_record.py
@@ -28,10 +28,13 @@ from elasticsearch import RequestError
 from dojson.contrib.marc21.utils import create_record
 from invenio_db import db
 from invenio_pidstore.models import PersistentIdentifier, Redirect
+from invenio_search import current_search_client as es
 
 from inspirehep.dojson.hep import hep
 from inspirehep.modules.records.api import InspireRecord
+from inspirehep.modules.records.tasks import update_refs
 from inspirehep.modules.migrator.tasks.records import record_upsert
+from inspirehep.utils.record import get_value
 from inspirehep.utils.record_getter import get_db_record, get_es_records
 
 
@@ -180,6 +183,54 @@ def not_yet_merged_records(app):
         _delete_merged_records('lit', 111, 222, merged_uuid, deleted_uuid)
 
 
+@pytest.fixture(scope='function')
+def records_to_be_merged(app):
+    merged_record = {
+        '$schema': 'http://localhost:5000/schemas/records/hep.json',
+        'control_number': 111,
+        'self': {
+            '$ref': 'http://localhost:5000/api/literature/111',
+        },
+    }
+
+    deleted_record = {
+        '$schema': 'http://localhost:5000/schemas/records/hep.json',
+        'control_number': 222,
+        'self': {
+            '$ref': 'http://localhost:5000/api/literature/222',
+        },
+    }
+
+    pointing_record = {
+        '$schema': 'http://localhost:5000/schemas/records/hep.json',
+        'accelerator_experiments': [
+            {
+                'record': {
+                    '$ref': 'http://localhost:5000/api/literature/222',
+                },
+            },
+        ],
+        'control_number': 333,
+        'self': {
+            '$ref': 'http://localhost:5000/api/literature/333',
+        },
+    }
+
+    with app.app_context():
+        with db.session.begin_nested():
+            merged_uuid = record_upsert(merged_record).id
+            deleted_uuid = record_upsert(deleted_record).id
+            record_upsert(pointing_record)
+        db.session.commit()
+    es.indices.refresh('records-hep')
+
+    yield
+
+    with app.app_context():
+        _delete_merged_records('lit', 111, 222, merged_uuid, deleted_uuid)
+        _delete_record('lit', 333)
+
+
 def test_deleted_record_stays_deleted(app, deleted_record):
     with app.test_client() as client:
         assert client.get('/api/literature/111').status_code == 410
@@ -218,6 +269,24 @@ def test_records_can_be_merged(app, not_yet_merged_records):
     with app.test_client() as client:
         assert client.get('/api/literature/111').status_code == 200
         assert client.get('/api/literature/222').status_code == 301
+
+
+def test_references_can_be_updated(app, records_to_be_merged):
+    merged_record = get_db_record('lit', 111)
+    deleted_record = get_db_record('lit', 222)
+
+    deleted_record.merge(merged_record)
+    update_refs.delay(
+        'http://localhost:5000/api/literature/222',
+        'http://localhost:5000/api/literature/111')
+
+    pointing_record = get_db_record('lit', 333)
+
+    expected = 'http://localhost:5000/api/literature/111'
+    result = get_value(
+        pointing_record, 'accelerator_experiments[0].record.$ref')
+
+    assert expected == result
 
 
 def test_get_es_records_raises_on_empty_list(app):
