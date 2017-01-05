5311f6b479081460018cf82ed1cadcacf2ef382b
rikirenz <riccardocandido@gmail.com>
2017-01-05 15:30:54 +0100

    test: add test for prepare_file function

--- tests/unit/workflows/test_workflows_tasks_submission.py
+++ tests/unit/workflows/test_workflows_tasks_submission.py
@@ -22,7 +22,13 @@
 
 from __future__ import absolute_import, division, print_function
 
-from inspirehep.modules.workflows.tasks.submission import add_note_entry
+from mock import MagicMock
+import mock
+
+from inspirehep.modules.workflows.tasks.submission import (
+    add_note_entry,
+    prepare_files,
+)
 
 
 class StubObj(object):
@@ -35,9 +41,65 @@ class DummyEng(object):
     pass
 
 
+class MockFilesCollectionIterator(object):
+    def __init__(self, data):
+        self.data = data
+
+    @property
+    def keys(self):
+        return self.data.keys()
+
+    def __getitem__(self, item):
+        return self.data[item]
+
+
 def test_add_note_entry_does_not_add_value_that_is_already_present():
     obj = StubObj({'public_notes': [{'value': '*Temporary entry*'}]}, {'core': 'something'})
     eng = DummyEng()
 
     assert add_note_entry(obj, eng) is None
     assert {'public_notes': [{'value': '*Temporary entry*'}]} == obj.data
+
+
+@mock.patch('inspirehep.modules.workflows.tasks.submission.is_arxiv_paper')
+def test_prepare_file(fake_is_arxiv_paper):
+    eng = DummyEng()
+    mock_file = MagicMock()
+    files_collection = MagicMock()
+    fake_is_arxiv_paper.return_value = True
+
+    mock_file.name = MagicMock(return_value='fake_record')
+    mock_file.obj.file.uri = 'fake_uri'
+
+    files_collection.data = {}
+    files_collection.files = MockFilesCollectionIterator({
+        'fake_record.pdf': mock_file
+    })
+
+    assert prepare_files(files_collection, eng) is None
+    assert [{'url': '/code/fake_uri',
+            'filetype': '.pdf',
+            'docfile_type': 'INSPIRE-PUBLIC',
+            'filename': 'arxiv:fake_record'}] == files_collection.data['fft']
+
+
+@mock.patch('inspirehep.modules.workflows.tasks.submission.is_arxiv_paper')
+def test_prepare_file_wrong(fake_is_arxiv_paper):
+    eng = DummyEng()
+    mock_file = MagicMock()
+    files_collection = MagicMock()
+    fake_is_arxiv_paper.return_value = True
+
+    mock_file.name = MagicMock(return_value='fake_record')
+    mock_file.obj.file.uri = 'fake_uri'
+
+    files_collection.data = {}
+    files_collection.files = MockFilesCollectionIterator({
+        'fake_record.txt': mock_file,
+        'fake_record': mock_file,
+        '': mock_file,
+        'fake_record1.pdf': None,
+    })
+
+    assert prepare_files(files_collection, eng) is None
+    assert files_collection.data['fft'] == []
