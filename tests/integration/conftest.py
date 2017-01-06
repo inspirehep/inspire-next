08c597db188e7744aae90b4ce4db34a69b7321e2
Spiros Delviniotis <spyridon.delviniotis@cern.ch>
2017-01-06 15:55:05 +0100

    tests: set celery eager mode in integration tests
    Signed-off-by: Spiros Delviniotis <spyridon.delviniotis@cern.ch>

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -38,12 +38,14 @@ from inspirehep.factory import create_app
 @pytest.fixture(scope='session')
 def app():
     """Flask application fixture."""
-    app = create_app()
-    app.config.update(
-        {
-            'DEBUG': True,
-            'WTF_CSRF_ENABLED': False
-        }
+    app = create_app(
+        DEBUG=True,
+        WTF_CSRF_ENABLED=False,
+        CELERY_ALWAYS_EAGER=True,
+        CELERY_RESULT_BACKEND='cache',
+        CELERY_CACHE_BACKEND='memory',
+        CELERY_EAGER_PROPAGATES_EXCEPTIONS=True,
+        TESTING=True,
     )
 
     with app.app_context():
