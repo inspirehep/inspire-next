a2bb208ad2812b4dc3e76090ca08beef3a57845b
rikirenz <riccardocandido@gmail.com>
2017-02-28 10:31:52 +0100

    tests: updated holdingpen url for acceptance tests
    Signed-off-by: Riccardo Candido <riccardo.candido@gmail.com>

--- inspirehep/bat/pages/holding_panel_literature_list.py
+++ inspirehep/bat/pages/holding_panel_literature_list.py
@@ -36,7 +36,7 @@ from ..arsenic import Arsenic, ArsenicResponse
 
 
 def go_to():
-    Arsenic().get(os.environ['SERVER_NAME'] + '/holdingpen/list/?page=1&size=10&source=submission&workflow_name=HEP&status=HALTED')
+    Arsenic().get(os.environ['SERVER_NAME'] + '/holdingpen/list/?page=1&size=10&status=HALTED')
 
 
 def click_first_record():
