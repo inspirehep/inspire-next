416cb64ab767ed333f43bbf14cfed462c9d615fa
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2017-05-08 07:30:35 +0200

    global: fix rogue licenses
    Signed-off-by: Jacopo Notarstefano <jacopo.notarstefano@gmail.com>

--- inspirehep/modules/pidstore/fetchers.py
+++ inspirehep/modules/pidstore/fetchers.py
@@ -19,8 +19,6 @@
 # In applying this license, CERN does not waive the privileges and immunities
 # granted to it by virtue of its status as an Intergovernmental Organization
 # or submit itself to any jurisdiction.
-# waive the privileges and immunities granted to it by virtue of its status
-# as an Intergovernmental Organization or submit itself to any jurisdiction.
 
 """Persistent identifier minters."""
 
