5c80a9df4f7e9d82e9c5052e8e566ff2a3882fc2
Szymon Łopaciuk <szymon.lopaciuk@cern.ch>
2017-10-12 17:01:27 +0200

    orcid: created inspire literature to orcid work record converter
    * Works on top of two classes. The converter takes out fields
      from INSPIRE record and passes them to a builder which
      is responsible for building a correct ORCID record based
      on the information given.
    
    Signed-off-by: Szymon Łopaciuk <szymon.lopaciuk@cern.ch>
    Co-authored-by: Dinika Saxena <dinika.saxena@cern.ch>

---
+++ inspirehep/modules/orcid/converter.py
@@ -0,0 +1,246 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of INSPIRE.
+# Copyright (C) 2014-2017 CERN.
+#
+# INSPIRE is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# INSPIRE is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with INSPIRE. If not, see <http://www.gnu.org/licenses/>.
+#
+# In applying this license, CERN does not waive the privileges and immunities
+# granted to it by virtue of its status as an Intergovernmental Organization
+# or submit itself to any jurisdiction.
+
+"""Handle conversion from INSPIRE records to ORCID."""
+
+from __future__ import absolute_import, division, print_function
+
+import re
+
+from inspire_utils.date import PartialDate
+from inspire_utils.record import get_value
+
+from inspirehep.modules.hal.utils import get_journal_title, \
+    get_publication_date, get_conference_title, get_conference_record, \
+    get_conference_country, get_doi
+
+from .builder import OrcidBuilder
+
+
+class OrcidConverter(object):
+    """Coverter for the Orcid format."""
+
+    # Maps INSPIRE author roles to ORCID contributor roles
+    # for full list see: https://git.io/vdKXv#L235-L245
+    INSPIRE_TO_ORCID_ROLES_MAP = {
+        'author': 'author',
+        'editor': 'editor',
+        'supervisor': None
+    }
+
+    # Maps INSPIRE document type to ORCID work types
+    # for full list see: https://git.io/vdKXv#L118-L155
+    INSPIRE_DOCTYPE_TO_ORCID_TYPE = {
+        'activity report': 'report',
+        'article': 'journal-article',
+        'book': 'book',
+        'book chapter': 'book-chapter',
+        'conference paper': 'conference-paper',
+        'note': 'other',
+        'proceedings': 'edited-book',
+        'report': 'report',
+        'thesis': 'dissertation',
+    }
+
+    def __init__(self, record, server_name, put_code=None,
+                 visibility=None, bibtex_citation=None):
+        """Constructor.
+
+        Args:
+            record (dict): a record.
+        """
+        self.record = record
+        self.put_code = put_code
+        self.visibility = visibility
+        self.server_name = server_name
+        self.bibtex_citation = bibtex_citation
+
+    def get_xml(self):
+        """Create an ORCID XML representation of the record.
+
+        Returns:
+            lxml.etree._Element: ORCID XML work record
+        """
+        builder = OrcidBuilder()
+
+        # Set attributes
+        if self.visibility:
+            builder.set_visibility(self.visibility)
+
+        if self.put_code:
+            builder.set_put_code(self.put_code)
+
+        # Add a title
+        if self.title:
+            builder.add_title(self.title, self.subtitle, self.title_translation)
+
+        # Add a journal title
+        containing_publication_title = self.journal_title or self.conference_title or self.book_series_title
+        if containing_publication_title:
+            builder.add_journal_title(containing_publication_title)
+
+        # Add a citation
+        if self.bibtex_citation is not None:
+            builder.add_citation('bibtex', self.bibtex_citation)
+
+        # Add a type
+        builder.add_type(self.orcid_work_type)
+
+        # Add a publication date
+        if self.publication_date:
+            builder.add_publication_date(self.publication_date)
+
+        # Add external IDs
+        if self.doi:
+            builder.add_doi(self.doi, 'self')
+
+        if self.arxiv_eprint:
+            builder.add_arxiv(self.arxiv_eprint, 'self')
+
+        for isbn in get_value(self.record, 'isbns.value', []):
+            builder.add_external_id('isbn', isbn)
+
+        # Add URL pointing to INSPIRE to ORCID
+        if not re.match('^https?://', self.server_name):
+            self.server_name = 'http://{}'.format(self.server_name)
+        builder.add_url('{}/record/{}'.format(self.server_name, self.recid))
+
+        # Add authors/editors/etc. to the ORCID record
+        for author in self.record.get('authors', []):
+            orcid_role = self.orcid_role_for_inspire_author(author)
+            if not orcid_role:
+                continue
+            person_orcid = self.orcid_for_inspire_author(author)
+            email = get_value(author, 'emails[0]')
+            builder.add_contributor(author['full_name'], orcid_role, person_orcid, email)
+
+        # Add a country (only available for conferences)
+        if self.conference_country:
+            builder.add_country(self.conference_country)
+
+        return builder.get_xml()
+
+    def orcid_role_for_inspire_author(self, author):
+        """ORCID role for an INSPIRE author field.
+
+        Args:
+            author (dict): an author field from INSPIRE literature record
+
+        Returns:
+            string: ORCID role of a person
+        """
+        inspire_roles = sorted(get_value(author, 'inspire_roles', ['author']))
+        if inspire_roles:
+            return self.INSPIRE_TO_ORCID_ROLES_MAP[inspire_roles[0]]
+
+    def orcid_for_inspire_author(self, author):
+        """ORCID identifier for an INSPIRE author field.
+
+        Args:
+            author (dict): an author field from INSPIRE literature record
+
+        Returns:
+            string: ORCID identifier of an author, if available
+        """
+        ids = author.get('ids', [])
+        for id in ids:
+            if id['schema'] == 'ORCID':
+                return id['value']
+
+    @property
+    def orcid_work_type(self):
+        """Get record's ORCID work type."""
+        inspire_doc_type = get_value(self.record, 'document_type[0]')
+        return self.INSPIRE_DOCTYPE_TO_ORCID_TYPE[inspire_doc_type]
+
+    @property
+    def title(self):
+        """Get record title."""
+        return get_value(self.record, 'titles[0].title')
+
+    @property
+    def subtitle(self):
+        """Get record subtitle."""
+        return get_value(self.record, 'titles[0].subtitle')
+
+    @property
+    def journal_title(self):
+        """Get record's journal title."""
+        return get_journal_title(self.record)
+
+    @property
+    def conference_title(self):
+        """Get record's conference title."""
+        try:
+            conference_record = get_conference_record(self.record)
+            return get_conference_title(conference_record)
+        except TypeError:  # TODO: Fixed in the bibtex PR
+            pass
+
+    @property
+    def book_series_title(self):
+        """Get record's book series title."""
+        return get_value(self.record, 'book_series[0].title')
+
+    @property
+    def conference_country(self):
+        """Get conference record country."""
+        return get_conference_country(self.record)
+
+    @property
+    def doi(self):
+        """Get DOI of a record."""
+        return get_doi(self.record)
+
+    @property
+    def arxiv_eprint(self):
+        """Get arXiv ID of a record."""
+        return get_value(self.record, 'arxiv_eprints.value[0]')
+
+    @property
+    def recid(self):
+        """Get INSPIRE record ID."""
+        return self.record['self_recid']
+
+    @property
+    def title_translation(self):
+        """Translated title.
+
+        Returns:
+            Tuple[string, string]: translated title and the language code of the translation, if available
+        """
+        title = get_value(self.record, 'title_translations[0].title')
+        language_code = get_value(self.record, 'title_translations[0].language')
+        if title and language_code:
+            return title, language_code
+
+    @property
+    def publication_date(self):
+        """(Partial) date of publication.
+
+        Returns:
+            partial_date (inspire_utils.date.PartialDate): publication date
+        """
+        try:
+            return PartialDate.loads(get_value(self.record, 'imprints.date[0]') or get_publication_date(self.record))
+        except ValueError:
+            return None
---
+++ tests/integration/orcid/test_orcid_export.py
@@ -0,0 +1,335 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of INSPIRE.
+# Copyright (C) 2014-2017 CERN.
+#
+# INSPIRE is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# INSPIRE is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with INSPIRE. If not, see <http://www.gnu.org/licenses/>.
+#
+# In applying this license, CERN does not waive the privileges and immunities
+# granted to it by virtue of its status as an Intergovernmental Organization
+# or submit itself to any jurisdiction.
+
+from __future__ import absolute_import, division, print_function
+
+import json
+import os
+import pkg_resources
+
+from flask import current_app
+from inspirehep.modules.orcid import OrcidConverter
+from lxml import etree
+
+
+def valid_against_schema(xml):
+    """Validate xml schema."""
+    schema_path = pkg_resources.resource_filename(
+        __name__, os.path.join('fixtures', 'record_2.0', 'work-2.0.xsd')
+    )
+    schema = etree.XMLSchema(file=schema_path)
+    schema.assertValid(xml)
+    return True
+
+
+def xml_parse(xml_string):
+    """Parse an ``xml_string`` into XML."""
+    parser = etree.XMLParser(remove_blank_text=True)
+    return etree.fromstring(xml_string, parser)
+
+
+def xml_compare(expected, result):
+    """Assert two XML nodes equal."""
+    assert etree.tostring(result, pretty_print=True) == \
+        etree.tostring(expected, pretty_print=True)
+    return True
+
+
+def test_format_article(app, api_client):
+    response = api_client.get('/literature/4328')
+    assert response.status_code == 200
+    article = json.loads(response.data)
+
+    expected = xml_parse("""
+    <work:work xmlns:common="http://www.orcid.org/ns/common" xmlns:work="http://www.orcid.org/ns/work">
+        <work:title>
+            <common:title>Partial Symmetries of Weak Interactions</common:title>
+        </work:title>
+        <work:journal-title>Nucl.Phys.</work:journal-title>
+        <work:type>journal-article</work:type>
+        <common:publication-date>
+            <common:year>1961</common:year>
+        </common:publication-date>
+        <common:external-ids>
+            <common:external-id>
+                <common:external-id-type>doi</common:external-id-type>
+                <common:external-id-value>10.1016/0029-5582(61)90469-2</common:external-id-value>
+                <common:external-id-url>http://dx.doi.org/10.1016/0029-5582(61)90469-2</common:external-id-url>
+                <common:external-id-relationship>self</common:external-id-relationship>
+            </common:external-id>
+        </common:external-ids>
+        <work:url>http://localhost:5000/record/4328</work:url>
+        <work:contributors>
+            <work:contributor>
+                <work:credit-name>Glashow, S.L.</work:credit-name>
+                <work:contributor-attributes>
+                    <work:contributor-sequence>first</work:contributor-sequence>
+                    <work:contributor-role>author</work:contributor-role>
+                </work:contributor-attributes>
+            </work:contributor>
+        </work:contributors>
+    </work:work>
+    """)
+
+    result = OrcidConverter(
+        article['metadata'],
+        server_name=current_app.config['SERVER_NAME']
+    ).get_xml()
+    assert valid_against_schema(result)
+    assert xml_compare(expected, result)
+
+
+def test_format_conference_paper(app, api_client):
+    response = api_client.get('/literature/524480')
+    assert response.status_code == 200
+    inproceedings = json.loads(response.data)
+
+    expected = xml_parse("""
+    <work:work xmlns:common="http://www.orcid.org/ns/common" xmlns:work="http://www.orcid.org/ns/work">
+        <work:title>
+            <common:title>CMB anisotropies: A Decadal survey</common:title>
+        </work:title>
+        <work:journal-title>4th RESCEU International Symposium on Birth and Evolution of the Universe</work:journal-title>
+        <work:type>conference-paper</work:type>
+        <common:external-ids>
+            <common:external-id>
+                <common:external-id-type>arxiv</common:external-id-type>
+                <common:external-id-value>astro-ph/0002520</common:external-id-value>
+                <common:external-id-url>http://arxiv.org/abs/astro-ph/0002520</common:external-id-url>
+                <common:external-id-relationship>self</common:external-id-relationship>
+            </common:external-id>
+        </common:external-ids>
+        <work:url>http://localhost:5000/record/524480</work:url>
+        <work:contributors>
+            <work:contributor>
+                <work:credit-name>Hu, Wayne</work:credit-name>
+                <work:contributor-attributes>
+                    <work:contributor-sequence>first</work:contributor-sequence>
+                    <work:contributor-role>author</work:contributor-role>
+                </work:contributor-attributes>
+            </work:contributor>
+        </work:contributors>
+    </work:work>
+    """)
+
+    result = OrcidConverter(
+        inproceedings['metadata'],
+        server_name=current_app.config['SERVER_NAME']
+    ).get_xml()
+    assert valid_against_schema(result)
+    assert xml_compare(expected, result)
+
+
+def test_format_proceedings(app, api_client):
+    response = api_client.get('/literature/701585')
+    assert response.status_code == 200
+    proceedings = json.loads(response.data)
+
+    expected = xml_parse("""
+    <work:work xmlns:common="http://www.orcid.org/ns/common" xmlns:work="http://www.orcid.org/ns/work">
+        <work:title>
+            <common:title>HERA and the LHC: A Workshop on the implications of HERA for LHC physics: Proceedings Part A</common:title>
+        </work:title>
+        <work:type>edited-book</work:type>
+        <common:publication-date>
+            <common:year>2005</common:year>
+        </common:publication-date>
+        <common:external-ids>
+            <common:external-id>
+                <common:external-id-type>arxiv</common:external-id-type>
+                <common:external-id-value>hep-ph/0601012</common:external-id-value>
+                <common:external-id-url>http://arxiv.org/abs/hep-ph/0601012</common:external-id-url>
+                <common:external-id-relationship>self</common:external-id-relationship>
+            </common:external-id>
+        </common:external-ids>
+        <work:url>http://localhost:5000/record/701585</work:url>
+        <work:contributors>
+            <work:contributor>
+                <work:credit-name>De Roeck, A.</work:credit-name>
+                <work:contributor-attributes>
+                    <work:contributor-sequence>first</work:contributor-sequence>
+                    <work:contributor-role>editor</work:contributor-role>
+                </work:contributor-attributes>
+            </work:contributor>
+            <work:contributor>
+                <work:credit-name>Jung, H.</work:credit-name>
+                <work:contributor-attributes>
+                    <work:contributor-sequence>additional</work:contributor-sequence>
+                    <work:contributor-role>editor</work:contributor-role>
+                </work:contributor-attributes>
+            </work:contributor>
+        </work:contributors>
+    </work:work>
+    """)
+
+    result = OrcidConverter(
+        proceedings['metadata'],
+        server_name=current_app.config['SERVER_NAME']
+    ).get_xml()
+    assert valid_against_schema(result)
+    assert xml_compare(expected, result)
+
+
+def test_format_thesis(app, api_client):
+    response = api_client.get('/literature/1395663')
+    assert response.status_code == 200
+    phdthesis = json.loads(response.data)
+
+    expected = xml_parse("""
+    <work:work xmlns:common="http://www.orcid.org/ns/common" xmlns:work="http://www.orcid.org/ns/work">
+        <work:title>
+            <common:title>MAGIC $\\gamma$-ray observations of distant AGN and a study of source variability and the extragalactic background light using FERMI and air Cherenkov telescopes</common:title>
+        </work:title>
+        <work:type>dissertation</work:type>
+        <work:url>http://localhost:5000/record/1395663</work:url>
+        <work:contributors>
+            <work:contributor>
+                <work:credit-name>Mankuzhiyil, Nijil</work:credit-name>
+                <work:contributor-attributes>
+                    <work:contributor-sequence>first</work:contributor-sequence>
+                    <work:contributor-role>author</work:contributor-role>
+                </work:contributor-attributes>
+            </work:contributor>
+        </work:contributors>
+    </work:work>
+    """)
+
+    result = OrcidConverter(
+        phdthesis['metadata'],
+        server_name=current_app.config['SERVER_NAME']
+    ).get_xml()
+    assert valid_against_schema(result)
+    assert xml_compare(expected, result)
+
+
+def test_format_book(app, api_client):
+    response = api_client.get('/literature/736770')
+    assert response.status_code == 200
+    book = json.loads(response.data)
+
+    expected = xml_parse("""
+    <work:work xmlns:common="http://www.orcid.org/ns/common" xmlns:work="http://www.orcid.org/ns/work">
+        <work:title>
+            <common:title>Differential geometry and Lie groups for physicists</common:title>
+        </work:title>
+        <work:type>book</work:type>
+        <common:publication-date>
+            <common:year>2011</common:year>
+            <common:month>03</common:month>
+            <common:day>03</common:day>
+        </common:publication-date>
+        <common:external-ids>
+            <common:external-id>
+                <common:external-id-type>isbn</common:external-id-type>
+                <common:external-id-value>9780521187961</common:external-id-value>
+            </common:external-id>
+            <common:external-id>
+                <common:external-id-type>isbn</common:external-id-type>
+                <common:external-id-value>9780521845076</common:external-id-value>
+            </common:external-id>
+            <common:external-id>
+                <common:external-id-type>isbn</common:external-id-type>
+                <common:external-id-value>9780511242960</common:external-id-value>
+            </common:external-id>
+        </common:external-ids>
+        <work:url>http://localhost:5000/record/736770</work:url>
+        <work:contributors>
+            <work:contributor>
+                <work:credit-name>Fecko, M.</work:credit-name>
+                <work:contributor-attributes>
+                    <work:contributor-sequence>first</work:contributor-sequence>
+                    <work:contributor-role>author</work:contributor-role>
+                </work:contributor-attributes>
+            </work:contributor>
+        </work:contributors>
+    </work:work>
+    """)
+
+    result = OrcidConverter(
+        book['metadata'],
+        server_name=current_app.config['SERVER_NAME']
+    ).get_xml()
+    assert valid_against_schema(result)
+    assert xml_compare(expected, result)
+
+
+def test_format_book_chapter(app, api_client):
+    response = api_client.get('/literature/1375491')
+    assert response.status_code == 200
+    inbook = json.loads(response.data)
+
+    expected = xml_parse("""
+    <work:work xmlns:common="http://www.orcid.org/ns/common" xmlns:work="http://www.orcid.org/ns/work">
+        <work:title>
+            <common:title>Supersymmetry</common:title>
+        </work:title>
+        <work:type>book-chapter</work:type>
+        <common:publication-date>
+            <common:year>2015</common:year>
+        </common:publication-date>
+        <common:external-ids>
+            <common:external-id>
+                <common:external-id-type>doi</common:external-id-type>
+                <common:external-id-value>10.1007/978-3-319-15001-7_10</common:external-id-value>
+                <common:external-id-url>http://dx.doi.org/10.1007/978-3-319-15001-7_10</common:external-id-url>
+                <common:external-id-relationship>self</common:external-id-relationship>
+            </common:external-id>
+            <common:external-id>
+                <common:external-id-type>arxiv</common:external-id-type>
+                <common:external-id-value>1506.03091</common:external-id-value>
+                <common:external-id-url>http://arxiv.org/abs/1506.03091</common:external-id-url>
+                <common:external-id-relationship>self</common:external-id-relationship>
+            </common:external-id>
+        </common:external-ids>
+        <work:url>http://localhost:5000/record/1375491</work:url>
+        <work:contributors>
+            <work:contributor>
+                <work:credit-name>Bechtle, Philip</work:credit-name>
+                <work:contributor-attributes>
+                    <work:contributor-sequence>first</work:contributor-sequence>
+                    <work:contributor-role>author</work:contributor-role>
+                </work:contributor-attributes>
+            </work:contributor>
+            <work:contributor>
+                <work:credit-name>Plehn, Tilman</work:credit-name>
+                <work:contributor-attributes>
+                    <work:contributor-sequence>additional</work:contributor-sequence>
+                    <work:contributor-role>author</work:contributor-role>
+                </work:contributor-attributes>
+            </work:contributor>
+                <work:contributor>
+                <work:credit-name>Sander, Christian</work:credit-name>
+                <work:contributor-attributes>
+                    <work:contributor-sequence>additional</work:contributor-sequence>
+                    <work:contributor-role>author</work:contributor-role>
+                </work:contributor-attributes>
+            </work:contributor>
+        </work:contributors>
+    </work:work>
+    """)
+
+    result = OrcidConverter(
+        inbook['metadata'],
+        server_name=current_app.config['SERVER_NAME']
+    ).get_xml()
+    assert valid_against_schema(result)
+    assert xml_compare(expected, result)
