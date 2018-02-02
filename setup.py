e3ca383f08deb9c1bd74a630240ccaa2d612a327
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-02-02 23:39:02 +0100

    global: don't use pytest-flake8
    Remove ``pytest-flake8`` and replace it with just ``flake8`` as the
    former is sometimes not finding style violations present in the code.
    
    Signed-off-by: Jacopo Notarstefano <jacopo.notarstefano@gmail.com>

--- setup.py
+++ setup.py
@@ -115,7 +115,6 @@ tests_require = [
     'flake8-future-import~=0.0,>=0.4.3',
     'mock~=2.0,>=2.0.0',
     'pytest-cov~=2.0,>=2.5.1',
-    'pytest-flake8~=0.0,>=0.9',
     'pytest-selenium~=1.0,>=1.11.1',
     'pytest~=3.0,>=3.3.0',
     'requests_mock~=1.0,>=1.3.0',
