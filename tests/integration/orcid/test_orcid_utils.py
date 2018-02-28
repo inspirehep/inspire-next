eb30bdafdb4ca6b515b9e09802ea9c8a2062889e
Micha Moskovic <michamos@gmail.com>
2018-02-28 17:55:24 +0100

    orcid: also push to orcid found in author record
    Signed-off-by: Micha Moskovic <michamos@gmail.com>

--- tests/integration/orcid/test_orcid_utils.py
+++ tests/integration/orcid/test_orcid_utils.py
@@ -26,7 +26,34 @@ from __future__ import absolute_import, division, print_function
 
 import pytest
 
-from inspirehep.modules.orcid.utils import _get_api_url_for_recid
+from inspire_dojson.utils import get_record_ref
+from inspire_schemas.api import validate
+from inspirehep.modules.migrator.tasks import record_insert_or_replace
+from inspirehep.modules.orcid.utils import _get_api_url_for_recid, get_orcids_for_push
+
+
+@pytest.fixture(scope='function')
+def author_in_isolated_app(isolated_app):
+    record = {
+        '$schema': 'http://localhost:5000/schemas/records/authors.json',
+        '_collections': ['Authors'],
+        'control_number': 123456789,  # FIXME remove when there is an easy way to insert new records
+        'ids': [
+            {
+                'schema': 'INSPIRE BAI',
+                'value': 'J.Smith.1',
+            },
+            {
+                'schema': 'ORCID',
+                'value': '0000-0002-1825-0097',
+            },
+        ],
+        'name': {'value': 'Smith, John'},
+    }
+
+    assert validate(record, 'authors') is None
+
+    yield record_insert_or_replace(record)['control_number']
 
 
 @pytest.mark.parametrize(
@@ -42,3 +69,129 @@ from inspirehep.modules.orcid.utils import _get_api_url_for_recid
 def test_get_api_url_for_recid(server_name, api_endpoint, recid, expected):
     result = _get_api_url_for_recid(server_name, api_endpoint, recid)
     assert expected == result
+
+
+def test_orcids_for_push_no_authors(isolated_app):
+    record = {
+        '_collections': ['Literature'],
+        'corporate_author': ['Corporate Author'],
+        'document_type': ['article'],
+        'titles': [
+            {'title': 'A paper with no authors'},
+        ],
+    }
+
+    assert validate(record, 'hep') is None
+    assert list(get_orcids_for_push(record)) == []
+
+
+def test_orcids_for_push_no_orcids(isolated_app):
+    record = {
+        '_collections': ['Literature'],
+        'authors': [
+            {
+                'full_name': 'Smith, John',
+                'ids': [
+                    {
+                        'schema': 'INSPIRE BAI',
+                        'value': 'J.Smith.1',
+                    },
+                ],
+            },
+        ],
+        'document_type': ['article'],
+        'titles': [
+            {'title': 'An interesting paper'},
+        ],
+    }
+
+    assert validate(record, 'hep') is None
+    assert list(get_orcids_for_push(record)) == []
+
+
+def test_orcids_for_push_orcid_in_paper(isolated_app):
+    record = {
+        '_collections': ['Literature'],
+        'authors': [
+            {
+                'full_name': 'No Orcid, Jimmy',
+            },
+            {
+                'full_name': 'Smith, John',
+                'ids': [
+                    {
+                        'schema': 'INSPIRE BAI',
+                        'value': 'J.Smith.1',
+                    },
+                    {
+                        'schema': 'ORCID',
+                        'value': '0000-0002-1825-0097',
+                    },
+                ],
+            },
+        ],
+        'document_type': ['article'],
+        'titles': [
+            {'title': 'An interesting paper'},
+        ],
+    }
+
+    assert validate(record, 'hep') is None
+    assert list(get_orcids_for_push(record)) == ['0000-0002-1825-0097']
+
+
+def test_orcids_for_push_orcid_in_author_no_claim(author_in_isolated_app):
+    record = {
+        '_collections': ['Literature'],
+        'authors': [
+            {
+                'full_name': 'No Orcid, Jimmy',
+            },
+            {
+                'full_name': 'Smith, John',
+                'ids': [
+                    {
+                        'schema': 'INSPIRE BAI',
+                        'value': 'J.Smith.1',
+                    },
+                ],
+                'record': get_record_ref(author_in_isolated_app, 'authors'),
+            },
+        ],
+        'document_type': ['article'],
+        'titles': [
+            {'title': 'An interesting paper'},
+        ],
+    }
+
+    assert validate(record, 'hep') is None
+    assert list(get_orcids_for_push(record)) == []
+
+
+def test_orcids_for_push_orcid_in_author_with_claim(author_in_isolated_app):
+    record = {
+        '_collections': ['Literature'],
+        'authors': [
+            {
+                'full_name': 'No Orcid, Jimmy',
+            },
+            {
+                'full_name': 'Smith, John',
+                'ids': [
+                    {
+                        'schema': 'INSPIRE BAI',
+                        'value': 'J.Smith.1',
+                    },
+                ],
+                'record': get_record_ref(author_in_isolated_app, 'authors'),
+                'curated_relation': True,
+            },
+        ],
+        'document_type': ['article'],
+        'titles': [
+            {'title': 'An interesting paper'},
+        ],
+    }
+
+    assert validate(record, 'hep') is None
+    assert list(get_orcids_for_push(record)) == ['0000-0002-1825-0097']
