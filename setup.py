e320af3b77a1464800b4484e4621b959e9494a67
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-03-12 13:50:27 +0100

    setup: reorder install dependencies
    Signed-off-by: Jacopo Notarstefano <jacopo.notarstefano@gmail.com>

--- setup.py
+++ setup.py
@@ -54,9 +54,9 @@ install_requires = [
     'backports.tempfile>=1.0rc1',
     'beard~=0.0,>=0.2.0',
     'celery~=3.0,>=3.1.25',
-    'enum34~=1.0,>=1.1.6',
     'elasticsearch-dsl~=2.0,>=2.2.0',
     'elasticsearch~=2.0,>=2.4.1',
+    'enum34~=1.0,>=1.1.6',
     'flask-shell-ipython~=0.0,>=0.3.0',
     'fs~=0.0,>=0.5.4',
     'inspire-crawler~=2.0',
@@ -104,8 +104,8 @@ install_requires = [
     'requests~=2.0,>=2.18.4',
     'setproctitle~=1.0,>=1.1.10',
     'timeout-decorator~=0.0,>=0.4.0',
-    'workflow~=2.0,>=2.1.3',
     'vcrpy~=1.0,>=1.11.1',
+    'workflow~=2.0,>=2.1.3',
 ]
 
 docs_require = [
