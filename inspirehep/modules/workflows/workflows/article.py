715cc7af04f0612eb2ed0b3742559b43564adcc4
Salman Maqbool <salman.maqbool@cern.ch>
2018-04-05 18:55:40 +0200

    workflows: removed remove_reference from workflow

--- inspirehep/modules/workflows/workflows/article.py
+++ inspirehep/modules/workflows/workflows/article.py
@@ -92,7 +92,6 @@ from inspirehep.modules.workflows.tasks.submission import (
     create_ticket,
     filter_keywords,
     prepare_keywords,
-    remove_references,
     reply_ticket,
     send_robotupload,
     wait_webcoll,
@@ -227,7 +226,6 @@ POSTENHANCE_RECORD = [
     add_core,
     filter_keywords,
     prepare_keywords,
-    remove_references,
     set_refereed_and_fix_document_type,
     fix_submission_number,
 ]
