bb69daeb67b1012197f272e48d4c304aaccdc9d4
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-04-06 15:36:04 +0200

    workflows: refactor extension to load config
    Signed-off-by: Jacopo Notarstefano <jacopo.notarstefano@gmail.com>

--- inspirehep/modules/workflows/config.py
+++ inspirehep/modules/workflows/config.py
@@ -20,8 +20,6 @@
 # granted to it by virtue of its status as an Intergovernmental Organization
 # or submit itself to any jurisdiction.
 
-"""Workflows module."""
+"""Workflows configuration."""
 
 from __future__ import absolute_import, division, print_function
-
-from .ext import InspireWorkflows  # noqa: F401
