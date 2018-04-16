0524bf9cb97c237a6a21fabf45a02cfd9594b04c
Micha Moskovic <michamos@gmail.com>
2018-04-16 10:56:50 +0200

    alembic: merge the two heads
    Since commit 99aeb65 Alembic's INSPIRE branch has had two heads. This
    commit fixes this situation by adding an empty merge migration.
    
    Signed-off-by: Micha Moskovic <michamos@gmail.com>
    Co-authored-by: Jacopo Notarstefano <jacopo.notarstefano@gmail.com>

---
+++ inspirehep/alembic/d99c70308006_merge_two_alembic_branches.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of INSPIRE.
+# Copyright (C) 2014-2017 CERN.
+#
+# INSPIRE is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# INSPIRE is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with INSPIRE. If not, see <http://www.gnu.org/licenses/>.
+#
+# In applying this license, CERN does not waive the privileges and immunities
+# granted to it by virtue of its status as an Intergovernmental Organization
+# or submit itself to any jurisdiction.
+
+"""Merge two alembic branches."""
+
+from __future__ import absolute_import, division, print_function
+
+revision = 'd99c7038006'
+down_revision = ('cb9f81e8251c', 'cb5153afd839')
+branch_labels = ()
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
