4283c85cfd8361f0bda6a2aa3dda08d3fd31413a
Szymon Łopaciuk <szymon.lopaciuk@cern.ch>
2018-05-15 17:23:47 +0200

    set APP_BORKER_URL in test-scrapyd
    Signed-off-by: Szymon Łopaciuk <szymon.lopaciuk@cern.ch>

--- tests/e2e/test_arxiv_harvest.py
+++ tests/e2e/test_arxiv_harvest.py
@@ -42,7 +42,7 @@ def init_environment():
 def inspire_client():
     """Share the same client to reuse the same session"""
     # E2E_NS is set by k8s when running the test in Jenkins
-    inspire_url = os.environ.get('E2E_NS', 'http://test-web-e2e.local:5000')
+    inspire_url = os.environ.get('INSPIRE_API_URL', 'http://test-web-e2e.local:5000')
     return InspireApiClient(base_url=inspire_url)
 
 
