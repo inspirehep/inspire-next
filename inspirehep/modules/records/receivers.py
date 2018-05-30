1bf3d2149475727184495a5f4e514d7cf593a1bf
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-05-30 15:18:28 +0200

    records: place correctly the push_to_orcid receiver
    Signed-off-by: Jacopo Notarstefano <jacopo.notarstefano@gmail.com>

--- inspirehep/modules/records/receivers.py
+++ inspirehep/modules/records/receivers.py
@@ -111,31 +111,12 @@ def assign_uuid(sender, record, *args, **kwargs):
 
 
 #
-# models_committed
+# after_record_update
 #
-
-@models_committed.connect
-def index_after_commit(sender, changes):
-    """Index a record in ES after it was committed to the DB.
-
-    This cannot happen in an ``after_record_commit`` receiver from Invenio-Records
-    because, despite the name, at that point we are not yet sure whether the record
-    has been really committed to the DB.
-    """
-    indexer = RecordIndexer()
-
-    for model_instance, change in changes:
-        if isinstance(model_instance, RecordMetadata):
-            if change in ('insert', 'update'):
-                indexer.index(Record(model_instance.json, model_instance))
-            else:
-                indexer.delete(Record(model_instance.json, model_instance))
-
 
 @after_record_update.connect
 def push_to_orcid(sender, record, *args, **kwargs):
-    """If needed, queue the push of the new changes to ORCID.
-    """
+    """If needed, queue the push of the new changes to ORCID."""
     if not is_hep(record) or not current_app.config['FEATURE_FLAG_ENABLE_ORCID_PUSH']:
         return
 
@@ -158,6 +139,28 @@ def push_to_orcid(sender, record, *args, **kwargs):
                 'oauth_token': access_token,
             },
         )
+
+
+#
+# models_committed
+#
+
+@models_committed.connect
+def index_after_commit(sender, changes):
+    """Index a record in ES after it was committed to the DB.
+
+    This cannot happen in an ``after_record_commit`` receiver from Invenio-Records
+    because, despite the name, at that point we are not yet sure whether the record
+    has been really committed to the DB.
+    """
+    indexer = RecordIndexer()
+
+    for model_instance, change in changes:
+        if isinstance(model_instance, RecordMetadata):
+            if change in ('insert', 'update'):
+                indexer.index(Record(model_instance.json, model_instance))
+            else:
+                indexer.delete(Record(model_instance.json, model_instance))
 
 
 #
