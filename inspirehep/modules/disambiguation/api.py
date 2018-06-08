449db4eafe82bb5c9f84d69dddb50a3ad2422451
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-06-08 17:42:49 +0200

    disambiguation: refactor to use JSON Lines
    Also clarifies with appropriate names that the signatures used
    during training are only the curated ones, and that the clusters
    that are derived from them constitute the input that BEARD will
    use during clustering.
    
    Signed-off-by: Jacopo Notarstefano <jacopo.notarstefano@gmail.com>

--- inspirehep/modules/disambiguation/api.py
+++ inspirehep/modules/disambiguation/api.py
@@ -27,6 +27,7 @@ from __future__ import absolute_import, division, print_function
 import json
 from collections import defaultdict
 
+import six
 from flask import current_app
 
 from inspirehep.modules.disambiguation.core.db.readers import (
@@ -37,38 +38,50 @@ from inspirehep.modules.disambiguation.core.ml.models import EthnicityEstimator
 from inspirehep.modules.disambiguation.utils import open_file_in_folder
 
 
-def save_signatures_and_clusters():
-    """Save signatures and starting clusters to disk.
+def save_curated_signatures_and_input_clusters():
+    """Save curated signatures and input clusters to disk.
 
-    Saves two files to disk called (by default) ``clusters.json`` and ``signatures.jl``.
-    The former contains a dictionary that represents the starting clusters that are used
-    by ``BEARD`` as ground truth, while the latter contains one line per curated signature
-    present in INSPIRE.
+    Saves two files to disk called (by default) ``input_clusters.jsonl`` and
+    ``curated_signatures.jsonl``. The former contains one line per each cluster
+    initially present in INSPIRE, while the latter contains one line per each
+    curated signature that will be used as ground truth by ``BEARD``.
     """
-    clusters = defaultdict(list)
+    signatures_with_author = defaultdict(list)
+    signatures_without_author = []
 
-    with open_file_in_folder(current_app.config['DISAMBIGUATION_SIGNATURES_PATH'], 'w') as fd:
+    with open_file_in_folder(current_app.config['DISAMBIGUATION_CURATED_SIGNATURES_PATH'], 'w') as fd:
         for signature in get_all_curated_signatures():
             if signature.get('author_id'):
-                clusters[signature['author_id']].append(signature['signature_uuid'])
+                signatures_with_author[signature['author_id']].append(signature['signature_uuid'])
                 fd.write(json.dumps(signature) + '\n')
+            else:
+                signatures_without_author.append(signature['signature_uuid'])
 
-    with open_file_in_folder(current_app.config['DISAMBIGUATION_CLUSTERS_PATH'], 'w') as fd:
-        json.dump(clusters, fd)
+    with open_file_in_folder(current_app.config['DISAMBIGUATION_INPUT_CLUSTERS_PATH'], 'w') as fd:
+        for cluster_id, (author_id, signature_uuids) in enumerate(six.iteritems(signatures_with_author)):
+            fd.write(json.dumps({
+                'author_id': author_id,
+                'cluster_id': cluster_id,
+                'signature_uuids': signature_uuids,
+            }) + '\n')
+        for cluster_id, signature_uuid in enumerate(signatures_without_author, cluster_id + 1):
+            fd.write(json.dumps({
+                'author_id': None,
+                'cluster_id': cluster_id,
+                'signature_uuids': [signature_uuid],
+            }) + '\n')
 
 
 def save_publications():
     """Save publications to disk.
 
-    Saves a file to disk called (by default) ``publications.json`` which contains all
-    information that will be useful for ``BEARD`` during training and prediction.
+    Saves a file to disk called (by default) ``publications.jsonl``, which
+    contains one line per record in INSPIRE with information that will be
+    useful for ``BEARD`` during training and prediction.
     """
-    publications = {}
-    for publication in get_all_publications():
-        publications[publication['publication_id']] = publication
-
     with open_file_in_folder(current_app.config['DISAMBIGUATION_PUBLICATIONS_PATH'], 'w') as fd:
-        json.dump(publications, fd)
+        for publication in get_all_publications():
+            fd.write(json.dumps(publication) + '\n')
 
 
 def train_and_save_ethnicity_model():
