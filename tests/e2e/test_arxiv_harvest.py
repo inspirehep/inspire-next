3271d85c503995e7bdcbe58125e5e7bf1be8b72b
Antonio Cesarano <cesarano2607@gmail.com>
2018-06-20 15:35:59 +0200

    e2e: extend e2e test checking the edit workflow for jlab
    Signed-off-by: Antonio Cesarano <cesarano2607@gmail.com>

--- tests/e2e/test_arxiv_harvest.py
+++ tests/e2e/test_arxiv_harvest.py
@@ -23,7 +23,13 @@
 from __future__ import absolute_import, division, print_function
 
 import backoff
+import os
+import re
+import time
+import urllib2
+import yaml
 
+from inspirehep.testlib.api.holdingpen import HoldingpenResource
 from inspirehep.testlib.api.mitm_client import with_mitmproxy
 
 
@@ -53,6 +59,30 @@ def _all_in_status(inspire_client, status):
         )
         raise
     return hp_entries[0]
+
+
+def _workflow_in_status(inspire_client, holdingpen_id, status):
+    entry = inspire_client.holdingpen.get_detail_entry(holdingpen_id)
+    try:
+        assert entry.status == status, (
+            'Current holdingpen entries (waiting ' 'for them to be in %s status): %s'
+            % (status, entry)
+        )
+    except AssertionError:
+        raise
+    return entry
+
+
+def number_of_entries(inspire_client, num_entries):
+    hp_entries = inspire_client.holdingpen.get_list_entries()
+    try:
+        assert len(hp_entries) == num_entries, (
+            'Current holdingpen entries (waiting for them to be in %d current number): %d'
+            % (num_entries, len(hp_entries))
+        )
+    except AssertionError:
+        raise
+    return hp_entries
 
 
 @with_mitmproxy
@@ -214,3 +244,35 @@ def test_harvest_nucl_th_and_jlab_curation(inspire_client, mitm_client):
         interaction_name='ticket_new',
         times=1,
     )
+
+    def _get_ticket_content():
+        curr_path = os.path.dirname(__file__)
+        ticket_file = os.path.join(curr_path, 'scenarios', 'harvest_nucl_th_and_jlab_curation', 'RTService', 'ticket_new.yaml')
+        content = yaml.load(open(ticket_file))
+        ticket_content = urllib2.unquote(content['request']['body']).decode('utf8')
+        return re.search('\/workflows\/edit_article\/[0-9]+', ticket_content).group(0)
+
+    curation_link = _get_ticket_content()
+    assert inspire_client._client.get(curation_link).status_code == 200
+
+    new_entries = wait_for(lambda: number_of_entries(inspire_client, 2))
+    assert len(new_entries) == 2
+    edit_article_wf = filter(lambda entry: entry.status == 'WAITING', new_entries)[0]
+
+    entry = inspire_client.holdingpen.get_detail_entry(edit_article_wf.workflow_id)
+
+    def apply_changes_to_wf():
+        new_title = 'Title changed by JLab curator'
+        curated_content = entry._raw_json
+        curated_content['metadata']['titles'][0]['title'] = new_title
+        return HoldingpenResource.from_json(curated_content)
+
+    entry = apply_changes_to_wf()
+    inspire_client.holdingpen.resume_wf(entry)
+
+    entry = wait_for(lambda: _workflow_in_status(inspire_client, entry.workflow_id, 'COMPLETED'))
+
+    time.sleep(5)
+    # check literature record is available and consistent
+    record = inspire_client.literature.get_record(entry.control_number)
+    assert record.title == 'Title changed by JLab curator'
