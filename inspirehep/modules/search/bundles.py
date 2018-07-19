6dd38c0856822a4638f2024e8f0db1e278beb733
Harris Tzovanakis <me@drjova.com>
2018-07-19 10:19:07 +0200

    bump: version
    Signed-off-by: Harris Tzovanakis <me@drjova.com>

--- inspirehep/modules/search/bundles.py
+++ inspirehep/modules/search/bundles.py
@@ -34,6 +34,6 @@ js = NpmBundle(
     npm={
         'invenio-search-js': '~1.4.0',
         'angular-loading-bar': '~0.9.0',
-        'inspirehep-search-js': '~1.0.0'
+        'inspirehep-search-js': '~1.1.0'
     },
 )
