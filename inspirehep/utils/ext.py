899dabc588159dd9d45e7202692c675f073f0fe0
turtle321 <turtle321@foo.bar>
2018-12-13 15:39:44 +0100

    1776 monitoring: set queue size to 5000 for timeexecution and ignore ES hosts in vcr

--- inspirehep/utils/ext.py
+++ inspirehep/utils/ext.py
@@ -83,7 +83,7 @@ class INSPIREUtils(object):
                 backend_kwargs=dict(
                     hosts=app.config['APPMETRICS_ELASTICSEARCH_HOSTS'],
                     index=app.config['APPMETRICS_ELASTICSEARCH_INDEX']),
-                queue_maxsize=2000,
+                queue_maxsize=5000,
             )
         else:
             backend = ElasticsearchBackend(
