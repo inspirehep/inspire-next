f4767b0ff5c17f6eef6531dc96e72cd4ae8f4829
Antonio Cesarano <cesarano2607@gmail.com>
2019-01-10 15:53:37 +0100

    cli: add find_arxiv_duplicates command
    Signed-off-by: Antonio Cesarano <cesarano2607@gmail.com>

--- tests/integration/records/test_cli.py
+++ tests/integration/records/test_cli.py
@@ -24,7 +24,12 @@ from __future__ import absolute_import, division, print_function
 
 import os
 
-from inspirehep.modules.records.cli import check
+from invenio_db import db
+from invenio_workflows import workflow_object_class, ObjectStatus
+
+from inspirehep.modules.records.cli import check, find_arxiv_duplicates
+
+from factories.db.invenio_records import TestRecordMetadata
 
 
 def test_check_unlinked_references_generate_files(app_cli_runner, isolated_app, tmpdir):
@@ -64,3 +69,61 @@ def test_check_unlinked_references_works_without_arguments(app_cli_runner, isola
     os.remove("missing_cited_arxiv_eprints.txt")
 
     assert result.exit_code == 0
+
+
+def test_find_arxiv_duplicate_among_non_duplicates_and_merged(
+    app_cli_runner, isolated_app
+):
+    def create_wf(arxiv_id, control_number):
+        wf = workflow_object_class.create(
+            data_type='hep',
+            data={
+                'arxiv_eprints': [{'value': arxiv_id}],
+                'control_number': control_number
+            }
+        )
+        wf.status = ObjectStatus.COMPLETED
+        wf.save()
+
+    create_wf("1809.10823", 1)  # duplicate
+    create_wf("1809.10823", 2)  # duplicate
+    create_wf("1111.11111", 3)
+    create_wf("1809.10823", 4)  # duplicate but merged
+
+    TestRecordMetadata.create_from_kwargs(
+        json={
+            'arxiv_eprints': [{"value": "1809.10823"}],
+            'control_number': 1
+        }
+    )
+
+    TestRecordMetadata.create_from_kwargs(
+        json={
+            'arxiv_eprints': [{"value": "1809.10823"}],
+            'control_number': 2
+        }
+    )
+
+    TestRecordMetadata.create_from_kwargs(
+        json={
+            'arxiv_eprints': [{"value": "1111.11111"}],
+            'control_number': 3
+        }
+    )
+
+    TestRecordMetadata.create_from_kwargs(
+        json={
+            'arxiv_eprints': [{"value": "1809.10823"}],
+            'control_number': 4,
+            'deleted': True
+        }
+    )
+
+    db.session.commit()
+
+    result = app_cli_runner.invoke(find_arxiv_duplicates, [])
+
+    assert result.exit_code == 0
+    assert 'Found 1 arXiv duplicates' in result.output
+    assert '{"1809.10823": {"wf_ids": [2, 1], "recids": [2, 1]}}' in result.output
+    assert '4' not in result.output
