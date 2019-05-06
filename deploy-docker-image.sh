7d3ae8f0bd04a395589fd7f64443b58662958790
Benjamin Bergia <benjaminbergia@pcrcsisbb.dyndns.cern.ch>
2019-05-06 16:14:04 +0200

    Create a new image with assets already built in

--- deploy-docker-image.sh
+++ deploy-docker-image.sh
@@ -1,21 +1,39 @@
 #!/bin/bash -e
 
 retry() {
-    "$@" || "$@" || exit 2
+    "${@}" || "${@}" || exit 2
 }
 
-TAG="inspirehep/inspire-next:latest"
 
-echo "Building docker imagine"
-retry docker build -t "$TAG" .
+login() {
+  echo "Logging into Docker Hub"
+  retry docker login \
+      "--username=${DOCKER_USERNAME}" \
+      "--password=${DOCKER_PASSWORD}"
+}
 
-echo "Logging into Docker Hub"
-retry docker login \
-    "--username=$DOCKER_USERNAME" \
-    "--password=$DOCKER_PASSWORD"
 
-echo "Pushing image to ${TAG}"
-retry docker push "${TAG}"
+buildPush() {
+  TAG="${1}"
+  DOCKERFILE="${2:-Dockerfile}"
+  echo "Building docker imagine"
+  retry docker build -t "${TAG}" -f "${DOCKERFILE}" .
+  
+  echo "Pushing image to ${TAG}"
+  retry docker push "${TAG}"
+}
 
-echo "Logging out"
-retry docker logout
+
+logout() {
+  echo "Logging out""${@}"
+  retry docker logout
+}
+
+
+main() {
+  login
+  buildPush "inspirehep/inspire-next:latest"
+  buildPush "inspirehep/inspire-next:assets" Dockerfile.with_assets
+  logout
+}
+main
