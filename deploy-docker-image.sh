a7801dc3d76380d7e128bbf63663e416d314037e
Harris Tzovanakis <me@drjova.com>
2019-08-16 14:40:20 +0200

    setup: deploy on QA webhook
    Signed-off-by: Harris Tzovanakis <me@drjova.com>

--- deploy-docker-image.sh
+++ deploy-docker-image.sh
@@ -19,9 +19,10 @@ buildPush() {
   DOCKERFILE="${2:-Dockerfile}"
   GIT_DESC="$(git describe --always || echo)"
 
+
   echo "Building docker image"
   retry docker build -t "${IMAGE}:${TAG}" -f "${DOCKERFILE}" .
-  
+
   echo "Pushing image to ${IMAGE}:${TAG}"
   retry docker push "${IMAGE}:${TAG}"
 
@@ -36,6 +37,7 @@ buildPush() {
     echo "Pushing image to ${FULL_TAG}"
     docker tag "${IMAGE}:${TAG}" "${FULL_TAG}"
     retry docker push "${FULL_TAG}"
+    deployQA ${FULL_TAG}
   fi
 }
 
@@ -44,6 +46,17 @@ logout() {
   echo "Logging out""${@}"
   retry docker logout
 }
+
+deployQA() {
+  DEPLOY_TAG="${1}"
+
+  curl -X POST \
+    -F token=${DEPLOY_QA_TOKEN} \
+    -F ref=master \
+    -F variables[IMAGE_NAME]=inspirehep/hep \
+    -F variables[NEW_TAG]=${DEPLOY_TAG} \
+    https://gitlab.cern.ch/api/v4/projects/62928/trigger/pipeline
+ }
 
 
 main() {
