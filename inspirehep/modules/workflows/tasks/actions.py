736b7d28270fb66573fc95bec515e1d8ee78d0b2
MJedr <jedrych.marcysia@gmail.com>
2021-07-08 14:53:42 +0200

    refextract: don't raise error when document is xml ref: inspirehep/inspirehep#2027

--- inspirehep/modules/workflows/tasks/actions.py
+++ inspirehep/modules/workflows/tasks/actions.py
@@ -488,7 +488,7 @@ def refextract(obj, eng):
         text_references = dedupe_list(extract_references_from_text(text, source))
         matched_text_references = match_references_based_on_flag(text_references)
 
-    if len(matched_pdf_references) == len(matched_text_references) == 0:
+    if not matched_pdf_references and not matched_text_references:
         obj.log.info('No references extracted.')
     elif len(matched_pdf_references) > len(matched_text_references):
         obj.log.info('Extracted %d references from PDF.', len(matched_pdf_references))
