2d27cb1647001f293907191290b531c5afbf3c92
karolina-siemieniuk-morawska <karolina.siemieniukmorawska@gmail.com>
2022-05-03 16:11:19 +0200

    holdingpen: link workflow ids
    * use html tags inside string for proper render of links
    
    * ref: cern-sis/issues-inspire#20

--- inspirehep/modules/search/bundles.py
+++ inspirehep/modules/search/bundles.py
@@ -34,6 +34,6 @@ js = NpmBundle(
     npm={
         'invenio-search-js': '~1.4.0',
         'angular-loading-bar': '~0.9.0',
-        'inspirehep-search-js': '~2.0.72'
+        'inspirehep-search-js': '~2.0.74'
     },
 )
