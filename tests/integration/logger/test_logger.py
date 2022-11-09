02cad7b04906a71b994f1ccefada60763b8fd6ed
MJedr <jedrych.marcysia@gmail.com>
2022-11-09 15:38:50 +0100

    update logger settings

--- tests/integration/logger/test_logger.py
+++ tests/integration/logger/test_logger.py
@@ -9,4 +9,5 @@ from __future__ import absolute_import, division, print_function
 
 
 def test_authors_workflow_stops_when_record_is_not_valid(isolated_app):
+    assert not isolated_app.config['SENTRY_SEND_DEFAULT_PII']
     assert 'inspirehep-logger' in isolated_app.extensions
